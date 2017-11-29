d954bfa06b6f22c087df32be7154c807dd97499a
Anton Bannykh <anton.bannykh@gmail.com>
2017-11-29 11:41:35 +0300

    Initial commit
    Sample configuration
    
    :Sample tests, update Readme
    
    make gradle wrappers executable
    
    Add Travis CI config

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,15 @@
+fun main(args: Array<String>) {
+    println("Hello world!")
+}
+
+fun foo() = 10
+
+fun sum(vararg ints: Int): Int {
+    var result = 0
+    for (v in ints) {
+        result += v
+    }
+    return result
+}
+
+fun sumFun(vararg ints: Int) = ints.fold(0) {acc, i -> acc + i }
