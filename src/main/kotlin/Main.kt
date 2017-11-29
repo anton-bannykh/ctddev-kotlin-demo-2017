9688928f0dedaccb284bf1ea11002b6f62759fd8
Anton Bannykh <anton.bannykh@gmail.com>
2017-11-29 10:42:19 +0300

    Sample configuration

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,16 @@
+val a = 10
+
+fun main(args: Array<String>) {
+
+
+    args[1] = "sfsd"
+    val a = "sdfds"
+    val b = "sfds"
+
+    println("Hello world!" + "sdfdsf")
+}
+
+fun foo(ff: Int) {
+    var fm = ff
+    println(ff)
+}
