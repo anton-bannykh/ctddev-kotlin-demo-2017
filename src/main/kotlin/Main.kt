3ea8041701b7ebee72ce8e9ec3c7c919229e27be
Anton Bannykh <anton.bannykh@gmail.com>
2017-11-30 22:35:19 +0300

    :Sample tests, update Readme

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,16 +1,15 @@
-val a = 10
-
 fun main(args: Array<String>) {
-
-
-    args[1] = "sfsd"
-    val a = "sdfds"
-    val b = "sfds"
-
-    println("Hello world!" + "sdfdsf")
+    println("Hello world!")
 }
 
-fun foo(ff: Int) {
-    var fm = ff
-    println(ff)
+fun foo() = 10
+
+fun sum(vararg ints: Int): Int {
+    var result = 0
+    for (v in ints) {
+        result += v
+    }
+    return result
 }
+
+fun sumFun(vararg ints: Int) = ints.fold(0) {acc, i -> acc + i }
