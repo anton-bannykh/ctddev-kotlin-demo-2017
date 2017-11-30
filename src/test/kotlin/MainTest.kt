0799d9fed01113962332bf39e60f2add5a9e3bab
Alexandr Kravchenko <ratatuy.1995@mail.ru>
2017-12-01 01:22:33 +0300

    Quicksort with Test

--- src/test/kotlin/MainTest.kt
+++ src/test/kotlin/MainTest.kt
@@ -1,39 +1,56 @@
 import org.junit.Assert.assertEquals
+import org.junit.Assert.assertTrue
 import org.junit.Test
+import java.util.*
 
-class MainTest {
+/**
+ * Created by Александр on 29.11.2017.
+ */
+fun main(args: Array<String>) {
+
     @Test
-    fun testFoo() {
-        assertEquals(10, foo())
+    fun testSortedArray() {
+        val ms = intArrayOf(0, 1, 2, 3, 4, 5)
+        val ans = ms.clone()
+        Quicksort(ms)
+        ans.sort()
+        assertTrue(Arrays.equals(ans, ms))
+    }
+    @Test
+    fun testUnSortedArray() {
+        val ms = intArrayOf(100, 67, 32, 10, 0, -100)
+        val ans = ms.clone()
+        Quicksort(ms)
+        ans.sort()
+        assertTrue(Arrays.equals(ans, ms))
     }
 
     @Test
-    fun testSumEmpty() {
-        assertEquals(0, sum())
+    fun test12Element() {
+        val ms = intArrayOf(1, 4, 2, 3, 1, 8765, 876875, 876, 76786, 97678, 876876, -99999999)
+        val ans = ms.clone()
+        Quicksort(ms)
+        ans.sort()
+        assertTrue(Arrays.equals(ans, ms))
     }
 
     @Test
-    fun testSumSingle() {
-        assertEquals(42, sum(42))
+    fun test100Element() {
+        val ms = IntArray(100, { (-10000..10000).random() })
+        val ans = ms.clone()
+        Quicksort(ms)
+        ans.sort()
+        assertTrue(Arrays.equals(ans, ms))
     }
 
     @Test
-    fun testSumMany() {
-        assertEquals(6, sum(1, 2, 3))
+    fun test10000Element() {
+        val ms = IntArray(10000, { (-100000..100000).random() })
+        val ans = ms.clone()
+        Quicksort(ms)
+        ans.sort()
+        assertTrue(Arrays.equals(ans, ms))
     }
 
-    @Test
-    fun testSumFunEmpty() {
-        assertEquals(0, sumFun())
-    }
-
-    @Test
-    fun testSumFunSingle() {
-        assertEquals(42, sumFun(42))
-    }
-
-    @Test
-    fun testSumFunMany() {
-        assertEquals(6, sumFun(1, 2, 3))
-    }
 }
+
