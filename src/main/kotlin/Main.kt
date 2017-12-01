f5e091a2813f25f9f5af92389f8256c8f5cef133
Dogzik <levdovzhik@gmail.com>
2017-12-01 23:28:13 +0300

    Added HW1
    Dinitz's algorith implemintation added with some tests

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,15 +1,92 @@
-fun main(args: Array<String>) {
-    println("Hello world!")
-}
+import com.sun.jmx.remote.internal.ArrayQueue
+import kotlin.collections.ArrayList
+import kotlin.math.max
+import kotlin.math.min
 
-fun foo() = 10
+const val INF = (1e9).toInt()
 
-fun sum(vararg ints: Int): Int {
-    var result = 0
-    for (v in ints) {
-        result += v
+class Edge(val u: Int, val c: Int, var f: Int, val ind: Int, val rev: Int)
+
+fun bfs(g: Array<ArrayList<Edge>>, d: Array<Int>, s: Int, t: Int, a: Int): Boolean {
+    val n = g.size
+    d.fill(n + 10)
+    d[s] = 0
+    val que = ArrayQueue<Int>(n)
+    que.add(s)
+    while (!que.isEmpty()) {
+        val v = que.removeAt(0)
+        for (e in g[v]) {
+            val u = e.u
+            if ((e.c - e.f >= a) && (d[u] > d[v] + 1)) {
+                d[u] = d[v] + 1
+                que.add(u)
+            }
+        }
     }
-    return result
+    return (d[t] != n + 10)
 }
 
-fun sumFun(vararg ints: Int) = ints.fold(0) { acc, i -> acc + i }
+fun dfs(v: Int, minc: Int, g: Array<ArrayList<Edge>>, h: Array<Int>, d: Array<Int>, t: Int): Int {
+    if (v == t || minc == 0) {
+        return minc
+    }
+    while (h[v] < g[v].size) {
+        val e = g[v][h[v]]
+        if ((d[e.u] == d[v] + 1) && (e.c > e.f)) {
+            val delta = dfs(e.u, min(minc, e.c - e.f), g, h, d, t)
+            if (delta > 0) {
+                g[v][h[v]].f += delta
+                g[e.u][e.rev].f -= delta
+                return delta
+            }
+        }
+        ++h[v]
+    }
+    return 0
+}
+
+fun dinitz(g: Array<ArrayList<Edge>>, s: Int, t: Int): Array<Int> {
+    val d = Array(g.size, { 0 })
+    val h = Array(g.size, { 0 })
+    var cnt = 0
+    var maxc = -1
+    for (i in 0..(g.size - 1)) {
+        for (e in g[i]) {
+            maxc = max(maxc, e.c)
+            cnt = max(cnt, e.ind)
+        }
+    }
+    var a = 1
+    while (2 * a <= maxc) {
+        a *= 2
+    }
+    while (a > 0) {
+        while (bfs(g, d, s, t, a)) {
+            h.fill(0)
+            var flow = dfs(s, INF, g, h, d, t)
+            while (flow > 0) {
+                flow = dfs(s, INF, g, h, d, t)
+            }
+        }
+        a /= 2
+    }
+    val ans = Array(cnt, { 0 })
+    g.forEach { (it.filter { it.ind > 0 }).forEach { ans[it.ind - 1] = it.f } }
+    return ans
+}
+
+fun addEdge(a: Int, b: Int, c: Int, ind: Int, g: Array<ArrayList<Edge>>) {
+    val reva = g[b].size
+    val revb = g[a].size
+    g[a].add(Edge(b, c, 0, ind, reva))
+    g[b].add(Edge(a, 0, 0, -ind, revb))
+}
+
+fun maxFlow(n: Int, m: Int, v: Array<Int>, u: Array<Int>, c: Array<Int>, s: Int, t: Int): Array<Int> {
+    val g = Array(n) { ArrayList<Edge>() }
+    for (i in 0..(m - 1)) {
+        addEdge(v[i] - 1, u[i] - 1, c[i], i + 1, g)
+    }
+    return dinitz(g, s, t)
+}
+
--- src/test/kotlin/MainTest.kt
+++ src/test/kotlin/MainTest.kt
@@ -1,39 +1,124 @@
 import org.junit.Assert.assertEquals
 import org.junit.Test
+import kotlin.math.min
+import java.util.Random
 
 class MainTest {
+    class Graph(val v: Array<Int>, val u: Array<Int>, val c: Array<Int>)
+
+    private fun ClosedRange<Int>.random() = Random().nextInt(endInclusive + 1 - start) + start
+
+    private fun checkGraph(v: Int, g: Array<ArrayList<Int>>, used: Array<Boolean>, t: Int): Boolean {
+        if (v == t) {
+            return true
+        }
+        used[v] = true
+        return g[v].any { !used[it] && checkGraph(it, g, used, t) }
+    }
+
+    private fun genTest(n: Int, m: Int, maxc: Int): Graph {
+        while (true) {
+            val v = Array(m, { 0 })
+            val u = Array(m, { 0 })
+            val g = Array(n, { ArrayList<Int>() })
+            var ind = 0
+            val eds = Array(n + 1, { Array(n + 1) { false } })
+            while (ind < m) {
+                val a = (1..n).random()
+                val b = (1..n).random()
+                if ((a == b) || eds[a][b]) {
+                    continue
+                }
+                eds[a][b] = true
+                v[ind] = a
+                u[ind] = b
+                ++ind
+            }
+            for (i in 0..(m - 1)) {
+                g[v[i] - 1].add(u[i] - 1)
+            }
+            val used = Array(n, { false })
+            if (!checkGraph(0, g, used, n - 1)) {
+                continue
+            }
+            val c = Array(m) { (1..maxc).random() }
+            return Graph(v, u, c)
+        }
+    }
+
+    private fun runTest(n: Int, m: Int, maxc: Int) {
+        val data = genTest(n, m, maxc)
+        val ans = maxFlow(n, m, data.v, data.u, data.c, 0, n - 1)
+        val g = Array<ArrayList<Int>>(n) { _ -> ArrayList() }
+        ((1..(m - 1)).filter { data.c[it] - ans[it] > 0 }).forEach { g[data.v[it] - 1].add(data.u[it] - 1) }
+        val used = Array(n, { false })
+        assertEquals(false, checkGraph(0, g, used, n - 1))
+    }
+
+
     @Test
-    fun testFoo() {
-        assertEquals(10, foo())
+    fun testSmall() {
+        val n = 4
+        val m = 6
+        val maxc = 8
+        runTest(n, m, maxc)
     }
 
     @Test
-    fun testSumEmpty() {
-        assertEquals(0, sum())
+    fun testSmallWide() {
+        val n = 4
+        val m = 6
+        val maxc = 100000
+        runTest(n, m, maxc)
     }
 
     @Test
-    fun testSumSingle() {
-        assertEquals(42, sum(42))
+    fun testRandSmall() {
+        val n = (2..10).random()
+        val m = (1..min(50, n * (n - 1))).random()
+        val maxc = (1..100).random()
+        runTest(n, m, maxc)
     }
 
     @Test
-    fun testSumMany() {
-        assertEquals(6, sum(1, 2, 3))
+    fun testRandSmallWide() {
+        val n = (2..10).random()
+        val m = (1..min(50, n * (n - 1))).random()
+        val maxc = 10000
+        runTest(n, m, maxc)
+    }
+
+
+    @Test
+    fun testRand1() {
+        val n = (2..1000).random()
+        val m = (1..min(1000, n * (n - 1))).random()
+        val maxc = (1..1000_000_000).random()
+        runTest(n, m, maxc)
     }
 
     @Test
-    fun testSumFunEmpty() {
-        assertEquals(0, sumFun())
+    fun testRand2() {
+        val n = (2..1000).random()
+        val m = (1..min(1000, n * (n - 1))).random()
+        val maxc = (1..1000_000_000).random()
+        runTest(n, m, maxc)
     }
 
     @Test
-    fun testSumFunSingle() {
-        assertEquals(42, sumFun(42))
+    fun testRand3() {
+        val n = (2..1000).random()
+        val m = (1..min(1000, n * (n - 1))).random()
+        val maxc = (1..1000_000_000).random()
+        runTest(n, m, maxc)
     }
 
     @Test
-    fun testSumFunMany() {
-        assertEquals(6, sumFun(1, 2, 3))
+    fun testMax() {
+        val n = 1000
+        val m = 1000
+        val maxc = 1000_000_000
+        runTest(n, m, maxc)
     }
+
 }
