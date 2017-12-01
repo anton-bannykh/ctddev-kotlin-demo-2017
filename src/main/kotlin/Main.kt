70db199599c592645e9e6ba49039db2c6308820a
MadHypnofrog <madhypnofrog@gmail.com>
2017-12-01 22:38:11 +0300

    hw1

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,15 +1,34 @@
-fun main(args: Array<String>) {
-    println("Hello world!")
+import java.util.Stack
+
+fun main (args: Array<String>) {
+    println ()
+    return
 }
 
-fun foo() = 10
-
-fun sum(vararg ints: Int): Int {
-    var result = 0
-    for (v in ints) {
-        result += v
+fun dfsIterative (n: Int, graph: Array<Pair<Int, Int>>): MutableList<Int> {
+    //graph is defined by its edges; the returned list is the order in which the vertices are visited
+    //if a vertex has multiple neighbors, they are visited in an ascending order
+    //the first vertex is always (1)
+    val s = Stack<Int>()
+    val g: MutableList<MutableList<Int>> = mutableListOf()
+    for (i in 0..n - 1) g.add(mutableListOf())
+    for (i in graph.indices) {
+        g[graph[i].first - 1].add(graph[i].second - 1)
+        g[graph[i].second - 1].add(graph[i].first - 1)
     }
-    return result
+    for (i in 0..n - 1) g[i].sortDescending()
+    val res: MutableList<Int> = mutableListOf()
+    val v = Array<Boolean> (n, { false })
+    s.add(0)
+    while (!s.empty()) {
+        val e = s.pop()
+        if (!v[e]) {
+            v[e] = true
+            res.add(e + 1)
+        }
+        for (i in g[e]) {
+            if (!v[i]) s.add(i)
+        }
+    }
+    return res
 }
-
-fun sumFun(vararg ints: Int) = ints.fold(0) { acc, i -> acc + i }
