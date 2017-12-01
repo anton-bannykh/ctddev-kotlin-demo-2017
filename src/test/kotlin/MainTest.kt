70cebb58e87e023d7dc75b243926ef85914c4186
DmitryBelikov <belikov.dima@mail.ru>
2017-12-01 20:41:49 +0300

    HW1 added

--- src/test/kotlin/MainTest.kt
+++ src/test/kotlin/MainTest.kt
@@ -3,37 +3,103 @@ import org.junit.Test
 
 class MainTest {
     @Test
-    fun testFoo() {
-        assertEquals(10, foo())
+    fun test1() {
+        val n = 4
+        val edges = Array(5, {Edge(0, 0, 0)})
+
+        edges[0] = Edge(0, 1, 1)
+        edges[1] = Edge(0, 2, 2)
+        edges[2] = Edge(2, 1, 1)
+        edges[3] = Edge(1, 3, 2)
+        edges[4] = Edge(2, 3, 1)
+
+        val expectedResult = 3
+
+        assertEquals(expectedResult, solve(n, edges))
     }
 
     @Test
-    fun testSumEmpty() {
-        assertEquals(0, sum())
+    fun test2() {
+        val n = 5
+        val edges = Array(7, {Edge(0, 0, 0)})
+
+        edges[0] = Edge(0, 1, 1)
+        edges[1] = Edge(1, 4, 5)
+        edges[2] = Edge(0, 2, 6)
+        edges[3] = Edge(2, 3, 2)
+        edges[4] = Edge(3, 4, 1)
+        edges[5] = Edge(2, 1, 3)
+        edges[6] = Edge(1, 3, 1)
+
+        val expectedResult = 6
+
+        assertEquals(expectedResult, solve(n, edges))
     }
 
     @Test
-    fun testSumSingle() {
-        assertEquals(42, sum(42))
+    fun test3() {
+        val n = 4
+        val edges = Array(5, {Edge(0, 0, 0 )})
+
+        edges[0] = Edge(0, 1, 1000000000)
+        edges[1] = Edge(0, 2, 1000000000)
+        edges[2] = Edge(1, 2, 1)
+        edges[3] = Edge(2, 3, 1000000000)
+        edges[4] = Edge(1, 3, 1000000000)
+
+        val expectedResult = 2000000000
+
+        assertEquals(expectedResult, solve(n, edges))
     }
 
     @Test
-    fun testSumMany() {
-        assertEquals(6, sum(1, 2, 3))
+    fun testEmptyGraph() {
+        val n = 100
+        val edges = Array(0, {Edge(0, 0, 0)})
+
+        val expectedResult = 0
+
+        assertEquals(expectedResult, solve(n, edges))
     }
 
     @Test
-    fun testSumFunEmpty() {
-        assertEquals(0, sumFun())
+    fun testSimpleGraph() {
+        val n = 2
+        val edges = Array(1, {Edge(0, 1, 5)})
+
+        val expectedResult = 5
+
+        assertEquals(expectedResult, solve(n, edges))
     }
 
     @Test
-    fun testSumFunSingle() {
-        assertEquals(42, sumFun(42))
+    fun testGraphNoWay() {
+        val n = 5
+        val edges = Array(5, {Edge(0, 0, 0 )})
+
+        edges[0] = Edge(0, 1, 2)
+        edges[1] = Edge(0, 2, 4)
+        edges[2] = Edge(1, 2, 7)
+        edges[3] = Edge(3, 4, 7)
+        edges[4] = Edge(4, 3, 8)
+
+        val expectedResult = 0
+
+        assertEquals(expectedResult, solve(n, edges))
     }
 
     @Test
-    fun testSumFunMany() {
-        assertEquals(6, sumFun(1, 2, 3))
+    fun testBigGraph() {
+        val n = 100
+        val m = 10000
+        val edges = Array(m, {Edge(0, 0, 0)})
+
+        for(i in 0 until m) {
+            edges[i] = Edge(i / n, i % n, i * i % 12345)
+        }
+
+        val expectedResult = 897810
+
+        assertEquals(expectedResult, solve(n, edges))
     }
 }
