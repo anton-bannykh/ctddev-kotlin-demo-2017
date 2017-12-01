65dae3986db76b70a99e0b5cd419fb71119588bb
rofleksey <evilmorty@yandex.ru>
2017-12-01 20:30:56 +0300

    hw1

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,15 +1,76 @@
-fun main(args: Array<String>) {
-    println("Hello world!")
-}
+import kotlin.collections.ArrayList
+import java.util.TreeSet
 
-fun foo() = 10
+val INF = 1000000000
 
-fun sum(vararg ints: Int): Int {
-    var result = 0
-    for (v in ints) {
-        result += v
+class Graph {
+    class Edge(val from: Int, val to: Int, val len: Int)
+    class Vertex(val num: Int) : Comparable<Vertex> {
+        var key: Int = INF
+        override fun compareTo(other: Vertex) = key.compareTo(other.key)
     }
-    return result
+
+    var graph: ArrayList<MutableList<Edge>> = ArrayList()
+    var vertices: ArrayList<Vertex> = ArrayList()
+    var vertexCount: Int = 0
+    fun addEdge(a: Int, b: Int, c: Int) {
+        graph[a - 1].add(Edge(a - 1, b - 1, c))
+        graph[b - 1].add(Edge(b - 1, a - 1, c))
+    }
+
+    fun setSize(size: Int) {
+        vertexCount = size
+        graph.ensureCapacity(size)
+        vertices.ensureCapacity(size)
+        for (i in 0..vertexCount) {
+            graph.add(mutableListOf())
+            vertices.add(Vertex(i))
+        }
+    }
+
+    fun getMST(): List<Pair<Int, Int>> {
+        val p = IntArray(vertexCount, { _ -> -1 })
+        val isInQueue = BooleanArray(vertexCount, { _ -> true })
+        vertices[0].key = 0
+        val q = TreeSet<Vertex>()
+        for (i in 0..vertexCount - 1) {
+            q.add(vertices[i])
+        }
+        val result = mutableListOf<Pair<Int, Int>>()
+        while (!q.isEmpty()) {
+            val v = q.pollFirst().num
+            if (p[v] != -1) {
+                result.add(Pair(v, p[v]))
+            }
+            isInQueue[v] = false
+            for (e in graph[v]) {
+                if (isInQueue[e.to] && vertices[e.to].key > e.len) {
+                    p[e.to] = v
+                    q.remove(vertices[e.to])
+                    vertices[e.to].key = e.len
+                    q.add(vertices[e.to])
+                }
+            }
+        }
+        return result
+    }
+
+    fun getAnswAsString(): String {
+        val edges = getMST().sortedWith(compareBy({ it.second }, { it.first }))
+        val sb = StringBuilder()
+        for (e in edges) {
+            sb.append(Math.min(e.first + 1, e.second + 1).toString() + " " + Math.max(e.first + 1, e.second + 1) + " ")
+        }
+        return sb.toString()
+    }
 }
 
-fun sumFun(vararg ints: Int) = ints.fold(0) { acc, i -> acc + i }
+fun main(args: Array<String>) {
+    val g = Graph()
+    g.setSize(3)
+    g.addEdge(1, 2, 5)
+    g.addEdge(1, 3, 10)
+    g.addEdge(2, 3, 15)
+    println(g.getAnswAsString())
+
+}
--- src/test/kotlin/MainTest.kt
+++ src/test/kotlin/MainTest.kt
@@ -3,37 +3,58 @@ import org.junit.Test
 
 class MainTest {
     @Test
-    fun testFoo() {
-        assertEquals(10, foo())
+    fun testK1() {
+        var g = Graph()
+        g.setSize(1)
+        assertEquals(g.getAnswAsString(), "")
     }
 
     @Test
-    fun testSumEmpty() {
-        assertEquals(0, sum())
+    fun testK2() {
+        var g = Graph()
+        g.setSize(2)
+        g.addEdge(1, 2, 5)
+        assertEquals(g.getAnswAsString(), "1 2 ")
     }
 
     @Test
-    fun testSumSingle() {
-        assertEquals(42, sum(42))
+    fun testK3() {
+        var g = Graph()
+        g.setSize(3)
+        g.addEdge(1, 2, 5)
+        g.addEdge(1, 3, 10)
+        g.addEdge(2, 3, 15)
+        assertEquals("1 2 1 3 ", g.getAnswAsString())
     }
 
     @Test
-    fun testSumMany() {
-        assertEquals(6, sum(1, 2, 3))
+    fun testFromNeerc() {
+        var g = Graph()
+        g.setSize(5)
+        g.addEdge(1, 4, 1)
+        g.addEdge(1, 2, 3)
+        g.addEdge(1, 3, 4)
+        g.addEdge(2, 3, 5)
+        g.addEdge(3, 4, 6)
+        g.addEdge(3, 5, 2)
+        g.addEdge(4, 5, 7)
+        assertEquals("1 2 1 3 1 4 3 5 ", g.getAnswAsString())
     }
 
     @Test
-    fun testSumFunEmpty() {
-        assertEquals(0, sumFun())
-    }
-
-    @Test
-    fun testSumFunSingle() {
-        assertEquals(42, sumFun(42))
-    }
-
-    @Test
-    fun testSumFunMany() {
-        assertEquals(6, sumFun(1, 2, 3))
+    fun test8() {
+        var g = Graph()
+        g.setSize(8)
+        g.addEdge(1, 2, 6)
+        g.addEdge(2, 3, 17)
+        g.addEdge(1, 4, 25)
+        g.addEdge(3, 4, 8)
+        g.addEdge(2, 4, 11)
+        g.addEdge(4, 5, 2)
+        g.addEdge(5, 6, 21)
+        g.addEdge(5, 7, 14)
+        g.addEdge(7, 8, 9)
+        g.addEdge(8, 1, 19)
+        assertEquals("1 2 2 4 3 4 4 5 5 6 5 7 7 8 ", g.getAnswAsString())
     }
 }
