1cf071c66fe51376ea3ad750242d7661db496e34
kuzmin-ivan <Kuzmin99Ivan@yandex.ru>
2017-12-02 01:18:40 +0300

    heapSort

--- src/test/kotlin/MainTest.kt
+++ src/test/kotlin/MainTest.kt
@@ -1,39 +1,49 @@
-import org.junit.Assert.assertEquals
+import org.junit.Assert.assertArrayEquals
 import org.junit.Test
 
 class MainTest {
     @Test
-    fun testFoo() {
-        assertEquals(10, foo())
+    fun testUnsorted() {
+        val actual = arrayOf(32, -12, 2, 1, -4, -18, 21, 13, 8, 65)
+        val expected = actual.clone()
+        heapSort(actual)
+        expected.sort()
+        assertArrayEquals(expected, actual)
     }
 
     @Test
-    fun testSumEmpty() {
-        assertEquals(0, sum())
+    fun testSorted() {
+        val actual = arrayOf(0, 1, 2, 3, 4, 5, 6, 7, 8, 9)
+        val expected = actual.clone()
+        heapSort(actual)
+        expected.sort()
+        assertArrayEquals(expected, actual)
     }
 
     @Test
-    fun testSumSingle() {
-        assertEquals(42, sum(42))
+    fun randomTest1000Elements() {
+        val actual = Array(1000, { (-10000..10000).random() })
+        val expected = actual.clone()
+        heapSort(actual)
+        expected.sort()
+        assertArrayEquals(expected, actual)
     }
 
     @Test
-    fun testSumMany() {
-        assertEquals(6, sum(1, 2, 3))
+    fun randomTest10000Element() {
+        val actual = Array(10000, { (-100000..100000).random() })
+        val expected = actual.clone()
+        heapSort(actual)
+        expected.sort()
+        assertArrayEquals(expected, actual)
     }
 
     @Test
-    fun testSumFunEmpty() {
-        assertEquals(0, sumFun())
-    }
-
-    @Test
-    fun testSumFunSingle() {
-        assertEquals(42, sumFun(42))
-    }
-
-    @Test
-    fun testSumFunMany() {
-        assertEquals(6, sumFun(1, 2, 3))
+    fun randomTest100000elements() {
+        val actual = Array(100000, { (-1000000..1000000).random() })
+        val expected = actual.clone()
+        heapSort(actual)
+        expected.sort()
+        assertArrayEquals(expected, actual)
     }
 }
