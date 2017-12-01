d3e3dc63609295eb250667fd81341019febed88c
KokorinIlya <kokorin.ilya.1998@yandex.ru>
2017-12-02 00:56:10 +0300

    Added algorithm and tests

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,15 +1,177 @@
-fun main(args: Array<String>) {
-    println("Hello world!")
-}
+import java.util.*
+import kotlin.collections.ArrayList
 
-fun foo() = 10
+val INF = Int.MAX_VALUE
 
-fun sum(vararg ints: Int): Int {
-    var result = 0
-    for (v in ints) {
-        result += v
+//Scans one number from the Scanner. If fails, throws Invalid input exception
+fun getNextNumber(scanner: Scanner): Int {
+    try {
+        return scanner.nextInt()
     }
-    return result
+    catch (e: Exception) {
+        throw RuntimeException("Not a valid number, please, check input")
+    }
 }
 
-fun sumFun(vararg ints: Int) = ints.fold(0) { acc, i -> acc + i }
+typealias Graph = Array<ArrayList<Pair<Int, Int>>>
+
+//Reads graph from console input
+//element number 0 is a fake vertex, used in algorithm
+//graph[v] = [{u, w}], where vu in E and weight(vu) = w
+fun getGraph() : Graph {
+    val scanner = Scanner(System.`in`)
+    //Number of vertex and edges, |V| and |E|
+    val n = getNextNumber(scanner)
+    val m = getNextNumber(scanner)
+    val graph = Array<ArrayList<Pair<Int, Int>>>(n + 1, {i -> ArrayList() })
+    for (i in 0..m - 1) {
+        val u = getNextNumber(scanner)
+        val v = getNextNumber(scanner)
+        val w = getNextNumber(scanner)
+        graph[u].add(Pair(v, w))
+        //adds edge uv, weight(uw) = w
+    }
+    for (i in 1..n) {
+        //adds edges from fake vertex to other vertex with weight 0
+        graph[0].add(Pair(i, 0))
+    }
+    scanner.close()
+    return graph
+}
+
+//returns distances from start vertex to other vertex, if there is no negative cycle
+//if there is negative cycle, returns null
+fun bellmanFord(graph: Graph) : Array<Int>? {
+    val dist = Array(graph.size, {i ->
+        if (i == 0)
+            0
+        else
+            INF
+    }) //d <- INF, d[s] = 0
+
+    //Body of Bellman-Ford's algorithm, finds distance from s to other vertex
+    //Works with negative weights
+    for (i in 0..graph.size - 1) {
+        for (v in 0..graph.size - 1) {
+            if (dist[v] < INF) {
+                for (t in graph[v]) { //relaxation (v, u)
+                    val u = t.first
+                    val w = t.second
+                    dist[u] = minOf(dist[u], dist[v] + w)
+                }
+            }
+        }
+    }
+
+    //if we can do relaxation after the algorithm body, there is a negative cycle
+    for (v in 0..graph.size - 1) {
+        if (dist[v] < INF) {
+            for (t in graph[v]) {
+                val u = t.first
+                val w = t.second
+                if (dist[u] > dist[v] + w) {
+                    //if relaxation succeed, we've found a vertex on a negative cycle
+                    return null
+                }
+            }
+        }
+    }
+    return dist
+}
+
+//Dijkstra's algorithm. Finds paths with min weight from start vertex to other vertex
+//if al weights are not-negative
+//returns dist, where d[v] is distance from s to v, INF when there are no paths P: s~>v
+fun dijkstra(graph: Graph, q: Array<Int>, s: Int) : Array<Int> {
+    val dist = Array(graph.size, {i ->
+        if (i == s)
+            0
+        else
+            INF
+    }) //d <- INF, d[s] = 0
+
+    val used = Array(graph.size, {i -> false}) //used <- false
+
+    while (true) {
+        var z = -1
+        for (v in 1..graph.size - 1) {
+            if (!used[v] && dist[v] < INF) {
+                z = v
+                break
+            }
+        } //find first unused vertex
+        if (z == -1) { //if all vertex have been used, algorithm has found answer
+            return dist
+        }
+        for (v in z + 1..graph.size - 1) {
+            if (!used[v] && dist[v] < dist[z]) {
+                z = v
+            }
+        } //finds unused vertex with min distance from s to v
+        used[z] = true
+        for (t in graph[z]) {
+            val u = t.first
+            val w = t.second
+            dist[u] = minOf(dist[u], dist[z] + w + q[z] - q[u])
+            //relaxes (z, u), using weight' = weight(zu) + q[z] - q[u]
+            //where q - distances from s to other vertex
+        }
+    }
+}
+
+//Gets graph with fake vertex
+//Finds distances between all vertices, returns null if there is negative cycle
+// and distances if there are no negative cycles, d[v][u] is distance from v to u, INF if there are no paths
+// P: v~>u
+fun johnson(graph: Graph): Array<Array<Int>>? {
+    val n = graph.size - 1
+    val q = bellmanFord(graph) //returns null when there is negative cycle
+    if (q == null) {
+        //Negative cycle
+        return null
+    }
+    val ans = Array(n, { i -> Array(n, { i -> 0 }) })
+    for (v in 1..n) {
+        ans[v - 1] = dijkstra(graph, q, v).sliceArray(1..n)
+        //don't get fake vertex(has number 0)
+        for (u in 1..n) {
+            if (ans[v - 1][u - 1] == INF) {
+                //there is no path P: v~>u
+                //print("INF ")
+                continue
+            }
+            ans[v - 1][u - 1] += (q[u] - q[v])
+            //reestablish distances. If w' = w + q[v] - q[u] => w = w' - q[v] + q[u]
+        }
+    }
+    return ans
+}
+
+fun main(args: Array<String>) {
+    try {
+        val graph = getGraph()
+        val answer = johnson(graph)
+        if (answer == null) {
+            //if Johnson's algorithm has found negative cycle and returned null
+            println("Negative cycle");
+            return
+        }
+        for (x in answer) {
+            //distanced between vertex
+            for (d in x) {
+                if (d == INF) {
+                    //No path v~>u
+                    print("INF ")
+                    continue
+                }
+                print("$d ")
+            }
+            println()
+        }
+    } catch (e: Exception) {
+        //If input is incorrect, let user know about ir
+        println(e.message)
+        return
+    }
+    return
+}
--- src/test/kotlin/MainTest.kt
+++ src/test/kotlin/MainTest.kt
@@ -1,39 +1,138 @@
+import org.junit.Assert.assertArrayEquals
 import org.junit.Assert.assertEquals
 import org.junit.Test
 
 class MainTest {
     @Test
-    fun testFoo() {
-        assertEquals(10, foo())
+    fun test1() {
+        val graph = Array<ArrayList<Pair<Int, Int>>>(5, {i -> ArrayList() })
+        for (i in 1..4) {
+            graph[0].add(Pair(i, 0))
+        }
+        graph[1].add(Pair(2, 1))
+        graph[2].add(Pair(3, 3))
+        graph[4].add(Pair(3, -1))
+        graph[3].add(Pair(1, -2))
+        graph[2].add(Pair(4, 5))
+        val answer = arrayOf(
+                arrayOf(0, 1, 4, 6),
+                arrayOf(1, 0, 3, 5),
+                arrayOf(-2, -1, 0, 4),
+                arrayOf(-3, -2, -1, 0)
+        )
+        assertEquals(answer, johnson(graph))
     }
 
     @Test
-    fun testSumEmpty() {
-        assertEquals(0, sum())
+    fun test2() {
+        val graph = Array<ArrayList<Pair<Int, Int>>>(4, {i -> ArrayList() })
+        for (i in 1..3) {
+            graph[0].add(Pair(i, 0))
+        }
+        graph[1].add(Pair(2, -1))
+        graph[2].add(Pair(3, -1))
+        graph[3].add(Pair(1, -1))
+        assertEquals(null, johnson(graph))
     }
 
     @Test
-    fun testSumSingle() {
-        assertEquals(42, sum(42))
+    fun test3() {
+        val graph = Array<ArrayList<Pair<Int, Int>>>(3, {i -> ArrayList() })
+        for (i in 1..2) {
+            graph[0].add(Pair(i, 0))
+        }
+        graph[1].add(Pair(2, 1))
+        val answer = arrayOf(
+                arrayOf(0, 1),
+                arrayOf(Int.MAX_VALUE, 0)
+        )
+        assertEquals(answer, johnson(graph))
     }
 
     @Test
-    fun testSumMany() {
-        assertEquals(6, sum(1, 2, 3))
+    fun test4() {
+        val graph = Array<ArrayList<Pair<Int, Int>>>(6, {i -> ArrayList() })
+        for (i in 1..5) {
+            graph[0].add(Pair(i, 0))
+        }
+        graph[1].add(Pair(2, 2))
+        graph[2].add(Pair(3, 3))
+        graph[3].add(Pair(4, -1))
+        graph[4].add(Pair(5, 1))
+        graph[5].add(Pair(3, -10))
+        assertEquals(null, johnson(graph))
     }
 
     @Test
-    fun testSumFunEmpty() {
-        assertEquals(0, sumFun())
+    fun test5() {
+        val graph = Array<ArrayList<Pair<Int, Int>>>(5, {i -> ArrayList() })
+        for (i in 1..4) {
+            graph[0].add(Pair(i, 0))
+        }
+        graph[1].add(Pair(2, -1))
+        graph[2].add(Pair(3, 1))
+        graph[2].add(Pair(4, 10))
+        graph[3].add(Pair(4, 1))
+        val answer = arrayOf(
+                arrayOf(0, -1, 0, 1),
+                arrayOf(Int.MAX_VALUE, 0, 1, 2),
+                arrayOf(Int.MAX_VALUE, Int.MAX_VALUE, 0, 1),
+                arrayOf(Int.MAX_VALUE, Int.MAX_VALUE, Int.MAX_VALUE,0)
+        )
+        assertEquals(answer, johnson(graph))
     }
 
     @Test
-    fun testSumFunSingle() {
-        assertEquals(42, sumFun(42))
+    fun test6() {
+        val graph = Array<ArrayList<Pair<Int, Int>>>(8, {i -> ArrayList() })
+        for (i in 1..7) {
+            graph[0].add(Pair(i, 0))
+        }
+        graph[1].add(Pair(2, -1))
+        graph[1].add(Pair(3, 1))
+        graph[2].add(Pair(4, -10))
+        graph[3].add(Pair(6, 10))
+        graph[4].add(Pair(5, 0))
+        graph[5].add(Pair(6, 0))
+        graph[6].add(Pair(7, 0))
+        graph[7].add(Pair(4, 0))
+        val answer = arrayOf(
+                arrayOf(0, -1, 1, -11, -11, -11, -11),
+                arrayOf(Int.MAX_VALUE, 0, Int.MAX_VALUE, -10, -10, -10, -10),
+                arrayOf(Int.MAX_VALUE, Int.MAX_VALUE, 0, 10, 10, 10, 10),
+                arrayOf(Int.MAX_VALUE, Int.MAX_VALUE, Int.MAX_VALUE, 0, 0, 0, 0),
+                arrayOf(Int.MAX_VALUE, Int.MAX_VALUE, Int.MAX_VALUE, 0, 0, 0, 0),
+                arrayOf(Int.MAX_VALUE, Int.MAX_VALUE, Int.MAX_VALUE, 0, 0, 0, 0),
+                arrayOf(Int.MAX_VALUE, Int.MAX_VALUE, Int.MAX_VALUE, 0, 0, 0, 0)
+        )
+        assertEquals(answer, johnson(graph))
     }
 
     @Test
-    fun testSumFunMany() {
-        assertEquals(6, sumFun(1, 2, 3))
+    fun test7() {
+        val graph = Array<ArrayList<Pair<Int, Int>>>(8, {i -> ArrayList() })
+        for (i in 1..7) {
+            graph[0].add(Pair(i, 0))
+        }
+        graph[1].add(Pair(2, 1))
+        graph[1].add(Pair(5, -1))
+        graph[2].add(Pair(3, 1))
+        graph[2].add(Pair(4, -1))
+        graph[5].add(Pair(6, 1))
+        graph[5].add(Pair(7, -1))
+        graph[6].add(Pair(4, 1))
+        graph[4].add(Pair(3, 1))
+        graph[3].add(Pair(7,- 1))
+        graph[7].add(Pair(1, 10))
+        val answer = arrayOf(
+                arrayOf(0, 1, 1, 0, -1, 0, -2),
+                arrayOf(9, 0, 0, -1, 8, 9, -1),
+                arrayOf(9, 10, 0, 9, 8, 9, -1),
+                arrayOf(10, 11, 1, 0, 9, 10, 0),
+                arrayOf(9, 10, 3, 2, 0, 1, -1),
+                arrayOf(11, 12, 2, 1, 10, 0, 1),
+                arrayOf(10, 11, 11, 10, 9, 10, 0)
+        )
+        assertEquals(answer, johnson(graph))
     }
 }
