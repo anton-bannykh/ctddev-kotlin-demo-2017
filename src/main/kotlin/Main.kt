6b9ee78374bdf06c6e44a4d9713e3cf646a08667
kuzmin-ivan <Kuzmin99Ivan@yandex.ru>
2017-12-02 01:18:40 +0300

    heapSort

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,15 +1,35 @@
-fun main(args: Array<String>) {
-    println("Hello world!")
-}
+import java.util.Random
 
-fun foo() = 10
-
-fun sum(vararg ints: Int): Int {
-    var result = 0
-    for (v in ints) {
-        result += v
+fun heapSort(a: Array<Int>) {
+    buildHeap(a)
+    var n = a.size
+    while (n - 1 > 0) {
+        a[n - 1] = a[0].also { a[0] = a[n - 1] }
+        siftDown(a, 0, n - 1)
+        n--
     }
-    return result
 }
 
-fun sumFun(vararg ints: Int) = ints.fold(0) { acc, i -> acc + i }
+fun buildHeap(a: Array<Int>) {
+    for (i in a.size / 2 downTo 0) {
+        siftDown(a, i, a.size)
+    }
+}
+
+fun siftDown(a: Array<Int>, index: Int, size: Int) {
+    var i = index
+    while (i * 2 + 1 < size) {
+        var child = 2 * i + 1
+        if (child + 1 < size && a[child] < a[child + 1]) {
+            child++
+        }
+        if (a[i] < a[child]) {
+            a[child] = a[i].also { a[i] = a[child] }
+            i = child
+        } else {
+            return
+        }
+    }
+}
+
+fun ClosedRange<Int>.random() = Random().nextInt(endInclusive + 1 - start) + start
