6c346f5f3ea3f3f72b2cff5db76d419f546172ab
RuslanKhalimov <khalimov.ruslan@mail.ru>
2017-12-02 23:33:14 +0300

    HW1

--- src/test/kotlin/MainTest.kt
+++ src/test/kotlin/MainTest.kt
@@ -1,39 +1,91 @@
-import org.junit.Assert.assertEquals
+import org.junit.Assert.*
 import org.junit.Test
 
 class MainTest {
-    @Test
-    fun testFoo() {
-        assertEquals(10, foo())
+    fun checkAnswer(edges: Array<ArrayList<Int>>, answer: ArrayList<Int>?, n: Int, m: Int) {
+        if (answer == null) {
+            fail()
+            return
+        }
+
+        assertEquals(answer[0], answer[answer.size - 1])
+        assertEquals(answer.size, m + 1)
+
+        for (i in 1..answer.size - 1) {
+            assertTrue(edges[answer[i - 1]].contains(answer[i]))
+            edges[answer[i - 1]].remove(answer[i])
+        }
     }
 
     @Test
-    fun testSumEmpty() {
-        assertEquals(0, sum())
+    fun test1() {
+        val n = 3
+        val m = 3
+        val edges = Array(n, { ArrayList<Int>() })
+
+        edges[0] = arrayListOf(1)
+        edges[1] = arrayListOf(2)
+        edges[2] = arrayListOf(0)
+
+        checkAnswer(edges, solve(edges, n), n, m)
     }
 
     @Test
-    fun testSumSingle() {
-        assertEquals(42, sum(42))
+    fun test2() {
+        val n = 6
+        val m = 11
+        val edges = Array(n, { ArrayList<Int>() })
+
+        edges[0] = arrayListOf(1)
+        edges[1] = arrayListOf(2, 3)
+        edges[2] = arrayListOf(4, 5)
+        edges[3] = arrayListOf(2, 5)
+        edges[4] = arrayListOf(0, 3)
+        edges[5] = arrayListOf(1, 4)
+
+        checkAnswer(edges, solve(edges, n), n, m)
     }
 
     @Test
-    fun testSumMany() {
-        assertEquals(6, sum(1, 2, 3))
+    fun test3() {
+        val n = 2
+        val m = 1
+        val edges = Array(n, { ArrayList<Int>() })
+
+        edges[0] = arrayListOf(1)
+
+        assertNull(solve(edges, n))
     }
 
     @Test
-    fun testSumFunEmpty() {
-        assertEquals(0, sumFun())
+    fun test4() {
+        val n = 6
+        val m = 6
+        val edges = Array(n, { ArrayList<Int>() })
+
+        edges[0] = arrayListOf(1)
+        edges[1] = arrayListOf(2)
+        edges[2] = arrayListOf(0)
+
+        edges[3] = arrayListOf(4)
+        edges[4] = arrayListOf(5)
+        edges[5] = arrayListOf(3)
+
+        assertNull(solve(edges, n))
     }
 
     @Test
-    fun testSumFunSingle() {
-        assertEquals(42, sumFun(42))
-    }
+    fun test5() {
+        val n = 10
+        val m = 10
+        val edges = Array(n, { ArrayList<Int>() })
 
-    @Test
-    fun testSumFunMany() {
-        assertEquals(6, sumFun(1, 2, 3))
+        edges[0] = arrayListOf(3, 4)
+        edges[1] = arrayListOf(0, 4)
+        edges[2] = arrayListOf(0, 1)
+        edges[3] = arrayListOf(1, 2)
+        edges[4] = arrayListOf(2, 3)
+
+        checkAnswer(edges, solve(edges, n), n, m)
     }
 }
