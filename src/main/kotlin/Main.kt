690282ab0200818f3e6a9061a21be47e75990ba5
Glebwin <glebwin@gmail.com>
2017-12-03 22:08:46 +0300

    Add quick sort implementation. Add tests for quick sort.

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,15 +1,31 @@
-fun main(args: Array<String>) {
-    println("Hello world!")
-}
+import java.util.Random
 
-fun foo() = 10
+val random = Random()
 
-fun sum(vararg ints: Int): Int {
-    var result = 0
-    for (v in ints) {
-        result += v
+fun <T> partition(array: ArrayList<T>, leftBorder: Int, rightBorder: Int, predicate: (T) -> Boolean): Int {
+    var left = leftBorder
+    var right = rightBorder - 1
+    while (left < right) {
+        while (left < right && predicate(array[left]))
+            left++
+        while (left < right && !predicate(array[right]))
+            right--
+        if (left < right) {
+            val temp = array[left]
+            array[left] = array[right]
+            array[right] = temp
+        }
     }
-    return result
+    return left
 }
 
-fun sumFun(vararg ints: Int) = ints.fold(0) {acc, i -> acc + i }
+fun <T : Comparable<T>> quickSort(array: ArrayList<T>, leftBorder: Int = 0, rightBorder: Int = array.size) {
+    if (leftBorder + 1 >= rightBorder)
+        return
+    val pivotIndex = leftBorder + random.nextInt(rightBorder - leftBorder)
+    val pivot = array[pivotIndex]
+    val leftSeparator = partition(array, leftBorder, rightBorder, { x -> x < pivot })
+    val rightSeparator = partition(array, leftSeparator, rightBorder, { x -> x <= pivot })
+    quickSort(array, leftBorder, leftSeparator)
+    quickSort(array, rightSeparator, rightBorder)
+}
