3b70548265b2caec932c198a9e33b06e2f09626e
numberhanded <ilyuhaya@gmail.com>
2017-12-04 01:29:11 +0300

    Implemented the algorithm

--- src/test/kotlin/MainTest.kt
+++ src/test/kotlin/MainTest.kt
@@ -1,39 +1,77 @@
-import org.junit.Assert.assertEquals
+package test
+
+import main.getMaxMatching
 import org.junit.Test
+import java.lang.Integer.max
+import java.lang.Integer.min
+import java.util.*
+
+var random = Random()
+
+fun generateGraph(n: Int, m: Int, k: Int): List<Pair<Int, Int>> {
+    assert(k >= 0 && k <= n * m)
+    val set = mutableSetOf<Pair<Int, Int>>()
+
+    while (set.size != k) {
+        set.add(random.nextInt(n) to random.nextInt(m))
+    }
+
+    return set.toList()
+}
+
+fun checkMatching(matching: List<Pair<Int, Int>>): Boolean {
+    matching.forEach { e1 ->
+        matching.forEach { e2 ->
+            if (e1 != e2 && (e1.first == e2.first || e1.second == e2.second))
+                return false
+        }
+    }
+
+    return true
+}
+
+fun bruteMatching(edges: List<Pair<Int, Int>>): Int {
+    var ans = 0
+
+    for (mask in 0 until (1L shl edges.size)) {
+        val matching = arrayListOf<Pair<Int, Int>>()
+        edges.forEachIndexed { i, it -> if (((1L shl i) and mask) > 0) matching.add(it) }
+        if (checkMatching(matching))
+            ans = max(ans, matching.size)
+    }
+    println("Brute found $ans")
+
+    return ans
+}
 
 class MainTest {
-    @Test
-    fun testFoo() {
-        assertEquals(10, foo())
+
+    private fun testWithRandomGraphs(kMin: Double, kMax: Double, testsNum: Int, maxSetSize: Int = 10) {
+        (1 .. testsNum).forEach {
+            println("Test #$it")
+            val n = Random().nextInt(maxSetSize) + 1
+            val m = Random().nextInt(maxSetSize) + 1
+            val k = min(21, (n * m * (kMin + (kMax - kMin) * random.nextDouble())).toInt())
+            println("Params: n = $n, m = $m, k = $k")
+            val edges = generateGraph(n, m, k)
+//            edges.forEach { println("${it.first} ${it.second}") }
+            val matching = getMaxMatching(n, m, edges)
+            println("Main solution has found matching of size ${matching.size}")
+            assert(checkMatching(matching) && matching.size == bruteMatching(edges))
+            println("OK\n")
+        }
     }
 
     @Test
-    fun testSumEmpty() {
-        assertEquals(0, sum())
+    fun testWithRandomSparseGraphs() {
+        println("Testing with sparse graphs")
+        testWithRandomGraphs(0.0, 0.5, 100)
     }
 
     @Test
-    fun testSumSingle() {
-        assertEquals(42, sum(42))
+    fun testWithRandomDenseGraphs() {
+        println("Testing with dense graphs")
+        testWithRandomGraphs(0.5, 1.0, 100, 6)
     }
 
-    @Test
-    fun testSumMany() {
-        assertEquals(6, sum(1, 2, 3))
-    }
-
-    @Test
-    fun testSumFunEmpty() {
-        assertEquals(0, sumFun())
-    }
-
-    @Test
-    fun testSumFunSingle() {
-        assertEquals(42, sumFun(42))
-    }
-
-    @Test
-    fun testSumFunMany() {
-        assertEquals(6, sumFun(1, 2, 3))
-    }
 }
