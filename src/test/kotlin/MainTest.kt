e49b0491d0cd7b5abcd8130037778e9b40780f85
Drozdov Gleb <224989@niuitmo.ru>
2017-12-03 06:18:16 +0300

    init commit

--- src/test/kotlin/MainTest.kt
+++ src/test/kotlin/MainTest.kt
@@ -3,37 +3,129 @@ import org.junit.Test
 
 class MainTest {
     @Test
-    fun testFoo() {
-        assertEquals(10, foo())
+    fun testSimple() {
+        val g = Graph(4)
+        g.addEdge(0, 1, 4)
+        g.addEdge(1, 2, -7)
+        g.addEdge(2, 3, 2)
+        g.addEdge(1, 3, 5)
+        g.addEdge(3, 0, 6)
+        g.addEdge(0, 2, 3)
+        val dist = arrayListOf(0, 4, -3, -1)
+        val paths = arrayListOf(
+                arrayListOf(),
+                arrayListOf(0, 1),
+                arrayListOf(0, 1, 2),
+                arrayListOf(0, 1, 2, 3)
+        )
+        assertEquals(Pair(dist, paths), g.fordBellman(0))
     }
 
     @Test
-    fun testSumEmpty() {
-        assertEquals(0, sum())
+    fun testNegativeCycle() {
+        val g = Graph(6)
+        g.addEdge(0, 5, 14)
+        g.addEdge(5, 2, 2)
+        g.addEdge(5, 4, 9)
+        g.addEdge(0, 2, 9)
+        g.addEdge(4, 3, 6)
+        g.addEdge(2, 3, 11)
+        g.addEdge(3, 1, -15)
+        g.addEdge(2, 4, -10)
+        g.addEdge(1, 0, -7)
+        val paths = arrayListOf(
+                arrayListOf(0, 2, 4, 3, 1)
+        )
+        assertEquals(Pair(null, paths), g.fordBellman(0))
     }
 
     @Test
-    fun testSumSingle() {
-        assertEquals(42, sum(42))
+    fun testNot0Start() {
+        val g = Graph(6)
+        g.addEdge(0, 5, 14)
+        g.addEdge(5, 2, 2)
+        g.addEdge(5, 4, 9)
+        g.addEdge(0, 2, -9)
+        g.addEdge(4, 3, 6)
+        g.addEdge(2, 3, 11)
+        g.addEdge(3, 1, -4)
+        g.addEdge(2, 3, 11)
+        g.addEdge(1, 0, 7)
+        val dist = arrayListOf(14, 7, 0, 11, 37, 28)
+        val paths = arrayListOf(
+                arrayListOf(2, 3, 1, 0),
+                arrayListOf(2, 3, 1),
+                arrayListOf(),
+                arrayListOf(2, 3),
+                arrayListOf(2, 3, 1, 0, 5, 4),
+                arrayListOf(2, 3, 1, 0, 5)
+        )
+        assertEquals(Pair(dist, paths), g.fordBellman(2))
     }
 
     @Test
-    fun testSumMany() {
-        assertEquals(6, sum(1, 2, 3))
+    fun testUnusedVertex() {
+        val g = Graph(6)
+        g.addEdge(0, 1, -3)
+        g.addEdge(0, 0, 8)
+        g.addEdge(1, 2, 1)
+        g.addEdge(0, 2, 1)
+        g.addEdge(2, 3, -2)
+        g.addEdge(2, 0, 2)
+        g.addEdge(3, 5, 10)
+        val dist = arrayListOf(0, -3, -2, -4, Int.MAX_VALUE, 6)
+        val paths = arrayListOf(
+                arrayListOf(),
+                arrayListOf(0, 1),
+                arrayListOf(0, 1, 2),
+                arrayListOf(0, 1, 2, 3),
+                arrayListOf(Int.MAX_VALUE),
+                arrayListOf(0, 1, 2, 3, 5)
+        )
+        assertEquals(Pair(dist, paths), g.fordBellman(0))
     }
 
     @Test
-    fun testSumFunEmpty() {
-        assertEquals(0, sumFun())
+    fun test5() {
+        val g = Graph(5)
+        g.addEdge(0, 3, 2)
+        g.addEdge(0, 1, 7)
+        g.addEdge(4, 1, 9)
+        g.addEdge(1, 2, -2)
+        g.addEdge(2, 1, 5)
+        g.addEdge(4, 1, -3)
+        g.addEdge(2, 4, 8)
+        g.addEdge(3, 2, 6)
+        g.addEdge(3, 4, 7)
+        g.addEdge(2, 0, -4)
+        val dist = arrayListOf(-2, 4, 2, 0, 7)
+        val paths = arrayListOf(
+                arrayListOf(3, 4, 1, 2, 0),
+                arrayListOf(3, 4, 1),
+                arrayListOf(3, 4, 1, 2),
+                arrayListOf(),
+                arrayListOf(3, 4)
+        )
+        assertEquals(Pair(dist, paths), g.fordBellman(3))
     }
 
     @Test
-    fun testSumFunSingle() {
-        assertEquals(42, sumFun(42))
+    fun testAnotherNegativeCycle() {
+        val g = Graph(5)
+        g.addEdge(0, 3, 2)
+        g.addEdge(0, 1, 5)
+        g.addEdge(4, 1, 9)
+        g.addEdge(1, 2, -2)
+        g.addEdge(2, 1, 5)
+        g.addEdge(4, 1, -3)
+        g.addEdge(2, 4, 8)
+        g.addEdge(3, 2, 6)
+        g.addEdge(3, 4, 7)
+        g.addEdge(2, 0, -4)
+        val paths = arrayListOf(
+                arrayListOf(2, 0, 1)
+        )
+        assertEquals((Pair(null, paths)), g.fordBellman(3))
     }
 
-    @Test
-    fun testSumFunMany() {
-        assertEquals(6, sumFun(1, 2, 3))
-    }
 }
