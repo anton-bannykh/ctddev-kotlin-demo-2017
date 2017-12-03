d90d817a4262f910f166917f0d677e2ed4725790
AlexVolkow <alexander.volkow@gmail.com>
2017-12-03 19:49:47 +0300

    trie

---
+++ src/main/kotlin/Trie.kt
@@ -0,0 +1,87 @@
+/**
+ * Created by AlexandrVolkov on 03.12.2017.
+ */
+
+class Trie {
+    private val ROOT = Node(null, '$')
+
+    private inner class Node(private val parent: Node?, private val symb: Char) {
+        val next: MutableMap<Char, Node> = HashMap()
+        var pattern: String? = null
+        val autoMove: MutableMap<Char, Node> = HashMap()
+        var flag = false
+
+
+        val suffLink: Node by lazy {
+            if (this == ROOT || parent == null || parent == ROOT)
+                ROOT
+            else
+                getAutoMove(parent.suffLink, symb)
+        }
+
+        val suffShortLink: Node by lazy {
+            when {
+                suffLink == ROOT -> ROOT
+                suffLink.flag -> suffLink
+                else -> suffLink.suffShortLink
+            }
+        }
+    }
+
+    private fun <T> iterateInTrie(s: String, first: T, next: (Node, Char) -> T): T {
+        var currNode = ROOT
+        var acc = first
+        for (c in s) {
+            if (!currNode.next.containsKey(c)) {
+                acc = next(currNode, c)
+            }
+            if (!currNode.next.containsKey(c)) {
+                break
+            }
+            currNode = currNode.next[c]!!
+        }
+        return acc
+    }
+
+    fun add(s: String) {
+        val currNode = iterateInTrie(s, ROOT, { node, c ->
+            val vertex = Node(node, c)
+            node.next.put(c, vertex)
+            vertex
+        })
+        currNode.flag = true
+        currNode.pattern = s
+    }
+
+    fun contains(s: String): Boolean {
+        return iterateInTrie(s, true, { _, _ -> false })
+    }
+
+    private fun getAutoMove(v: Node, c: Char): Node {
+        if (!v.autoMove.containsKey(c)) {
+            when {
+                v.next.containsKey(c) -> v.autoMove.put(c, v.next[c]!!)
+                v == ROOT -> v.autoMove.put(c, ROOT)
+                else -> v.autoMove.put(c, getAutoMove(v.suffLink, c))
+            }
+        }
+        return v.autoMove[c]!!
+    }
+
+    fun findAll(s: String): List<Pair<Int, String>> {
+        var u = ROOT
+        val res: MutableList<Pair<Int, String>> = ArrayList()
+        for (i in 0 until s.length) {
+            u = getAutoMove(u, s[i])
+            var v = u
+            while (v != ROOT) {
+                if (v.flag) {
+                    res.add(Pair(i - v.pattern!!.length + 1, v.pattern!!))
+                }
+                v = v.suffShortLink
+            }
+        }
+        return res
+    }
+}
+
---
+++ src/test/kotlin/TrieTest.kt
@@ -0,0 +1,72 @@
+import org.junit.Assert.assertEquals
+import org.junit.Test
+import java.util.ArrayList
+
+class TrieTest {
+    @Test
+    fun simpleTest() {
+        val trie = Trie()
+        trie.add("abc")
+        trie.add("bcdc")
+        trie.add("cccb")
+        trie.add("bcdd")
+        trie.add("bbbc")
+        val entries = trie.findAll("abcdcbcddbbbcccbbbcccbb")
+        val ans = listOf(Pair(0, "abc"), Pair(1, "bcdc"), Pair(5, "bcdd"),
+                Pair(9, "bbbc"), Pair(12, "cccb"), Pair(15, "bbbc"), Pair(18, "cccb"))
+        assertEquals(entries, ans)
+    }
+
+    @Test
+    fun englishTest() {
+        val trie = Trie()
+        trie.add("buy")
+        trie.add("ticket")
+        val entries = trie.findAll("buy ticket for Harry Potter")
+        val ans = listOf(Pair(0, "buy"), Pair(4, "ticket"))
+        assertEquals(entries, ans)
+    }
+
+    @Test
+    fun containsTest() {
+        val trie = Trie()
+        trie.add("buy")
+        trie.add("ticket")
+        assert(trie.contains("buy"))
+        assert(!trie.contains("cats"))
+    }
+
+    @Test
+    fun russianTest() {
+        val trie = Trie()
+        trie.add("купить")
+        trie.add("билеты")
+        val entries = trie.findAll("купить билеты на Гарри Поттера")
+        val ans = listOf(Pair(0, "купить"), Pair(7, "билеты"))
+        assertEquals(entries, ans)
+    }
+
+    @Test
+    fun emptyTrieTest() {
+        val trie = Trie()
+        val entries = trie.findAll("aaababab")
+        assert(entries.isEmpty())
+    }
+
+    @Test
+    fun emptyStringTest() {
+        val trie = Trie()
+        trie.add(" ")
+        val entries = trie.findAll("a b")
+        assertEquals(entries, listOf(Pair(1, " ")))
+    }
+
+    @Test
+    fun hardTest() {
+        val trie = Trie()
+        trie.add("aa")
+        val entries = trie.findAll("aaaaa")
+        val ans = listOf(Pair(0, "aa"), Pair(1, "aa"), Pair(2, "aa"), Pair(3, "aa"))
+        assertEquals(entries, ans)
+    }
+}
