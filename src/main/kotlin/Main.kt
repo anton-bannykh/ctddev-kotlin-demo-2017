964e5ebeceec04e33b2aa3e67e3b69a0770a4871
numberhanded <ilyuhaya@gmail.com>
2017-12-04 01:29:11 +0300

    Implemented the algorithm

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,15 +1,24 @@
-fun main(args: Array<String>) {
-    println("Hello world!")
-}
+package main
 
-fun foo() = 10
+fun getMaxMatching(n: Int, m: Int, e: List<Pair<Int, Int>>): List<Pair<Int, Int>> {
+    val g = Array(n, { arrayListOf<Int>() })
 
-fun sum(vararg ints: Int): Int {
-    var result = 0
-    for (v in ints) {
-        result += v
+    e.forEach { (u, v) -> g[u].add(v) }
+
+    val pair = Array<Int?>(m, { null })
+    val used = Array(n, { false })
+
+    for (v in 0 until n) {
+        fun dfs(v: Int): Boolean {
+            if (used[v])
+                return false
+            used[v] = true
+            return g[v].any { to -> (pair[to]?.let { dfs(it) } != false).also { if (it) pair[to] = v } }
+        }
+
+        used.fill(false)
+        dfs(v)
     }
-    return result
-}
 
-fun sumFun(vararg ints: Int) = ints.fold(0) { acc, i -> acc + i }
+    return pair.filterNotNull().mapIndexed { index, v -> v to index }
+}
