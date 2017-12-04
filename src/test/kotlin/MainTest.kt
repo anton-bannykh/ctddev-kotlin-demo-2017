0694bf7bb025bd94c1d9835d6b9a727e19a79890
S0vk4 <34132895+S0vk4@users.noreply.github.com>
2017-12-04 16:42:51 +0300

    Update MainTest.kt

--- src/test/kotlin/MainTest.kt
+++ src/test/kotlin/MainTest.kt
@@ -1,39 +1,48 @@
-import org.junit.Assert.assertEquals
-import org.junit.Test
+import org.junit.jupiter.api.Assertions.assertArrayEquals
+import org.junit.jupiter.api.Test
 
 class MainTest {
     @Test
-    fun testFoo() {
-        assertEquals(10, foo())
+    fun test1() {
+        assertArrayEquals(arrayOf(1, -1, 1),
+                buildCentroidDec(arrayOf(mutableSetOf(1), mutableSetOf(0, 2), mutableSetOf(1))))
     }
 
     @Test
-    fun testSumEmpty() {
-        assertEquals(0, sum())
+    fun test2() {
+        assertArrayEquals(arrayOf(-1, 0, 1, 1, 0, 0, 5, 5, 7),
+                buildCentroidDec(arrayOf(mutableSetOf(1, 4, 5), mutableSetOf(2, 3, 0), mutableSetOf(1), mutableSetOf(1),
+                        mutableSetOf(0), mutableSetOf(0, 6, 7), mutableSetOf(5), mutableSetOf(5, 8), mutableSetOf(7))))
     }
 
     @Test
-    fun testSumSingle() {
-        assertEquals(42, sum(42))
+    fun test3() {
+        assertArrayEquals(arrayOf(-1, 0), buildCentroidDec(arrayOf(mutableSetOf(1), mutableSetOf(0))))
     }
 
     @Test
-    fun testSumMany() {
-        assertEquals(6, sum(1, 2, 3))
+    fun test4() {
+        assertArrayEquals(arrayOf(1, -1, 1, 2),
+                buildCentroidDec(arrayOf(mutableSetOf(1), mutableSetOf(0, 2), mutableSetOf(1, 3), mutableSetOf(2))))
     }
 
     @Test
-    fun testSumFunEmpty() {
-        assertEquals(0, sumFun())
+    fun test5() {
+        assertArrayEquals(arrayOf(-1, 0, 0, 0),
+                buildCentroidDec(arrayOf(mutableSetOf(1, 2, 3), mutableSetOf(0), mutableSetOf(0), mutableSetOf(0))))
     }
 
     @Test
-    fun testSumFunSingle() {
-        assertEquals(42, sumFun(42))
+    fun test6() {
+        assertArrayEquals(arrayOf(2, 0, -1, 2, 2),
+                buildCentroidDec(arrayOf(mutableSetOf(1, 2), mutableSetOf(0), mutableSetOf(0, 3, 4), mutableSetOf(2),
+                        mutableSetOf(2))))
     }
 
     @Test
-    fun testSumFunMany() {
-        assertEquals(6, sumFun(1, 2, 3))
+    fun test7() {
+        assertArrayEquals(arrayOf(1, -1, 1, 2, 1),
+                buildCentroidDec(arrayOf(mutableSetOf(1), mutableSetOf(0, 2, 4), mutableSetOf(1, 3), mutableSetOf(2),
+                        mutableSetOf(1))))
     }
 }
