333ea26b13f8da97b7ef058d385deecdd1bf23ac
Evgenii Glukhov <gluhov1998@yandex.ru>
2017-12-04 19:35:59 +0300

    Update test

--- src/test/kotlin/MainTest.kt
+++ src/test/kotlin/MainTest.kt
@@ -5,6 +5,13 @@ class MainTest {
     @Test
     fun testBinTree() {
         create_tree(7, arrayOf(1, 1, 2, 2, 3, 3))
+        /*
+        *      1
+        *     / \
+        *    2   3
+        *   / \ / \
+        *   4 5 6 7
+         */
         assertEquals(1, LCA(1, 2))
         assertEquals(1, LCA(1, 3))
         assertEquals(1, LCA(2, 3))
@@ -15,6 +22,17 @@ class MainTest {
     @Test
     fun testBambooTree() {
         create_tree(5, arrayOf(1, 2, 3, 4))
+        /*
+        *  1
+        *  |
+        *  2
+        *  |
+        *  3
+        *  |
+        *  4
+        *  |
+        *  5
+         */
         assertEquals(1, LCA(1, 2))
         assertEquals(2, LCA(2, 4))
         assertEquals(5, LCA(5, 5))
@@ -22,6 +40,11 @@ class MainTest {
     @Test
     fun testSunTree() {
         create_tree(5, arrayOf(1, 1, 1, 1))
+        /*
+        *       1
+        *   |  |  |  |
+        *   2  3  4  5
+         */
         assertEquals(1, LCA(2, 3))
         assertEquals(1, LCA(3, 4))
         assertEquals(1, LCA(1, 3))
