81da6bd3ee3c1286f5b49702d80fc10de47ec530
S0vk4 <34132895+S0vk4@users.noreply.github.com>
2017-12-04 16:42:11 +0300

    Update Main.kt

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,15 +1,54 @@
-fun main(args: Array<String>) {
-    println("Hello world!")
+var edges: Array<MutableSet<Int>> = emptyArray()
+var sizeV: Array<Int> = emptyArray()
+var prevCentroid: Array<Int> = emptyArray()
+
+fun buildCentroidDec(edgesIn: Array<MutableSet<Int>>): Array<Int> {
+    edges = edgesIn
+    sizeV = Array(edges.size, { 0 })
+    prevCentroid = Array(edges.size, { 0 })
+    build(0, -1)
+    return prevCentroid
 }
 
-fun foo() = 10
-
-fun sum(vararg ints: Int): Int {
-    var result = 0
-    for (v in ints) {
-        result += v
+fun dfs(p: Int, v: Int): Int {
+    var res: Int = 1
+    for (i in edges[v]) {
+        if (i != p) {
+            res += dfs(v, i)
+        }
     }
-    return result
+    sizeV[v] = res
+    return res
 }
 
-fun sumFun(vararg ints: Int) = ints.fold(0) {acc, i -> acc + i }
+fun findCentroid(v: Int): Int {
+    dfs(v, v)
+    val size: Int = sizeV[v]
+    var end: Boolean = false
+    var prev: Int = v
+    var centroid: Int = v
+    while (!end)
+    {
+        end = true
+        for (i in edges[v]) {
+            if (i != prev && sizeV[i] > size / 2) {
+                end = false
+                prev = centroid
+                centroid = i
+                break
+            }
+        }
+    }
+    return centroid
+}
+
+fun build(v: Int, prev: Int) {
+    val cur = findCentroid(v)
+    prevCentroid[cur] = prev
+    val nextStep: Set<Int> = edges[cur]
+    edges[cur] = mutableSetOf()
+    for (i in nextStep) {
+        edges[i].remove(cur)
+        build(i, cur)
+    }
+}
