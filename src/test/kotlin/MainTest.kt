5a39a73e44c452e6eed368aecbe1b24bbf7bc25c
S0vk4 <34132895+S0vk4@users.noreply.github.com>
2017-12-04 17:12:13 +0300

    Update MainTest.kt

--- src/test/kotlin/MainTest.kt
+++ src/test/kotlin/MainTest.kt
@@ -1,5 +1,5 @@
-import org.junit.jupiter.api.Assertions.assertArrayEquals
-import org.junit.jupiter.api.Test
+import org.junit.Assert.assertArrayEquals
+import org.junit.Test
 
 class MainTest {
     @Test
