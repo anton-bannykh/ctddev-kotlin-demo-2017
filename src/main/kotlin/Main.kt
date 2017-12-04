7ba0151be2be92c3f13e0319cf5ea9edf6ad8e68
Anver Khusainov <hanver12@gmail.com>
2017-12-04 20:55:48 +0300

    first commit
    algorithm and tests released

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,15 +1,51 @@
-fun main(args: Array<String>) {
-    println("Hello world!")
-}
-
-fun foo() = 10
-
-fun sum(vararg ints: Int): Int {
-    var result = 0
-    for (v in ints) {
-        result += v
+fun dfs1(v: Int, used: BooleanArray, graph: Array<ArrayList<Int>>, order: ArrayList<Int>) {
+    used[v] = true
+    for (u in graph[v]) {
+        if (!used[u]) {
+            dfs1(u, used, graph, order)
+        }
     }
-    return result
+    order += v
 }
 
-fun sumFun(vararg ints: Int) = ints.fold(0) {acc, i -> acc + i }
+fun dfs2(v: Int, used: BooleanArray, reverseGraph: Array<ArrayList<Int>>, comp: ArrayList<Int>) {
+    used[v] = true
+    comp += v
+    for (u in reverseGraph[v]) {
+        if (!used[u]) {
+            dfs2(u, used, reverseGraph, comp)
+        }
+    }
+}
+
+fun buildReverseGraph(graph: Array<ArrayList<Int>>): Array<ArrayList<Int>> {
+    val reverseGraph = Array(graph.size, { ArrayList<Int>() })
+    for (i in graph.indices) {
+        for (u in graph[i]) {
+            reverseGraph[u].add(i)
+        }
+    }
+    return reverseGraph
+}
+
+fun condensate(graph: Array<ArrayList<Int>>): ArrayList<ArrayList<Int>> {
+    val reverseGraph = buildReverseGraph(graph)
+    val used = BooleanArray(graph.size)
+    val order = ArrayList<Int>()
+    for (i in graph.indices) {
+        if (!used[i]) {
+            dfs1(i, used, graph, order)
+        }
+    }
+    used.fill(false)
+    val ans = ArrayList<ArrayList<Int>>()
+    for (i in graph.indices) {
+        val v = order[graph.size - 1 - i]
+        if (!used[v]) {
+            val comp = ArrayList<Int>()
+            dfs2(v, used, reverseGraph, comp)
+            ans += comp
+        }
+    }
+    return ans
+}
