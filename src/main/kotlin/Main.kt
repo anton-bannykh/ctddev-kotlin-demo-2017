fe3c467291fae0e472dff681cd783f83cf0197d1
yfxrby <nold98@bk.ru>
2017-12-05 21:39:11 +0300

    algorithm is done

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,15 +1,26 @@
 fun main(args: Array<String>) {
-    println("Hello world!")
+
 }
 
-fun foo() = 10
-
-fun sum(vararg ints: Int): Int {
-    var result = 0
-    for (v in ints) {
-        result += v
+fun solve(n: Int, a: Array<Int>): Int {
+    var d = Array(n + 1, { _ -> Int.MAX_VALUE })
+    d[0] = Int.MIN_VALUE
+    for (i in 0..n - 1) {
+        var j = d.binarySearch(a[i])
+        if (j < 0) {
+            j = -j - 1
+        }
+        if (d[j - 1] < a[i] && a[i] < d[j]) {
+            d[j] = a[i]
+        }
     }
-    return result
-}
 
-fun sumFun(vararg ints: Int) = ints.fold(0) { acc, i -> acc + i }
+    var ans = n
+    for (i in 0..n) {
+        if (d[i] == Int.MAX_VALUE) {
+            ans = i - 1
+            break
+        }
+    }
+    return ans
+}
