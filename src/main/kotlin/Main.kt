6db0cbbff93d6d68995b80f362e846593e50d221
Larionov Nikita <iksiygrik@gmail.com>
2017-12-05 18:15:01 +0300

    hw1

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,15 +1,46 @@
-fun main(args: Array<String>) {
-    println("Hello world!")
+import java.util.Random
+
+fun ClosedRange<Int>.random() = Random().nextInt(endInclusive - start) +  start
+
+fun Array<Int>.swap(index1: Int, index2: Int) {
+    val tmp = this[index1]
+    this[index1] = this[index2]
+    this[index2] = tmp
 }
 
-fun foo() = 10
-
-fun sum(vararg ints: Int): Int {
-    var result = 0
-    for (v in ints) {
-        result += v
+fun partition(arr: Array<Int>, left: Int, right: Int) : Int{
+    if (left != right) {
+        arr.swap(left + (0..right-left).random(), right)
     }
-    return result
+    var lastElementValue = arr[right];
+    var i = left - 1;
+    for (j in left..right){
+        if (arr[j] <= lastElementValue){
+            i++;
+            arr.swap(i, j);
+        }
+    }
+    return i;
 }
 
-fun sumFun(vararg ints: Int) = ints.fold(0) { acc, i -> acc + i }
+fun nth_element(arr: Array<Int>, ind : Int) : Int {
+    var array = arr
+    var left = 0
+    var right = array.size - 1
+    var index = ind - 1
+    while (true) {
+        var mid = partition(array, left, right)
+        if (mid == index) {
+            return array[mid]
+        } else if (index < mid) {
+            right = mid - 1
+        } else {
+            left = mid + 1
+        }
+    }
+}
+
+fun main(args: Array<String>) {
+    val input = arrayOf(-50, 50, -2500)
+    println(nth_element(input, 1))
+}
