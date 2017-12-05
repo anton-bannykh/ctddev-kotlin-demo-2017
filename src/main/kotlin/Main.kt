14c2e23ba665b235215b2030d675074d5da94ccb
DrRudi <dr.rudi2@gmail.com>
2017-12-05 23:07:06 +0300

    DSU

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,15 +1,38 @@
-fun main(args: Array<String>) {
-    println("Hello world!")
-}
+class myDSU {
+    private var parent: Array<Int>? = null
+    private var size: Array<Int>? = null
+    private var countSets: Int = 0
 
-fun foo() = 10
-
-fun sum(vararg ints: Int): Int {
-    var result = 0
-    for (v in ints) {
-        result += v
+    fun init(sizeX: Int) {
+        parent = Array(sizeX + 1, { it })
+        size = Array(sizeX + 1, { 1 })
+        countSets = sizeX
     }
-    return result
+
+    fun size(x: Int): Int {
+        return size!![find(x)]
+    }
+
+    fun countSets(): Int {
+        return countSets
+    }
+
+    fun union(x: Int, y: Int) {
+        var x = find(x)
+        var y = find(y)
+        if (x == y) return
+        if (size!![x] > size!![y]) x = y.also { y = x }
+        parent!![x] = y
+        size!![y] += size!![x]
+        countSets -= 1
+    }
+
+    fun find(x: Int): Int {
+        if (parent!![x] == x)
+            parent!![x] = x
+        else
+            parent!![x] = find(parent!![x])
+        return parent!![x]
+    }
 }
 
-fun sumFun(vararg ints: Int) = ints.fold(0) { acc, i -> acc + i }
