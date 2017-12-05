170fde7e12e34a47e251ab7517993c3803e751e8
Golovin Pavel <gopavel0@gmail.com>
2017-12-06 01:16:39 +0300

    Change src to my code

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,15 +1,55 @@
-fun main(args: Array<String>) {
-    println("Hello world!")
+import java.util.ArrayList
+
+/*
+* Implementation disjoint set union
+* */
+
+// global data for DSU
+var parent = ArrayList<Int>()
+var rank = ArrayList<Int>()
+var countElement = 0
+
+fun makeSet(newElement: Int) {
+    rank.add(0)
+    parent.add(parent.size)
+    countElement += 1
 }
 
-fun foo() = 10
-
-fun sum(vararg ints: Int): Int {
-    var result = 0
-    for (v in ints) {
-        result += v
+fun findLead(v: Int): Int {
+    if (parent[v] == v) {
+        return v
     }
-    return result
+    parent[v] = findLead(parent[v])
+    return parent[v]
 }
 
-fun sumFun(vararg ints: Int) = ints.fold(0) { acc, i -> acc + i }
+fun unionSet(x: Int, y: Int) {
+    var leadX = findLead(x)
+    var leadY = findLead(y)
+    if (leadX != leadY) {
+        if (rank[leadX] < rank[leadY]) {
+            leadX = leadY.also { leadY = leadX }
+        }
+        if (rank[leadX] == rank[leadY])
+            ++rank[leadX]
+        parent[leadY] = leadX
+    }
+}
+
+fun printCondition() {
+    for (it in 0..countElement - 1) {
+        print(parent[it])
+        print(" ")
+    }
+    println()
+}
+
+fun clear() {
+    parent.clear()
+    rank.clear()
+    countElement = 0
+}
+
+fun main(args: Array<String>) {
+
+}
