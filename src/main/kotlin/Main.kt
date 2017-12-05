f63155fe14d79c26ee5f1a42cc964ccc9f6ff581
RuslanKhalimov <khalimov.ruslan@mail.ru>
2017-12-05 23:16:07 +0300

    HW1 added

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,15 +1,75 @@
-fun main(args: Array<String>) {
-    println("Hello world!")
-}
+import java.util.Stack
+import kotlin.collections.ArrayList
 
-fun foo() = 10
+fun check(edges: Array<ArrayList<Int>>, n: Int): Boolean {
+    val used = Array(n, { false })
 
-fun sum(vararg ints: Int): Int {
-    var result = 0
-    for (v in ints) {
-        result += v
+    fun dfs(v: Int) {
+        used[v] = true
+        for (u in edges[v]) {
+            if (!used[u]) {
+                dfs(u)
+            }
+        }
     }
-    return result
+
+    for (v in edges.indices) {
+        if (edges[v].size > 0) {
+            dfs(v)
+            break
+        }
+    }
+
+    for (v in edges.indices) {
+        if (!used[v] && edges[v].size > 0) {
+            return false
+        }
+    }
+
+    val inDeg = Array(n, { 0 })
+    val outDeg = Array(n, { 0 })
+
+    for (v in edges.indices) {
+        outDeg[v] = edges[v].size
+        for (u in edges[v]) {
+            inDeg[u]++
+        }
+    }
+
+    for (v in inDeg.indices) {
+        if (outDeg[v] != inDeg[v]) {
+            return false
+        }
+    }
+
+    return true
 }
 
-fun sumFun(vararg ints: Int) = ints.fold(0) { acc, i -> acc + i }
+fun findCycle(edges: Array<ArrayList<Int>>, n: Int): ArrayList<Int> {
+    val answer = ArrayList<Int>()
+    val path = Stack<Int>()
+    val indices = Array(n, { 0 })
+
+    for (v in 0..n - 1) {
+        if (edges[v].size > 0) {
+            path.push(v)
+            break
+        }
+    }
+
+    while (path.isNotEmpty()) {
+        val v = path.peek()
+        if (edges[v].size > indices[v]) {
+            path.push(edges[v][indices[v]++])
+        } else {
+            answer.add(v)
+            path.pop()
+        }
+    }
+
+    answer.reverse()
+
+    return answer
+}
+
+fun solve(edges: Array<ArrayList<Int>>, n: Int) = if (check(edges, n)) findCycle(edges, n) else null
--- src/test/kotlin/MainTest.kt
+++ src/test/kotlin/MainTest.kt
@@ -1,39 +1,98 @@
 import org.junit.Assert.assertEquals
+import org.junit.Assert.assertTrue
+import org.junit.Assert.assertNull
+import org.junit.Assert.fail
 import org.junit.Test
 
 class MainTest {
-    @Test
-    fun testFoo() {
-        assertEquals(10, foo())
+    fun checkAnswer(edges: Array<ArrayList<Int>>, answer: ArrayList<Int>?, m: Int) {
+        if (answer == null) {
+            fail()
+            return
+        }
+
+        assertEquals(answer[0], answer[answer.size - 1])
+        assertEquals(answer.size, m + 1)
+
+        for (i in 1..answer.size - 1) {
+            assertTrue(edges[answer[i - 1]].contains(answer[i]))
+            edges[answer[i - 1]].remove(answer[i])
+        }
     }
 
     @Test
-    fun testSumEmpty() {
-        assertEquals(0, sum())
+    fun test1() {
+        val n = 3
+        val m = 3
+        val edges = arrayOf(
+                arrayListOf(1),
+                arrayListOf(2),
+                arrayListOf(0)
+        )
+
+        checkAnswer(edges, solve(edges, n), m)
     }
 
     @Test
-    fun testSumSingle() {
-        assertEquals(42, sum(42))
+    fun test2() {
+        val n = 6
+        val m = 11
+        val edges = arrayOf(
+                arrayListOf(1),
+                arrayListOf(2, 3),
+                arrayListOf(4, 5),
+                arrayListOf(2, 5),
+                arrayListOf(0, 3),
+                arrayListOf(1, 4)
+        )
+
+        checkAnswer(edges, solve(edges, n), m)
     }
 
     @Test
-    fun testSumMany() {
-        assertEquals(6, sum(1, 2, 3))
+    fun test3() {
+        val n = 2
+        val edges = arrayOf(
+                arrayListOf(1),
+                arrayListOf()
+        )
+
+        assertNull(solve(edges, n))
     }
 
     @Test
-    fun testSumFunEmpty() {
-        assertEquals(0, sumFun())
+    fun test4() {
+        val n = 6
+        val edges = arrayOf(
+                arrayListOf(1),
+                arrayListOf(2),
+                arrayListOf(0),
+                arrayListOf(4),
+                arrayListOf(5),
+                arrayListOf(3)
+        )
+
+        assertNull(solve(edges, n))
     }
 
     @Test
-    fun testSumFunSingle() {
-        assertEquals(42, sumFun(42))
-    }
+    fun test5() {
+        val n = 10
+        val m = 10
 
-    @Test
-    fun testSumFunMany() {
-        assertEquals(6, sumFun(1, 2, 3))
+        val edges = arrayOf(
+                arrayListOf(),
+                arrayListOf(4, 5),
+                arrayListOf(1, 5),
+                arrayListOf(1, 2),
+                arrayListOf(2, 3),
+                arrayListOf(3, 4),
+                arrayListOf(),
+                arrayListOf(),
+                arrayListOf(),
+                arrayListOf()
+        )
+
+        checkAnswer(edges, solve(edges, n), m)
     }
 }
