7613229c2d8d1f4a5af10c14c7dce1b8b9d4517d
Ilya <ilja.sahipoff@yandex.ru>
2017-12-05 19:47:00 +0300

    gjhg

--- lib/src/main/kotlin/my/lib/Main.kt
+++ lib/src/main/kotlin/my/lib/Main.kt
@@ -1,17 +1,48 @@
 package my.lib
 
-fun main(args: Array<String>) {
-    println("Hello world!")
-}
+fun sort_(arr: IntArray) {
+    var N: Int = arr.size
+    var k: Int = N / 2
 
-fun foo() = 10
-
-fun sum(vararg ints: Int): Int {
-    var result = 0
-    for (v in ints) {
-        result += v
+    while (k > 0) {
+        builtHeap(arr, k, N)
+        k--
     }
-    return result
+    do run {
+        var T = arr[0]
+        arr[0] = arr[N - 1]
+        arr[N - 1] = T
+        N--
+        builtHeap(arr, 1, N)
+    } while(N > 1)
 }
 
-fun sumFun(vararg ints: Int) = ints.fold(0) { acc, i -> acc + i }
+fun builtHeap(arr: IntArray, k: Int,  N: Int){
+    var k = k
+    var T = arr[k - 1]
+    while(k <= N / 2){
+        var j = 2 * k
+        if ((j < N) && (arr[j - 1] < arr[j])) j++
+        if (T >= arr[j - 1]) {
+            break
+        } else {
+            arr[k - 1] = arr[j - 1]
+            k = j
+        }
+    }
+    arr[k - 1] = T
+}
+
+fun printArray(ms: IntArray) {
+    for(it in ms) {
+        print("$it ")
+    }
+    println()
+}
+fun main(args: Array<String>) {
+    val ms = intArrayOf(1, 4, 2, 5, 6, 4, -4, -43, 313, 242, 32, 42, 2, 4, 42)
+    sort_(ms)
+    printArray(ms)
+}
+
+
