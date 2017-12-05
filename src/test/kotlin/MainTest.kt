3e33d418017ce201e11f59f92e4dc0e3d22cc5f1
AlexandraSannikova <Sannikovaal@mail.ru>
2017-12-05 21:22:18 +0300

    Centroid Decomposition, Alexandra Sannikova, M3236

--- src/test/kotlin/MainTest.kt
+++ src/test/kotlin/MainTest.kt
@@ -3,37 +3,34 @@ import org.junit.Test
 
 class MainTest {
     @Test
-    fun testFoo() {
-        assertEquals(10, foo())
+    fun testBase() {
+        val graph = Array(4){ arrayListOf<Int>()}
+        val used = Array(4, { false })
+        val parent = Array(4,  { 0 })
+        val size = Array(4, { 0 })
+            graph[1] = arrayListOf(2)
+            graph[2] = arrayListOf(1, 3)
+            graph[3].add(2)
+        build(1, 0, size, graph, used, parent)
+        assertEquals(arrayOf(2, 0, 2), ans(3, parent))
     }
 
-    @Test
-    fun testSumEmpty() {
-        assertEquals(0, sum())
-    }
-
-    @Test
-    fun testSumSingle() {
-        assertEquals(42, sum(42))
-    }
-
-    @Test
-    fun testSumMany() {
-        assertEquals(6, sum(1, 2, 3))
-    }
-
-    @Test
-    fun testSumFunEmpty() {
-        assertEquals(0, sumFun())
-    }
-
-    @Test
-    fun testSumFunSingle() {
-        assertEquals(42, sumFun(42))
-    }
-
-    @Test
-    fun testSumFunMany() {
-        assertEquals(6, sumFun(1, 2, 3))
+   @Test
+    fun testSecond() {
+       val graph = Array(10){ arrayListOf<Int>()}
+       val used = Array(10, { false })
+       val parent = Array(10,  { 0 })
+       val size = Array(10, { 0 })
+       graph[1] = arrayListOf(2, 5, 6)
+       graph[2] = arrayListOf(3, 4, 1)
+       graph[3] = arrayListOf(2)
+       graph[4] = arrayListOf(2)
+       graph[5] = arrayListOf(1)
+       graph[6] = arrayListOf(1, 7, 8)
+       graph[7] = arrayListOf(6)
+       graph[8] = arrayListOf(6, 9)
+       graph[9] = arrayListOf(8)
+       build(1, 0, size, graph, used, parent)
+       assertEquals(arrayOf(0, 1, 2, 2, 1, 1, 6, 6, 8), ans(9, parent))
     }
 }
