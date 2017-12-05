ae7210cb3cbc2e7aea225921f6d4aa4cf12b7503
DrRudi <dr.rudi2@gmail.com>
2017-12-05 23:07:06 +0300

    DSU

--- lib/src/test/kotlin/my/lib/MainTest.kt
+++ lib/src/test/kotlin/my/lib/MainTest.kt
@@ -1,41 +1,78 @@
 package my.lib
 
 import org.junit.Assert.assertEquals
+import org.junit.Assert.assertNotEquals
 import org.junit.Test
 
 class MainTest {
     @Test
-    fun testFoo() {
-        assertEquals(10, foo())
+    fun testFind() {
+        val a: myDSU = myDSU()
+        a.init(5)
+        a.union(3, 4)
+        assertEquals(a.find(3), a.find(4))
     }
 
     @Test
-    fun testSumEmpty() {
-        assertEquals(0, sum())
+    fun testFindAndNum() {
+        val a: myDSU = myDSU()
+        a.init(5)
+        a.union(3, 4)
+        assertEquals(4, a.find(3))
     }
 
     @Test
-    fun testSumSingle() {
-        assertEquals(42, sum(42))
+    fun testFindNotEquals() {
+        val a: myDSU = myDSU()
+        a.init(5)
+        a.union(3, 4)
+        assertNotEquals(a.find(1), a.find(3))
     }
 
     @Test
-    fun testSumMany() {
-        assertEquals(6, sum(1, 2, 3))
+    fun testFindInOneBigSet() {
+        val a: myDSU = myDSU()
+        a.init(10)
+        for (i in (1..9))
+            a.union(i, i + 1)
+        assertEquals(a.find(1), a.find(10))
     }
 
     @Test
-    fun testSumFunEmpty() {
-        assertEquals(0, sumFun())
+    fun testCountSets() {
+        val a: myDSU = myDSU()
+        a.init(10)
+        assertEquals(a.countSets(), 10)
     }
 
     @Test
-    fun testSumFunSingle() {
-        assertEquals(42, sumFun(42))
+    fun testFindAgain() {
+        val a: myDSU = myDSU()
+        a.init(5)
+        a.union(3, 4)
+        a.union(1, 2)
+        a.union(2, 3)
+        assertEquals(a.find(2), a.find(3))
     }
 
     @Test
-    fun testSumFunMany() {
-        assertEquals(6, sumFun(1, 2, 3))
+    fun testCountSetsInSmallGroup() {
+        val a: myDSU = myDSU()
+        a.init(5)
+        a.union(3, 4)
+        a.union(1, 2)
+        a.union(2, 3)
+        assertEquals(a.countSets(), 2)
     }
+
+    @Test
+    fun testSetSize() {
+        val a: myDSU = myDSU()
+        a.init(5)
+        a.union(3, 4)
+        a.union(1, 2)
+        a.union(2, 3)
+        assertEquals(a.size(1), 4)
+    }
+
 }
