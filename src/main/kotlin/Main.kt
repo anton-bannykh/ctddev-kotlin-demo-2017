6d02b335b6b3010b617ff14c446bf948dc2f262b
Shostina <shostina77@gmail.com>
2017-12-05 20:36:34 +0300

    inital commit 	modified:   src/main/kotlin/Main.kt 	modified:   src/test/kotlin/MainTest.kt

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,15 +1,47 @@
+/**
+ * Created by гыук on 03.12.2017.
+ */
 fun main(args: Array<String>) {
-    println("Hello world!")
+    println("Hi!")
 }
 
-fun foo() = 10
-
-fun sum(vararg ints: Int): Int {
-    var result = 0
-    for (v in ints) {
-        result += v
+fun nextPerm(vararg ints: Int):MutableList<Int> {
+    var args:MutableList<Int> = mutableListOf()
+    for (i in ints) {
+        args.add(i)
     }
-    return result
+    var i = args.size - 2
+    val n:Int = args.size
+    var res:MutableList<Int> = mutableListOf()
+    if (i > 0) {
+        while ((i >= 0) and (args[i] > args[i + 1])) {
+            i -= 1
+            if (i < 0) {
+                break
+            }
+        }
+    }
+    if (i < 0){
+        for(j in 0 .. n - 1) {
+            res.add(0)
+        }
+        return res
+    } else {
+        var j = args.size - 1
+        while(args[j] <= args[i]){
+            j -= 1
+        }
+        for(j in 0 .. n - 1) {
+            res.add(args[j])
+        }
+        var x = res[i]
+        res[i] = res[j]
+        res[j] = x
+        for(j in 0 .. ((n - i - 1) / 2) - 1) {
+            x = res[i + 1 + j]
+            res[i + 1 + j] = res[n - 1 - j]
+            res[n - 1 - j] = x
+        }
+    }
+    return res
 }
-
-fun sumFun(vararg ints: Int) = ints.fold(0) {acc, i -> acc + i }
