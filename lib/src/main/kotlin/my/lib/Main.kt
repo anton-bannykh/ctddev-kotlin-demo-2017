6848d9d3aebd35e27bb1f26246e047fac477b730
Alexandr Kravchenko <ratatuy.1995@mail.ru>
2017-12-05 21:33:18 +0300

    io347

--- lib/src/main/kotlin/my/lib/Main.kt
+++ lib/src/main/kotlin/my/lib/Main.kt
@@ -14,13 +14,13 @@ fun sort_(arr: IntArray) {
         arr[N - 1] = T
         N--
         builtHeap(arr, 1, N)
-    } while(N > 1)
+    } while (N > 1)
 }
 
-fun builtHeap(arr: IntArray, k: Int,  N: Int){
+fun builtHeap(arr: IntArray, k: Int, N: Int) {
     var k = k
     var T = arr[k - 1]
-    while(k <= N / 2){
+    while (k <= N / 2) {
         var j = 2 * k
         if ((j < N) && (arr[j - 1] < arr[j])) j++
         if (T >= arr[j - 1]) {
@@ -34,15 +34,14 @@ fun builtHeap(arr: IntArray, k: Int,  N: Int){
 }
 
 fun printArray(ms: IntArray) {
-    for(it in ms) {
+    for (it in ms) {
         print("$it ")
     }
     println()
 }
+
 fun main(args: Array<String>) {
     val ms = intArrayOf(1, 4, 2, 5, 6, 4, -4, -43, 313, 242, 32, 42, 2, 4, 42)
     sort_(ms)
     printArray(ms)
 }
-
-
