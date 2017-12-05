5e1ba35ab290328ceef59a3767a65cbc7c1909f8
Shokk357 <pavcygan17@gmail.com>
2017-12-05 20:27:17 +0300

    update project

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,15 +1,31 @@
-fun main(args: Array<String>) {
-    println("Hello world!")
-}
+var n: Int = 0
+var array = Array<Int>(1, { 0 })
 
-fun foo() = 10
-
-fun sum(vararg ints: Int): Int {
-    var result = 0
-    for (v in ints) {
-        result += v
+fun quickSort(left: Int, right: Int) {
+    var l = left
+    var r = right
+    var save: Int
+    var midel: Int = array[(l + r) / 2]
+    while (l <= r) {
+        while (array[l] < midel)
+            l++
+        while (array[r] > midel)
+            r--
+        if (l <= r) {
+            save = array[l]
+            array[l] = array[r]
+            array[r] = save
+            l++
+            r--
+        }
     }
-    return result
+    if (left < r)
+        quickSort(left, r)
+    if (l < right)
+        quickSort(l, right)
 }
 
-fun sumFun(vararg ints: Int) = ints.fold(0) { acc, i -> acc + i }
+fun create(count: Int, arr: Array<Int>) {
+    n = count
+    array = arr
+}
--- src/test/kotlin/MainTest.kt
+++ src/test/kotlin/MainTest.kt
@@ -3,37 +3,21 @@ import org.junit.Test
 
 class MainTest {
     @Test
-    fun testFoo() {
-        assertEquals(10, foo())
+    fun test1() {
+        create(10, arrayOf(7, 4, 3, 8, 0, 1, 9, 3, 7, 2))
+        quickSort(0, 9)
+        assertEquals(arrayOf(0, 1, 2, 3, 3, 4, 7, 7, 8, 9), array)
     }
-
     @Test
-    fun testSumEmpty() {
-        assertEquals(0, sum())
+    fun test2() {
+        create(15, arrayOf(15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1))
+        quickSort(0, 14)
+        assertEquals(arrayOf(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15), array)
     }
-
     @Test
-    fun testSumSingle() {
-        assertEquals(42, sum(42))
-    }
-
-    @Test
-    fun testSumMany() {
-        assertEquals(6, sum(1, 2, 3))
-    }
-
-    @Test
-    fun testSumFunEmpty() {
-        assertEquals(0, sumFun())
-    }
-
-    @Test
-    fun testSumFunSingle() {
-        assertEquals(42, sumFun(42))
-    }
-
-    @Test
-    fun testSumFunMany() {
-        assertEquals(6, sumFun(1, 2, 3))
+    fun test3() {
+        create(25, arrayOf(17, 52, 78, 1, 49, 35, 18, 7, 25, 17, 85, 8, 25, 0, 51, 89, 50, 84, 52, 19, 31, 43, 84, 481, 215))
+        quickSort(0, 24)
+        assertEquals(arrayOf(0, 1, 7, 8, 17, 17, 18, 19, 25, 25, 31, 35, 43, 49, 50, 51, 52, 52, 78, 84, 84, 85, 89, 215, 481), array)
     }
 }
