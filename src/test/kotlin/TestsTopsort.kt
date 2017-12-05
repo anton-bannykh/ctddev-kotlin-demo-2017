8dcaf371027a7313c32f44bb2f0564e4a99e2ba0
airosso <ros.arina1@gmail.com>
2017-12-05 21:28:47 +0300

    Topsort with three tests

---
+++ src/test/kotlin/TestsTopsort.kt
@@ -0,0 +1,51 @@
+/**
+ * Created by User on 05.12.2017.
+ */
+import org.junit.Assert
+import org.junit.Assert.assertEquals
+import org.junit.Test
+import java.util.*
+
+class MainTest2 {
+    var n = 0
+    @Test
+    fun testCiclic() {
+        n = 5;
+        val (color, graph, topol) = triple()
+        for(i in 0..n-1){
+            graph[i].add((i+1)%n)
+        }
+        assertEquals(false, topsort(color,graph,topol))
+    }
+
+    @Test
+    fun testSimple(){
+        n = 4
+        val (color, graph, topol) = triple()
+        graph[0].add(3)
+        graph[3].add(1)
+        graph[3].add(2)
+        graph[2].add(1)
+        val answer = arrayOf(1,4,3,2)
+        topsort(color,graph, topol)
+        Assert.assertArrayEquals(answer, topol)
+    }
+
+    @Test
+    fun testChain(){
+        n = 4
+        val (color, graph, topol) = triple()
+        for(i in 0..n-2)
+            graph[i].add(i+1)
+        val answer = arrayOf(1,2,3,4)
+        topsort(color,graph, topol)
+        Assert.assertArrayEquals(answer,topol)
+    }
+
+    private fun triple(): Triple<Array<Int>, Array<ArrayList<Int>>, Array<Int>> {
+        val color = Array(n, { i -> 0 })
+        val graph = Array<ArrayList<Int>>(n, { i -> ArrayList() })
+        val topol = Array<Int>(n, { 0 })
+        return Triple(color, graph, topol)
+    }
+}
