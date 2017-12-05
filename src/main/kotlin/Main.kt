e0ab7395234bb5858d3eca9cb656e9fc8d611ea2
Elmanov Grigoriy <gregory.mc.com@gmail.com>
2017-12-06 02:29:33 +0300

    Elmanov Grigoriy, M3234: Dijkstra algo commit

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,15 +1,35 @@
-fun main(args: Array<String>) {
-    println("Hello world!")
-}
+import kotlin.collections.ArrayList
 
-fun foo() = 10
+/**
+ * Created by григорий on 04.12.2017.
+ */
 
-fun sum(vararg ints: Int): Int {
-    var result = 0
-    for (v in ints) {
-        result += v
+fun s_path(n: Int, s1: Int, f1: Int, g: Array<ArrayList<Pair<Int, Int>>>): Int {
+    var s = s1 - 1
+    var f = f1 - 1
+    val inf: Int = 100000000
+    val maxn: Int = 2000
+    val d = Array<Int>(maxn, { _ -> inf })
+    val used = Array<Boolean>(maxn, { _ -> false })
+    d[s] = 0
+    for (i in 0..n - 1) {
+        var v = -1
+        for (j in 0.. n - 1) {
+            if (!used[j] && (v == - 1 || d[j] < d[v])) {
+                v = j
+            }
+        }
+        if (d[v] == inf) {
+            break
+        }
+        used[v] = true
+        for (j in 0..g[v].size - 1) {
+            val to = g[v][j].first
+            val cost = g[v][j].second
+            if (d[v] + cost < d[to]) {
+                d[to] = d[v] + cost
+            }
+        }
     }
-    return result
+    return d[f]
 }
-
-fun sumFun(vararg ints: Int) = ints.fold(0) { acc, i -> acc + i }
--- src/test/kotlin/MainTest.kt
+++ src/test/kotlin/MainTest.kt
@@ -3,37 +3,61 @@ import org.junit.Test
 
 class MainTest {
     @Test
-    fun testFoo() {
-        assertEquals(10, foo())
+    fun testPathExample() {
+        val maxn = 2000
+        val g = Array(maxn, { _ -> ArrayList<Pair<Int, Int>>(0) })
+        g[0] = arrayListOf(Pair(0, 0), Pair(2, 2))
+        g[1] = arrayListOf(Pair(0, 3), Pair(1, 0))
+        g[2] = arrayListOf(Pair(1, 4), Pair(2, 0))
+        assertEquals(6, s_path(3, 1, 2, g))
     }
 
     @Test
-    fun testSumEmpty() {
-        assertEquals(0, sum())
+    fun testPathForOneNode() {
+        val maxn = 2000
+        val g = Array(maxn, { _ -> ArrayList<Pair<Int, Int>>(0) })
+        assertEquals(0, s_path(1, 1, 1, g))
     }
 
     @Test
-    fun testSumSingle() {
-        assertEquals(42, sum(42))
+    fun testPathForTwoNodes() {
+        val maxn = 2000
+        val g = Array(maxn, { _ -> ArrayList<Pair<Int, Int>>(0) })
+        g[0] = arrayListOf(Pair(1, 1))
+        g[1] = arrayListOf(Pair(0, 3))
+        assertEquals(1, s_path(2, 1, 2, g))
+        assertEquals(3, s_path(2, 2, 1, g))
     }
 
     @Test
-    fun testSumMany() {
-        assertEquals(6, sum(1, 2, 3))
+    fun testPathForCycle() {
+        val maxn = 2000
+        val g = Array(maxn, { _ -> ArrayList<Pair<Int, Int>>(0) })
+        g[0] = arrayListOf(Pair(1, 1))
+        g[1] = arrayListOf(Pair(2, 2))
+        g[2] = arrayListOf(Pair(3, 3))
+        g[3] = arrayListOf(Pair(0, 4))
+        assertEquals(1, s_path(4, 1, 2, g))
+        assertEquals(2, s_path(4, 2, 3, g))
+        assertEquals(3, s_path(4, 3, 4, g))
+        assertEquals(4, s_path(4, 4, 1, g))
     }
 
     @Test
-    fun testSumFunEmpty() {
-        assertEquals(0, sumFun())
-    }
-
-    @Test
-    fun testSumFunSingle() {
-        assertEquals(42, sumFun(42))
-    }
-
-    @Test
-    fun testSumFunMany() {
-        assertEquals(6, sumFun(1, 2, 3))
+    fun testPathForCycleNotOriented() {
+        val maxn = 2000
+        val g = Array(maxn, { _ -> ArrayList<Pair<Int, Int>>(0) })
+        g[0] = arrayListOf(Pair(1, 1), Pair(3, 4))
+        g[1] = arrayListOf(Pair(2, 2), Pair(0, 1))
+        g[2] = arrayListOf(Pair(3, 3), Pair(1, 2))
+        g[3] = arrayListOf(Pair(0, 4), Pair(2, 3))
+        assertEquals(1, s_path(4, 1, 2, g))
+        assertEquals(1, s_path(4, 2, 1, g))
+        assertEquals(2, s_path(4, 2, 3, g))
+        assertEquals(2, s_path(4, 3, 2, g))
+        assertEquals(3, s_path(4, 3, 4, g))
+        assertEquals(3, s_path(4, 4, 3, g))
+        assertEquals(4, s_path(4, 4, 1, g))
+        assertEquals(4, s_path(4, 1, 4, g))
     }
 }
