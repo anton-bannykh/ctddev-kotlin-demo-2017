6708fd5ea6f7585cdf6b1d242cfc394348c63961
TrusienkoDV <34181539+TrusienkoDV@users.noreply.github.com>
2017-12-05 19:16:39 +0300

    Add files via upload

--- src/test/kotlin/MainTest.kt
+++ src/test/kotlin/MainTest.kt
@@ -3,37 +3,38 @@ import org.junit.Test
 
 class MainTest {
     @Test
-    fun testFoo() {
-        assertEquals(10, foo())
+    fun test1() {
+        val n = 6
+        val queries = arrayOf(arrayOf(-1, 1, 2), arrayOf(-1, 2, 3), arrayOf(-1, 3, 4), arrayOf(-2), arrayOf(-1, 4, 5),
+                arrayOf(-1, 5, 1), arrayOf(-2), arrayOf(-1, 4, 6), arrayOf(-2), arrayOf(-1, 5, 6), arrayOf(-2))
+        val ans = mutableListOf<Int>(3, 0, 1, 0)
+        assertEquals(ans, runTest(n, queries))
     }
 
     @Test
-    fun testSumEmpty() {
-        assertEquals(0, sum())
+    fun test2() {
+        val n = 6
+        val queries = arrayOf(arrayOf(-1, 1, 2), arrayOf(-1, 1, 3), arrayOf(-2), arrayOf(-1, 2, 3), arrayOf(-2), arrayOf(-1, 3, 4), arrayOf(-2),
+                arrayOf(-1, 4, 5), arrayOf(-2), arrayOf(-1, 5, 1), arrayOf(-2), arrayOf(-1, 5, 6), arrayOf(-2))
+        val ans = mutableListOf<Int>(2, 0, 1, 2, 0, 1)
+        assertEquals(ans, runTest(n, queries))
     }
 
     @Test
-    fun testSumSingle() {
-        assertEquals(42, sum(42))
+    fun test3() {
+        val n = 6
+        val queries = arrayOf(arrayOf(-1, 1, 2), arrayOf(-1, 1, 3), arrayOf(-1, 1, 4), arrayOf(-2), arrayOf(-1, 2, 3), arrayOf(-2),
+                arrayOf(-1, 1, 5), arrayOf(-2), arrayOf(-1, 4, 5), arrayOf(-2), arrayOf(-1, 5, 6), arrayOf(-2))
+        val ans = mutableListOf<Int>(3, 1, 2, 0, 1)
+        assertEquals(ans, runTest(n, queries))
     }
 
     @Test
-    fun testSumMany() {
-        assertEquals(6, sum(1, 2, 3))
-    }
-
-    @Test
-    fun testSumFunEmpty() {
-        assertEquals(0, sumFun())
-    }
-
-    @Test
-    fun testSumFunSingle() {
-        assertEquals(42, sumFun(42))
-    }
-
-    @Test
-    fun testSumFunMany() {
-        assertEquals(6, sumFun(1, 2, 3))
+    fun test4() {
+        val n = 7
+        val queries = arrayOf(arrayOf(-1, 1, 2), arrayOf(-1, 1, 3), arrayOf(-1, 1, 4), arrayOf(-1, 1, 5),
+                arrayOf(-1, 1, 6), arrayOf(-1, 1, 7), arrayOf(-2), arrayOf(-1, 2, 4), arrayOf(-2))
+        val ans = mutableListOf<Int>(6, 4)
+        assertEquals(ans, runTest(n, queries))
     }
 }
