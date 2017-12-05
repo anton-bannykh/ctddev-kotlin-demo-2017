66ee36917c805a544b2ad0a1d8f4781a5ce82986
Larionov Nikita <iksiygrik@gmail.com>
2017-12-05 18:15:01 +0300

    hw1

--- src/test/kotlin/MainTest.kt
+++ src/test/kotlin/MainTest.kt
@@ -1,39 +1,14 @@
 import org.junit.Assert.assertEquals
 import org.junit.Test
 
-class MainTest {
+class mainTest {
     @Test
-    fun testFoo() {
-        assertEquals(10, foo())
-    }
-
-    @Test
-    fun testSumEmpty() {
-        assertEquals(0, sum())
-    }
-
-    @Test
-    fun testSumSingle() {
-        assertEquals(42, sum(42))
-    }
-
-    @Test
-    fun testSumMany() {
-        assertEquals(6, sum(1, 2, 3))
-    }
-
-    @Test
-    fun testSumFunEmpty() {
-        assertEquals(0, sumFun())
-    }
-
-    @Test
-    fun testSumFunSingle() {
-        assertEquals(42, sumFun(42))
-    }
-
-    @Test
-    fun testSumFunMany() {
-        assertEquals(6, sumFun(1, 2, 3))
+    fun nthTest() {
+        assertEquals(3, nth_element(arrayOf(1, 2, 3, 4, 5, 6), 3));
+        assertEquals(-50 , nth_element(arrayOf(-2500, -50, -10, 10, -40), 2));
+        assertEquals(152 , nth_element(arrayOf(0, 1, 2, -30, 152), 5));
+        assertEquals(42 , nth_element(arrayOf(30, -30, 42, -42, 52), 4));
+        assertEquals(1337 , nth_element(arrayOf(-1337, 0, 6, 13, 1337, 37, 2400, 2437), 6));
+        assertEquals(0 , nth_element(arrayOf(-1, 0, 1, 2, 6, 10), 2));
     }
 }
