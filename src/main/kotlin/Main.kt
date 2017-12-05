c2bcb7f4b047c494228f60c9f64924a4243a0a3b
Vitaliy Glamozda <31710290+Sketcher14@users.noreply.github.com>
2017-12-05 19:05:19 +0300

    Add files via upload

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,15 +1,52 @@
-fun main(args: Array<String>) {
-    println("Hello world!")
-}
-
-fun foo() = 10
-
-fun sum(vararg ints: Int): Int {
-    var result = 0
-    for (v in ints) {
-        result += v
+fun logariphm(x: Int): Int {
+    if (x == 1) {
+        return 0
+    } else {
+        return logariphm(Math.floor(x / 2.0).toInt()) + 1
     }
-    return result
 }
 
-fun sumFun(vararg ints: Int) = ints.fold(0) { acc, i -> acc + i }
+fun power(aa: Int, nn: Int): Int {
+    var a = aa
+    var n = nn
+    var res = 1
+    while (n > 0) {
+        if (n % 2 == 1) {
+            res *= a
+        }
+        a *= a
+        n /= 2
+    }
+    return res
+}
+
+fun solve(n: Int, mas: IntArray, ll: Int, rr: Int): Int {
+    val logariphms = Array<Int>(n + 1, {i -> 0})
+    for (i in 1..n) {
+        logariphms[i] = logariphm(i)
+    }
+    val powers = Array<Int>(logariphms[n] + 1, {i -> (power(2, i))})
+    val sparseTable = Array<Array<Int>>(logariphms[n] + 1, { i -> Array<Int>(n - powers[i] + 1, { j -> 0 }) })
+    for (i in 0..sparseTable[0].size - 1) {
+        sparseTable[0][i] = mas[i]
+    }
+    for (i in 1..sparseTable.size - 1) {
+        for (j in 0..sparseTable[i].size - 1) {
+            sparseTable[i][j] = Math.min(sparseTable[i - 1][j], sparseTable[i - 1][j + powers[i - 1]])
+        }
+    }
+
+    var left = ll
+    var right = rr
+    if (left > right) {
+        val p = left
+        left = right
+        right = p
+    }
+    val k = logariphms[right - left]
+    return Math.min(sparseTable[k][left - 1], sparseTable[k][right - powers[k]])
+}
+
+fun main(args: Array<String>) {
+
+}
