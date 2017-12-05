4fc9dcdded563935f49508b71abf54832982ee95
Golovin Pavel <gopavel0@gmail.com>
2017-12-06 01:16:39 +0300

    Change src to my code

--- src/test/kotlin/MainTest.kt
+++ src/test/kotlin/MainTest.kt
@@ -1,39 +1,157 @@
 import org.junit.Assert.assertEquals
 import org.junit.Test
+import java.util.Random
+import java.util.concurrent.ThreadLocalRandom
+import kotlin.collections.ArrayList
 
 class MainTest {
     @Test
-    fun testFoo() {
-        assertEquals(10, foo())
+    fun testCountSets() {
+        var count = 0
+        for (num in 0..1000) {
+            makeSet(num)
+            count += 1
+            assertEquals(countElement, count)
+            assertEquals(parent.size, count)
+            assertEquals(rank.size, count)
+        }
+        clear()
+        count = 0
+        assertEquals(countElement, count)
+        assertEquals(parent.size, count)
+        assertEquals(rank.size, count)
     }
 
     @Test
-    fun testSumEmpty() {
-        assertEquals(0, sum())
+    fun testFindAfterMake1() {
+        var array = ArrayList<Int>()
+        for (num in 0..10) {
+            array.add(num)
+        }
+        ranmodShuffle(array)
+        for (it in array) {
+            makeSet(it)
+        }
+        for (num1 in 0..10) {
+            for (num2 in 0..10) {
+                assertEquals(num1 == num2,
+                        findLead(num1) == findLead(num2))
+            }
+        }
+        clear()
     }
 
     @Test
-    fun testSumSingle() {
-        assertEquals(42, sum(42))
+    fun testFindAfterMake2() {
+        var array = ArrayList<Int>()
+        for (num in 0..100) {
+            array.add(num)
+        }
+        ranmodShuffle(array)
+        for (it in array) {
+            makeSet(it)
+        }
+        for (num1 in 0..100) {
+            for (num2 in 0..100) {
+                assertEquals(num1 == num2,
+                        findLead(num1) == findLead(num2))
+            }
+        }
+        clear()
     }
 
     @Test
-    fun testSumMany() {
-        assertEquals(6, sum(1, 2, 3))
+    fun testFindAfterMake3() {
+        var array = ArrayList<Int>()
+        for (num in 0..1000) {
+            array.add(num)
+        }
+        ranmodShuffle(array)
+        for (it in array) {
+            makeSet(it)
+        }
+        for (num1 in 0..1000) {
+            for (num2 in 0..1000) {
+                assertEquals(num1 == num2,
+                        findLead(num1) == findLead(num2))
+            }
+        }
+        clear()
     }
 
     @Test
-    fun testSumFunEmpty() {
-        assertEquals(0, sumFun())
+    fun testAllUnion1() {
+        var array = ArrayList<Int>()
+        for (num in 0..1000) {
+            array.add(num)
+        }
+        ranmodShuffle(array)
+        for (num in array) {
+            makeSet(num)
+        }
+        ranmodShuffle(array)
+        for (num in array) {
+            unionSet(0, num)
+        }
+        ranmodShuffle(array)
+        val leadAll = findLead(0)
+        for (v in array) {
+            assertEquals(findLead(v), leadAll)
+        }
+        clear()
     }
 
     @Test
-    fun testSumFunSingle() {
-        assertEquals(42, sumFun(42))
-    }
+    fun testUnion() {
+        var array = ArrayList<Int>()
+        for (num in 0..1000) {
+            array.add(num)
+        }
+        ranmodShuffle(array)
+        for (num in array) {
+            makeSet(num)
+        }
 
-    @Test
-    fun testSumFunMany() {
-        assertEquals(6, sumFun(1, 2, 3))
+        for (num1 in array)
+            for (num2 in array) {
+                if (num1 % 2 == num2 % 2) {
+                    unionSet(num1, num2)
+                }
+            }
+
+        ranmodShuffle(array)
+        for (num1 in array)
+            for (num2 in array) {
+            assertEquals(findLead(num1) == findLead(num2), num1 % 2 == num2 % 2)
+        }
+
+        ranmodShuffle(array)
+
+        for (num1 in array)
+            for (num2 in array) {
+                if (num1 % 2 == num2 % 2) {
+                    unionSet(num1, num2)
+                }
+            }
+
+        ranmodShuffle(array)
+        for (num1 in array)
+            for (num2 in array) {
+                assertEquals(findLead(num1) == findLead(num2), num1 % 2 == num2 % 2)
+            }
+        clear()
+    }
+}
+
+fun ranmodShuffle(arr: ArrayList<Int>) {
+    var j: Int
+    var rand: Random = ThreadLocalRandom.current()
+    for ( i in arr.size - 1 downTo 0) {
+        j = rand.nextInt(i + 1)
+        if (i != j) {
+            arr[j] = arr[i] xor arr[j]
+            arr[i] = arr[i] xor arr[j]
+            arr[j] = arr[i] xor arr[j]
+        }
     }
 }
