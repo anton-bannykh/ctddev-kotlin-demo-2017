b8481609d53f2abcfb4b99404948ce22031a91a3
Osipov Aleksandr <sashaosipov.98@gmail.com>
2017-12-06 18:07:04 +0300

    Small fix

--- src/main/kotlin/SuffixArray.kt
+++ src/main/kotlin/SuffixArray.kt
@@ -15,11 +15,8 @@ fun suffixArray(input: String): IntArray {
     for (i in 0 until n) p[--cnt[s[i]]] = i
 
     c[p[0]] = 0
-    var cl = 0
-    for (i in 1 until n) {
-        if (s[p[i]] != s[p[i - 1]]) cl++
-        c[p[i]] = cl
-    }
+    for (i in 1 until n)
+        c[p[i]] = c[p[i - 1]] + (if (s[p[i]] == s[p[i - 1]]) 0 else 1)
 
     val pn = IntArray(n)
     var cn: IntArray
@@ -28,15 +25,15 @@ fun suffixArray(input: String): IntArray {
     while (k < n) {
         cn = IntArray(n)
         cnt.fill(0)
-        for (i in 0 until n) pn[i] = (p[i] - (k) + n) % n
+        for (i in 0 until n) pn[i] = (p[i] - k + n) % n
 
-        for (i in 0 until n) ++cnt[c[pn[i]]]
+        for (i in 0 until n) cnt[c[pn[i]]]++
         for (i in 1 until c[p[n - 1]] + 1) cnt[i] += cnt[i - 1]
         for (i in n - 1 downTo 0) p[--cnt[c[pn[i]]]] = pn[i]
 
         cn[p[0]] = 0
         for (i in 1 until n) {
-            if (c[p[i]] == c[p[i - 1]] && c[(p[i] + (k)) % n] == c[(p[i - 1] + (k)) % n]) {
+            if (c[p[i]] == c[p[i - 1]] && c[(p[i] + k) % n] == c[(p[i - 1] + k) % n]) {
                 cn[p[i]] = cn[p[i - 1]]
             } else {
                 cn[p[i]] = cn[p[i - 1]] + 1
@@ -44,7 +41,7 @@ fun suffixArray(input: String): IntArray {
         }
 
         c = cn
-        k*=2
+        k *= 2
     }
 
     val answer = IntArray(n - 1)
