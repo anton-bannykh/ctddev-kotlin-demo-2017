197bca9f0064d2264d3971e8e634746c87b48cec
Alexandr Ustinov <34143236+alexust27@users.noreply.github.com>
2017-12-07 02:40:28 +0300

    update tests

--- src/test/kotlin/MainTest.kt
+++ src/test/kotlin/MainTest.kt
@@ -2,38 +2,60 @@ import org.junit.Assert.assertEquals
 import org.junit.Test
 
 class MainTest {
-    @Test
-    fun testFoo() {
-        assertEquals(10, foo())
+
+    private fun checkAns(n: Int, W: Int, weight: Array<Int>, price: Array<Int>): ArrayList<Int> {
+        val ans = ArrayList<Int>()
+        var pr = 0
+        val x = 1 shl (n) - 1
+
+        for (i in 1..x) {
+            var k = i
+            var c = 0
+            var pr2 = 0
+            var v = 0
+            val ans2 = ArrayList<Int>()
+            while (k > 0) {
+                if (k % 2 == 1 && v + weight[n - 1 - c] <= W) {
+                    v += weight[n - 1 - c]
+                    pr2 += price[n - 1 - c]
+                    ans2.add(n - c)
+                }
+                k /= 2
+                c++
+            }
+            if (pr2 > pr) {
+                pr = pr2
+                ans.clear()
+                ans.addAll(ans2)
+            }
+        }
+        ans.sort()
+        return ans
     }
 
     @Test
-    fun testSumEmpty() {
-        assertEquals(0, sum())
+    fun test1() {
+        assertEquals(arrayListOf(1, 3, 4), solve(4, 6, arrayOf(2, 4, 1, 2), arrayOf(7, 2, 5, 1)))
     }
 
     @Test
-    fun testSumSingle() {
-        assertEquals(42, sum(42))
+    fun test2() {
+        assertEquals(arrayListOf(1), solve(1, 1, arrayOf(1), arrayOf(1)))
+    }
+
+
+    @Test
+    fun test3() {
+        val v = 10
+        for (n in 3..15) {
+            val price = Array(n, { i -> i * i })
+            val weight = Array(n, { i -> i + 1 })
+            assertEquals(checkAns(n, v, weight, price), solve(n, v, weight, price))
+        }
     }
 
     @Test
-    fun testSumMany() {
-        assertEquals(6, sum(1, 2, 3))
-    }
-
-    @Test
-    fun testSumFunEmpty() {
-        assertEquals(0, sumFun())
-    }
-
-    @Test
-    fun testSumFunSingle() {
-        assertEquals(42, sumFun(42))
-    }
-
-    @Test
-    fun testSumFunMany() {
-        assertEquals(6, sumFun(1, 2, 3))
+    fun test4() {
+        assertEquals(arrayListOf<Int>(), solve(3, 1, arrayOf(3, 3, 3), arrayOf(4, 4, 4)))
     }
 }
