241581f43f6b8899d0103725f68e43aa19dc8013
SalmonLoki <kseniiakrovlya@gmail.com>
2017-12-06 16:42:56 +0300

    algorihm was added

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,14 +2,45 @@ fun main(args: Array<String>) {
     println("Hello world!")
 }
 
-fun foo() = 10
-
-fun sum(vararg ints: Int): Int {
+val d = 256
+fun search(pat: String, txt: String, q: Int): Int {
     var result = 0
-    for (v in ints) {
-        result += v
+    val M = pat.length
+    val N = txt.length
+    var p = 0 // hash value for pattern
+    var t = 0 // hash value for txt
+    var h = 1
+    var i: Int
+    var j: Int
+    i = 0
+    while (i < M - 1) {
+        h = h * d % q
+        i++
+    }
+    i = 0
+    while (i < M) {
+        p = (d * p + pat[i].toInt()) % q
+        t = (d * t + txt[i].toInt()) % q
+        i++
+    }
+    i = 0
+    while (i <= N - M) {
+        if (p == t) {
+            j = 0
+            while (j < M) {
+                if (txt[i + j] != pat[j])
+                    break
+                j++
+            }
+            if (j == M)
+                result = i + 1
+        }
+        if (i < N - M) {
+            t = (d * (t - txt[i].toInt() * h) + txt[i + M].toInt()) % q
+            if (t < 0)
+                t += q
+        }
+        i++
     }
     return result
 }
-
-fun sumFun(vararg ints: Int) = ints.fold(0) { acc, i -> acc + i }
