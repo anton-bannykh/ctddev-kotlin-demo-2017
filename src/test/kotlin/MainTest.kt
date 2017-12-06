11161559eea67698e270bce89b9d99cb6548551e
sashasashasasha151 <sashasashasasha151@gmail.com>
2017-12-06 05:07:45 +0300

    Add Solution

--- src/test/kotlin/MainTest.kt
+++ src/test/kotlin/MainTest.kt
@@ -3,37 +3,37 @@ import org.junit.Test
 
 class MainTest {
     @Test
-    fun testFoo() {
-        assertEquals(10, foo())
+    fun firstTest() {
+        val automation = Automation(1, 1, "1",
+                "1 1 a")
+        assertEquals(-1, automation.numberOfWords())
     }
 
     @Test
-    fun testSumEmpty() {
-        assertEquals(0, sum())
+    fun secondTest() {
+        val automation = Automation(3, 5, "3",
+                "1 2 a 1 2 b 2 3 a 2 3 b 2 3 c")
+        assertEquals(6, automation.numberOfWords())
     }
 
     @Test
-    fun testSumSingle() {
-        assertEquals(42, sum(42))
+    fun thirdTest() {
+        val automation = Automation(6, 6, "2 3 4 5",
+                "1 2 a 2 3 b 3 4 a 3 5 b 4 6 b 6 5 a")
+        assertEquals(5, automation.numberOfWords())
     }
 
     @Test
-    fun testSumMany() {
-        assertEquals(6, sum(1, 2, 3))
+    fun fourthTest() {
+        val automation = Automation(2, 1, "",
+                "1 2 a")
+        assertEquals(0, automation.numberOfWords())
     }
 
     @Test
-    fun testSumFunEmpty() {
-        assertEquals(0, sumFun())
-    }
-
-    @Test
-    fun testSumFunSingle() {
-        assertEquals(42, sumFun(42))
-    }
-
-    @Test
-    fun testSumFunMany() {
-        assertEquals(6, sumFun(1, 2, 3))
+    fun fifthTest() {
+        val automation = Automation(11, 13, "4 6 11 7",
+                "1 2 a 2 4 b 2 5 c 2 3 a 4 8 a 8 9 b 9 11 a 5 7 d 3 7 a 3 6 b 6 7 b 7 10 e 10 9 f")
+        assertEquals(9, automation.numberOfWords())
     }
 }
