3f068335d5b38efe46bef98dbea510b1b65c424e
Max <alvinmax@mail.ru>
2017-12-06 04:03:32 +0300

    Update MainTest.kt

--- src/test/kotlin/MainTest.kt
+++ src/test/kotlin/MainTest.kt
@@ -1,39 +1,64 @@
-import org.junit.Assert.assertEquals
+import junit.framework.Assert.assertEquals
 import org.junit.Test
+import java.util.Random
+
+/**
+ * Created by Maxim on 05.12.2017.
+ */
 
 class MainTest {
-    @Test
-    fun testFoo() {
-        assertEquals(10, foo())
+    private fun ClosedRange<Int>.random() = Random().nextInt(endInclusive - start) + start
+
+    private fun brutForce(a: IntArray) : Int {
+        var f = IntArray(a.size)
+
+        var result = 0
+        for(i in 0..a.size) {
+            f[i] = 1
+            for(j in 0..i - 1) {
+                if(a[i] > a[j]) {
+                    f[i] = maxOf(f[i], f[j] + 1)
+                }
+            }
+            result = maxOf(result, f[i])
+        }
+
+        return result
     }
 
     @Test
-    fun testSumEmpty() {
-        assertEquals(0, sum())
+    fun testRandom5() {
+        val arr = IntArray(5, { (-1000000000..1000000000).random() })
+        assertEquals(brutForce(arr), getLIS(arr))
     }
 
     @Test
-    fun testSumSingle() {
-        assertEquals(42, sum(42))
+    fun testRandom10() {
+        val arr = IntArray(10, { (-1000000000..1000000000).random() })
+        assertEquals(brutForce(arr), getLIS(arr))
     }
 
     @Test
-    fun testSumMany() {
-        assertEquals(6, sum(1, 2, 3))
+    fun testRandom50() {
+        val arr = IntArray(50, { (-1000000000..1000000000).random() })
+        assertEquals(brutForce(arr), getLIS(arr))
     }
 
     @Test
-    fun testSumFunEmpty() {
-        assertEquals(0, sumFun())
+    fun testRandom100() {
+        val arr = IntArray(100, { (-1000000000..1000000000).random() })
+        assertEquals(brutForce(arr), getLIS(arr))
     }
 
     @Test
-    fun testSumFunSingle() {
-        assertEquals(42, sumFun(42))
+    fun testRandom1000() {
+        val arr = IntArray(1000, { (-1000000000..1000000000).random() })
+        assertEquals(brutForce(arr), getLIS(arr))
     }
 
     @Test
-    fun testSumFunMany() {
-        assertEquals(6, sumFun(1, 2, 3))
+    fun testRandom5000() {
+        val arr = IntArray(5000, { (-1000000000..1000000000).random() })
+        assertEquals(brutForce(arr), getLIS(arr))
     }
 }
