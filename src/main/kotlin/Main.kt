1a8c05820964046f9d6c2c6f68c6eb06d9b8f52d
Max <alvinmax@mail.ru>
2017-12-06 03:42:07 +0300

    Update Main.kt

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,15 +1,58 @@
-fun main(args: Array<String>) {
-    println("Hello world!")
+package main
+
+/**
+ * Created by Maxim on 05.12.2017.
+ */
+
+fun compress(a: IntArray) : IntArray{
+    val b : IntArray = a.copyOf()
+    b.sort()
+    for(i in 0..a.size - 1) {
+        a[i] = b.binarySearch(a[i])
+    }
+    return a
 }
 
-fun foo() = 10
+class FenwickTree {
 
-fun sum(vararg ints: Int): Int {
-    var result = 0
-    for (v in ints) {
-        result += v
+    private val SIZE : Int
+    private var f : IntArray
+
+    constructor(size: Int) {
+        SIZE = size
+        f = IntArray(size)
+    }
+
+    fun add(start: Int, value: Int) {
+        var pos = start
+        while(pos < SIZE) {
+            f[pos] = maxOf(f[pos], value)
+            pos = pos or (pos + 1)
+        }
+    }
+
+    fun get(start: Int) : Int {
+        var pos = start
+        var result = 0
+        while(pos >= 0) {
+            result = maxOf(result, f[pos])
+            pos = (pos and (pos +1)) - 1
+        }
+        return result
+    }
+}
+
+fun getLIS(numbers : IntArray) : Int {
+    val a = compress(numbers)
+    val tree = FenwickTree(a.size)
+    var result : Int = 0
+    for(x in a) {
+        val curr : Int = tree.get(x - 1) + 1
+        result = maxOf(result, curr)
+        tree.add(x, curr)
     }
     return result
 }
 
-fun sumFun(vararg ints: Int) = ints.fold(0) { acc, i -> acc + i }
+
+
