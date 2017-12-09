ed7339530909f64aed57e71e841e45e0dca69fc1
Rodion <34244911+DrRudi@users.noreply.github.com>
2017-12-09 19:51:38 +0300

    Update MainTest.kt

--- lib/src/test/kotlin/my/lib/MainTest.kt
+++ lib/src/test/kotlin/my/lib/MainTest.kt
@@ -7,7 +7,7 @@ import org.junit.Test
 class MainTest {
     @Test
     fun testFind() {
-        val a: myDSU = myDSU()
+        val a: MyDSU = MyDSU()
         a.init(5)
         a.union(3, 4)
         assertEquals(a.find(3), a.find(4))
@@ -15,7 +15,7 @@ class MainTest {
 
     @Test
     fun testFindAndNum() {
-        val a: myDSU = myDSU()
+        val a: MyDSU = MyDSU()
         a.init(5)
         a.union(3, 4)
         assertEquals(4, a.find(3))
@@ -23,7 +23,7 @@ class MainTest {
 
     @Test
     fun testFindNotEquals() {
-        val a: myDSU = myDSU()
+        val a: MyDSU = MyDSU()
         a.init(5)
         a.union(3, 4)
         assertNotEquals(a.find(1), a.find(3))
@@ -31,7 +31,7 @@ class MainTest {
 
     @Test
     fun testFindInOneBigSet() {
-        val a: myDSU = myDSU()
+        val a: MyDSU = MyDSU()
         a.init(10)
         for (i in (1..9))
             a.union(i, i + 1)
@@ -40,14 +40,14 @@ class MainTest {
 
     @Test
     fun testCountSets() {
-        val a: myDSU = myDSU()
+        val a: MyDSU = MyDSU()
         a.init(10)
         assertEquals(a.countSets(), 10)
     }
 
     @Test
     fun testFindAgain() {
-        val a: myDSU = myDSU()
+        val a: MyDSU = MyDSU()
         a.init(5)
         a.union(3, 4)
         a.union(1, 2)
@@ -57,7 +57,7 @@ class MainTest {
 
     @Test
     fun testCountSetsInSmallGroup() {
-        val a: myDSU = myDSU()
+        val a: MyDSU = MyDSU()
         a.init(5)
         a.union(3, 4)
         a.union(1, 2)
@@ -67,7 +67,7 @@ class MainTest {
 
     @Test
     fun testSetSize() {
-        val a: myDSU = myDSU()
+        val a: MyDSU = MyDSU()
         a.init(5)
         a.union(3, 4)
         a.union(1, 2)
