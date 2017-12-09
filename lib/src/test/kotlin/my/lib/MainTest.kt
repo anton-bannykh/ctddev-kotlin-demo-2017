090739ffd2db5a99368bcd6c3acc4997ab81dfc5
DrRudi <dr.rudi2@gmail.com>
2017-12-09 20:37:33 +0300

    Fix
    Fix

--- lib/src/test/kotlin/my/lib/MainTest.kt
+++ lib/src/test/kotlin/my/lib/MainTest.kt
@@ -1,5 +1,3 @@
-package my.lib
-
 import org.junit.Assert.assertEquals
 import org.junit.Assert.assertNotEquals
 import org.junit.Test
@@ -7,32 +5,28 @@ import org.junit.Test
 class MainTest {
     @Test
     fun testFind() {
-        val a: MyDSU = MyDSU()
-        a.init(5)
+        val a: MyDSU = MyDSU(5)
         a.union(3, 4)
         assertEquals(a.find(3), a.find(4))
     }
 
     @Test
     fun testFindAndNum() {
-        val a: MyDSU = MyDSU()
-        a.init(5)
+        val a: MyDSU = MyDSU(5)
         a.union(3, 4)
         assertEquals(4, a.find(3))
     }
 
     @Test
     fun testFindNotEquals() {
-        val a: MyDSU = MyDSU()
-        a.init(5)
+        val a: MyDSU = MyDSU(5)
         a.union(3, 4)
         assertNotEquals(a.find(1), a.find(3))
     }
 
     @Test
     fun testFindInOneBigSet() {
-        val a: MyDSU = MyDSU()
-        a.init(10)
+        val a: MyDSU = MyDSU(10)
         for (i in (1..9))
             a.union(i, i + 1)
         assertEquals(a.find(1), a.find(10))
@@ -40,15 +34,13 @@ class MainTest {
 
     @Test
     fun testCountSets() {
-        val a: MyDSU = MyDSU()
-        a.init(10)
+        val a: MyDSU = MyDSU(10)
         assertEquals(a.countSets(), 10)
     }
 
     @Test
     fun testFindAgain() {
-        val a: MyDSU = MyDSU()
-        a.init(5)
+        val a: MyDSU = MyDSU(5)
         a.union(3, 4)
         a.union(1, 2)
         a.union(2, 3)
@@ -57,8 +49,7 @@ class MainTest {
 
     @Test
     fun testCountSetsInSmallGroup() {
-        val a: MyDSU = MyDSU()
-        a.init(5)
+        val a: MyDSU = MyDSU(5)
         a.union(3, 4)
         a.union(1, 2)
         a.union(2, 3)
@@ -67,8 +58,7 @@ class MainTest {
 
     @Test
     fun testSetSize() {
-        val a: MyDSU = MyDSU()
-        a.init(5)
+        val a: MyDSU = MyDSU(5)
         a.union(3, 4)
         a.union(1, 2)
         a.union(2, 3)
