691bcc3567201d18f99fb7b8a2026feb4f4d4bb9
Duginets Nikita <duginetsn98@gmail.com>
2017-12-10 22:19:16 +0300

    fix bugs

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -9,158 +9,164 @@ fun main(args: Array<String>) {
 * type = 2 -> add edge from v to u
 * type = 3 -> check v and u are in one component
 * */
-class Command(type_: Int, vertex_: Pair<Int, Int>) {
-    val type = type_
-    val vertex = if (vertex_.first > vertex_.second) Pair<Int, Int>(vertex_.second, vertex_.first) else vertex_
+class Command(val type: Int, inputV: Pair<Int, Int>) {
+    val vertex = if (inputV.first > inputV.second) Pair(inputV.second, inputV.first) else inputV
+
 }
 
 typealias PairInt = Pair<Pair<Int, Int>, Int>
 typealias EdgeArray = ArrayList<Pair<Int, Int>>
 
+class DynamicConnectivity(val n: Int, val m: Int, val commands: ArrayList<Command>) {
+    private val dsuSize = ArrayList<Int>()
+    private val dsuParent = ArrayList<Int>()
+    private val edge = ArrayList<PairInt>()
+    private val inputCommand = ArrayList<Command>()
+    private val requestSize: Int = 0
+    private val segmentTree = ArrayList<EdgeArray>()
+    //((b, c), a) a - last value, b != -1 => update dsuParent, c != -1 => update dsuSize
+    private val addedEdges = Vector<PairInt>()
 
-var InputCommand = ArrayList<Command>()
-var dsuSize = ArrayList<Int>()
-var dsuParent = ArrayList<Int>()
-var SegmentTree = Array<EdgeArray>(1, { i -> EdgeArray() })
-var edge = ArrayList<PairInt>()
-var RequestSize: Int = 0
 
-//((b, c), a) a - last value, b != -1 => update dsuParent, c != -1 => update dsuSize
-var AddedEdges = Vector<PairInt>()
+    init {
+        commands.forEach { inputCommand.add(it) }
+        buildSegmentTree(m).forEach { segmentTree.add(it) }
+        requestSize.plus(m)
+        dsuSize.clear()
+        dsuParent.clear()
+    }
 
-/*
-* return segment tree
-* */
-fun BuildSegmentTree(n: Int): Array<EdgeArray> {
-    var curSize = 1
-    while (curSize < n) {
-        curSize *= 2
-    }
-    return Array<EdgeArray>(2 * curSize, { i -> EdgeArray() })
-}
-
-/*
-* return root v
-* */
-fun getDsuRoot(v: Int): Int {
-    var cur = v
-    while (dsuParent[cur] != cur) {
-        cur = dsuParent[cur]
-    }
-    return cur
-}
-
-/*
-* union (u, v)
-* */
-fun UnionVertex(v: Int, u: Int) {
-    val a = getDsuRoot(v)
-    val b = getDsuRoot(u)
-    if (a == b) {
-        return
-    }
-    if (dsuSize[a] > dsuSize[b]) {
-        AddedEdges.add(PairInt(Pair(b, -1), dsuParent[b]))
-        dsuParent[b] = a
-    } else if (dsuSize[a] < dsuSize[b]) {
-        AddedEdges.add(PairInt(Pair(a, -1), dsuParent[a]))
-        dsuParent[a] = b
-    } else {
-        AddedEdges.add(PairInt(Pair(a, -1), dsuParent[a]))
-        dsuParent[a] = b
-        AddedEdges.add(PairInt(Pair(-1, b), dsuSize[b]))
-        dsuSize[b]++
-    }
-}
-
-/*
-* Add edge (v, u) when it exist in graph
-* */
-fun AddToSegmentTree(v: Int, tl: Int, tr: Int, l: Int, r: Int, value: Pair<Int, Int>): Unit {
-    if (l > r) {
-        return
-    }
-    if (l == tl && r == tr) {
-        SegmentTree[v].add(value)
-        return
-    }
-    val tm = (tl + tr) / 2
-    AddToSegmentTree(v * 2, tl, tm, l, Math.min(r, tm), value);
-    AddToSegmentTree(v * 2 + 1, tm + 1, tr, Math.max(tm + 1, l), r, value)
-}
-
-/*
-* solve task
-* */
-fun getResult(v: Int, tl: Int, tr: Int): String {
-    var result = ""
-    val sz = AddedEdges.size
-    for (i in SegmentTree[v]) {
-        UnionVertex(i.first, i.second)
-    }
-    if (tl == tr) {
-        if (tl <= RequestSize && InputCommand[tl - 1].type == 3) {
-            if (getDsuRoot(InputCommand[tl - 1].vertex.first) == getDsuRoot(InputCommand[tl - 1].vertex.second)) {
-                result = "1"
-            } else {
-                result = "0"
-            }
+    /*
+    * return segment tree
+    * */
+    private fun buildSegmentTree(n: Int): Array<EdgeArray> {
+        var curSize = 1
+        while (curSize < n) {
+            curSize *= 2
         }
-    } else {
-        val tm = (tl + tr) / 2
-        result += getResult(v * 2, tl, tm)
-        result += getResult(v * 2 + 1, tm + 1, tr)
+        return Array<EdgeArray>(2 * curSize, { i -> EdgeArray() })
     }
-    while (AddedEdges.size > sz) {
-        if (AddedEdges[AddedEdges.size - 1].first.first != -1) {
-            val last = AddedEdges[AddedEdges.size - 1].first.first
-            dsuParent[last] = AddedEdges[AddedEdges.size - 1].second
+
+    /*
+    * return root v
+    * */
+    private fun getDsuRoot(v: Int): Int {
+        var cur = v
+        while (dsuParent[cur] != cur) {
+            cur = dsuParent[cur]
+        }
+        return cur
+    }
+
+    /*
+    * union (u, v)
+    * */
+    private fun unionVertex(v: Int, u: Int) {
+        val a = getDsuRoot(v)
+        val b = getDsuRoot(u)
+        if (a == b) {
+            return
+        }
+        if (dsuSize[a] > dsuSize[b]) {
+            addedEdges.add(PairInt(Pair(b, -1), dsuParent[b]))
+            dsuParent[b] = a
+        } else if (dsuSize[a] < dsuSize[b]) {
+            addedEdges.add(PairInt(Pair(a, -1), dsuParent[a]))
+            dsuParent[a] = b
         } else {
-            val last = AddedEdges[AddedEdges.size - 1].first.second
-            dsuSize[last] = AddedEdges[AddedEdges.size - 1].second
+            addedEdges.add(PairInt(Pair(a, -1), dsuParent[a]))
+            dsuParent[a] = b
+            addedEdges.add(PairInt(Pair(-1, b), dsuSize[b]))
+            dsuSize[b]++
         }
-        AddedEdges.remove(AddedEdges[AddedEdges.size - 1])
     }
 
-    return result
-}
-
-/*
-* m must be equal commands.size
-* */
-fun solve(n: Int, m: Int, commands: ArrayList<Command>): String {
-    InputCommand = commands
-    SegmentTree = BuildSegmentTree(m)
-    RequestSize = m
-    dsuSize.clear()
-    dsuParent.clear()
-    var time = 1
-
-    for (i in 0..n - 1) {
-        dsuSize.add(1)
-        dsuParent.add(i)
+    /*
+    * Add edge (v, u) when it exist in graph
+    * */
+    private fun addToSegmentTree(v: Int, tl: Int, tr: Int, l: Int, r: Int, value: Pair<Int, Int>): Unit {
+        if (l > r) {
+            return
+        }
+        if (l == tl && r == tr) {
+            segmentTree[v].add(value)
+            return
+        }
+        val tm = (tl + tr) / 2
+        addToSegmentTree(v * 2, tl, tm, l, Math.min(r, tm), value);
+        addToSegmentTree(v * 2 + 1, tm + 1, tr, Math.max(tm + 1, l), r, value)
     }
 
-    for (i in commands) {
-        if (i.type == 1) {
-            edge.add(PairInt(i.vertex, time))
-        } else if (i.type == 2) {
-            var remove = ArrayList<PairInt>()
-            for (it in edge) {
-                if (it.first != i.vertex) {
-                    continue
+    /*
+    * solve task
+    * */
+    private fun getResult(v: Int, tl: Int, tr: Int): String {
+        var result = ""
+        val sz = addedEdges.size
+        for (i in segmentTree[v]) {
+            unionVertex(i.first, i.second)
+        }
+        if (tl == tr) {
+            if (tl <= requestSize && inputCommand[tl - 1].type == 3) {
+                if (getDsuRoot(inputCommand[tl - 1].vertex.first) == getDsuRoot(inputCommand[tl - 1].vertex.second)) {
+                    result = "1"
+                } else {
+                    result = "0"
                 }
-                AddToSegmentTree(1, 1, SegmentTree.size / 2, it.second, time, it.first)
-                remove.add(it)
             }
-            for (it in remove) {
-                edge.remove(it)
-            }
+        } else {
+            val tm = (tl + tr) / 2
+            result += getResult(v * 2, tl, tm)
+            result += getResult(v * 2 + 1, tm + 1, tr)
         }
-        time++
+        while (addedEdges.size > sz) {
+            if (addedEdges[addedEdges.size - 1].first.first != -1) {
+                val last = addedEdges[addedEdges.size - 1].first.first
+                dsuParent[last] = addedEdges[addedEdges.size - 1].second
+            } else {
+                val last = addedEdges[addedEdges.size - 1].first.second
+                dsuSize[last] = addedEdges[addedEdges.size - 1].second
+            }
+            addedEdges.remove(addedEdges[addedEdges.size - 1])
+        }
+
+        return result
     }
-    for (i in edge) {
-        AddToSegmentTree(1, 1, SegmentTree.size / 2, i.second, m, i.first)
+
+    /*
+    * m must be equal commands.size
+    * */
+    fun solve(): String {
+
+        var time = 1
+
+        for (i in 0..n - 1) {
+            dsuSize.add(1)
+            dsuParent.add(i)
+        }
+
+        for (i in commands) {
+            if (i.type == 1) {
+                edge.add(PairInt(i.vertex, time))
+            } else if (i.type == 2) {
+                val remove = ArrayList<PairInt>()
+                for (it in edge) {
+                    if (it.first != i.vertex) {
+                        continue
+                    }
+                    addToSegmentTree(1, 1, segmentTree.size / 2, it.second, time, it.first)
+                    remove.add(it)
+                }
+                for (it in remove) {
+                    edge.remove(it)
+                }
+            }
+            time++
+        }
+        for (i in edge) {
+            addToSegmentTree(1, 1, segmentTree.size / 2, i.second, m, i.first)
+        }
+        return getResult(1, 1, segmentTree.size / 2)
     }
-    return getResult(1, 1, SegmentTree.size / 2)
+
 }
--- src/test/kotlin/MainTest.kt
+++ src/test/kotlin/MainTest.kt
@@ -4,69 +4,6 @@ import java.util.*
 
 class MainTest {
     @Test
-    fun testBuildSegmentTree1() {
-        val result = BuildSegmentTree(4)
-        val expectedValue = Array<EdgeArray>(8, { i -> EdgeArray() })
-        assertEquals(expectedValue, result)
-    }
-
-    @Test
-    fun testBuildSegmentTree2() {
-        val result = BuildSegmentTree(3)
-        val expectedValue = Array<EdgeArray>(8, { i -> EdgeArray() })
-        assertEquals(expectedValue, result)
-    }
-
-    @Test
-    fun testAddSegmentTree1() {
-        val expectedValue = Array<EdgeArray>(8, { i -> EdgeArray() })
-        expectedValue[2].add(Pair<Int, Int>(1, 2))
-        expectedValue[6].add(Pair<Int, Int>(1, 2))
-        SegmentTree = BuildSegmentTree(4)
-        AddToSegmentTree(1, 1, SegmentTree.size / 2, 1, 3, Pair<Int, Int>(1, 2))
-        assertEquals(expectedValue, SegmentTree)
-    }
-
-    @Test
-    fun testAddSegmentTree2() {
-        val expectedValue = Array<EdgeArray>(8, { i -> EdgeArray() })
-        expectedValue[1].add(Pair<Int, Int>(1, 2))
-        expectedValue[7].add(Pair<Int, Int>(3, 3))
-        expectedValue[2].add(Pair<Int, Int>(3, 3))
-
-        SegmentTree = BuildSegmentTree(4)
-        AddToSegmentTree(1, 1, SegmentTree.size / 2, 1, 4, Pair<Int, Int>(1, 2))
-        AddToSegmentTree(1, 1, SegmentTree.size / 2, 4, 4, Pair<Int, Int>(3, 3))
-        AddToSegmentTree(1, 1, SegmentTree.size / 2, 1, 2, Pair<Int, Int>(3, 3))
-        assertEquals(expectedValue, SegmentTree)
-    }
-
-    @Test
-    fun testUnionSet1() {
-        val expectedValueParent = arrayOf(0, 2, 2, 3)
-        for (i in 0..3) {
-            dsuParent.add(i)
-            dsuSize.add(i)
-        }
-        UnionVertex(1, 2)
-        assertEquals(expectedValueParent, dsuParent.toArray())
-    }
-
-    @Test
-    fun testUnionSet2() {
-        dsuParent.clear()
-        dsuSize.clear()
-        val expectedValueParent = arrayOf(2, 2, 2, 3)
-        for (i in 0..3) {
-            dsuParent.add(i)
-            dsuSize.add(i)
-        }
-        UnionVertex(1, 2)
-        UnionVertex(2, 0)
-        assertEquals(expectedValueParent, dsuParent.toArray())
-    }
-
-    @Test
     fun testSolve1() {
         val input = arrayOf(
                 Command(1, Pair<Int, Int>(0, 1)),
@@ -75,7 +12,8 @@ class MainTest {
                 Command(3, Pair<Int, Int>(0, 4))
         )
         val command = ArrayList<Command>(input.asList())
-        val result = solve(6, 4, command)
+        val task = DynamicConnectivity(6, 4, command);
+        val result = Task.solve()
         assertEquals("10", result)
     }
 
@@ -89,7 +27,9 @@ class MainTest {
                 Command(3, Pair<Int, Int>(0, 3))
         )
         val command = ArrayList<Command>(input.asList())
-        val result = solve(6, 5, command)
+        val task = DynamicConnectivity(6, 4, command);
+        val result = Task.solve()
         assertEquals("10", result)
     }
+
 }
