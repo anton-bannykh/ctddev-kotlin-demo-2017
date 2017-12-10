35b16eb944ee1ff647c93d2ebdc9a8b3f63cb6eb
VladislavN <33672258+VladislavN@users.noreply.github.com>
2017-12-10 17:47:49 +0300

    Update MainTest.kt

--- src/test/kotlin/MainTest.kt
+++ src/test/kotlin/MainTest.kt
@@ -6,8 +6,7 @@ class MainTest{
     val random = Random()
 
     @Test
-    fun test1() {
-        println("Test 1")
+    fun test1_1() {
         val n = 3
         val m = 3
         val ex = intArrayOf(1, 2, 1, 3, 2, 3)
@@ -17,8 +16,7 @@ class MainTest{
     }
 
     @Test
-    fun test2() {
-        println("Test 2")
+    fun test1_2() {
         val n = 5
         val m = 5
         val ex = intArrayOf(1, 5, 4, 2, 3, 2, 5, 3, 5, 4)
@@ -28,8 +26,7 @@ class MainTest{
     }
 
     @Test
-    fun test3() {
-        println("Test 3")
+    fun test1_3() {
         val n = 6
         val m = 6
         val ex = intArrayOf(3, 2, 4, 2, 1, 3, 3, 3, 1, 2, 1, 4)
@@ -39,8 +36,7 @@ class MainTest{
     }
 
     @Test
-    fun test4() {
-        println("Test 4")
+    fun test1_4() {
         val n = 6
         val m = 8
         val ex = intArrayOf(3, 1, 1, 2, 1, 3, 1, 4, 4, 4, 4, 5, 4, 6, 4, 3)
@@ -50,8 +46,7 @@ class MainTest{
     }
 
     @Test
-    fun test5() {
-        println("Test 5")
+    fun test1_5() {
         val n = 8
         val m = 9
         val ex = intArrayOf(5, 4, 3, 4, 4, 2, 7, 5, 6, 1, 6, 6, 3, 1, 2, 4, 4, 5)
@@ -62,7 +57,6 @@ class MainTest{
 
     @Test
     fun test2_1() {
-        println("Test 2.1")
         var r = random.nextInt(2) + 3
         val n = r
         val m = r + 1
@@ -72,7 +66,6 @@ class MainTest{
 
     @Test
     fun test2_2() {
-        println("Test 2.2")
         val r = random.nextInt(7) + 3
         val n = r
         val m = r - 1
@@ -82,7 +75,6 @@ class MainTest{
 
     @Test
     fun test2_3() {
-        println("Test 2.3")
         val r = random.nextInt(17) + 3
         val n = r
         val m = r + 3
@@ -92,7 +84,6 @@ class MainTest{
 
     @Test
     fun test2_4() {
-        println("Test 2.4")
         val r = random.nextInt(27) + 3
         val n = r
         val m = r - 2
@@ -102,7 +93,6 @@ class MainTest{
 
     @Test
     fun test2_5() {
-        println("Test 2.5")
         val r = random.nextInt(37) + 3
         val n = r
         val m = r + 5
