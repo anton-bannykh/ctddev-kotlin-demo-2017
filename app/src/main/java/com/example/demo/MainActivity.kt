41f28bb5dd35166ddf7fde838290cb3ca904e4a4
Anton Kopitsa <anton17.04.1998@gmail.com>
2017-12-12 17:36:24 +0300

    Add - answer output

--- app/src/main/java/com/example/demo/MainActivity.kt
+++ app/src/main/java/com/example/demo/MainActivity.kt
@@ -2,13 +2,13 @@ package com.example.demo
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
-import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
 import my.lib.EulerTourTree
 import java.lang.Integer.parseInt
 import java.lang.String.format
+import java.lang.String.valueOf
 
 
 class MainActivity : AppCompatActivity() {
@@ -20,17 +20,14 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val answerField: TextView = findViewById(R.id.answer)
-        val treeField: TextView = findViewById(R.id.tree)
 
         val nodesCount: EditText = findViewById(R.id.nodes_count_input)
         val buildButton: Button = findViewById(R.id.build)
 
-        buildButton.setOnClickListener(View.OnClickListener() {
-            fun onClick(v: View) {
-                currentTree = EulerTourTree(parseInt(nodesCount.getText().toString()))
-                refreshOutput(treeField)
-            }
-        })
+        buildButton.setOnClickListener {
+            currentTree = EulerTourTree(parseInt(nodesCount.getText().toString()))
+            refreshOutput()
+        }
 
         val firstNode: EditText = findViewById(R.id.first_node_input)
         val secondNode: EditText = findViewById(R.id.second_node_input)
@@ -40,55 +37,58 @@ class MainActivity : AppCompatActivity() {
         val connectedButton: Button = findViewById(R.id.connected)
         val sizeButton: Button = findViewById(R.id.size)
 
-        linkButton.setOnClickListener(View.OnClickListener() {
-            fun onClick(v: View) {
-                currentTree.link(parseInt(firstNode.getText().toString()),
-                        parseInt(secondNode.getText().toString()))
-                refreshOutput(treeField)
-            }
-        })
+        linkButton.setOnClickListener {
+            currentTree.link(parseInt(firstNode.getText().toString()),
+                    parseInt(secondNode.getText().toString()))
+            refreshOutput()
+        }
 
-        cutButton.setOnClickListener(View.OnClickListener() {
-            fun onClick(v: View) {
-                currentTree.cut(parseInt(firstNode.getText().toString()),
-                        parseInt(secondNode.getText().toString()))
-                refreshOutput(treeField)
-            }
-        })
 
-        connectedButton.setOnClickListener(View.OnClickListener() {
-            fun onClick(v: View) {
-                val firstNode: Int = parseInt(firstNode.getText().toString())
-                val secondNode: Int = parseInt(secondNode.getText().toString())
-                if (currentTree.connected(firstNode, secondNode)) {
-                    answerField.setText(format("%d and %d are connected", firstNode, secondNode))
-                } else {
-                    answerField.setText(format("%d and %d are not connected", firstNode, secondNode))
-                }
-            }
-        })
+        cutButton.setOnClickListener {
+            currentTree.cut(parseInt(firstNode.getText().toString()),
+                    parseInt(secondNode.getText().toString()))
+            refreshOutput()
+        }
 
-        sizeButton.setOnClickListener(View.OnClickListener() {
-            fun onClick(v: View) {
-                answerField.setText(currentTree.sizeOfNodesComponent(parseInt(firstNode.getText().toString())))
+        connectedButton.setOnClickListener {
+            val firstNode: Int = parseInt(firstNode.getText().toString())
+            val secondNode: Int = parseInt(secondNode.getText().toString())
+            if (currentTree.connected(firstNode, secondNode)) {
+                answerField.setText(format("%d and %d are connected", firstNode, secondNode))
+            } else {
+                answerField.setText(format("%d and %d are not connected", firstNode, secondNode))
             }
-        })
+
+        }
+
+        sizeButton.setOnClickListener {
+            answerField.setText(valueOf(currentTree.sizeOfNodesComponent(parseInt(firstNode.getText()
+                    .toString()))))
+        }
     }
 
-    private fun refreshOutput(treeField: TextView) {
+    private fun refreshOutput() {
+        refreshTree()
+        refreshConnections()
+    }
+
+    private fun refreshTree() {
+        val treeField: TextView = findViewById(R.id.tree)
+
         if (currentTree.size == 0) {
-            treeField.setText(0)
             return
         }
 
         var tree = ""
         var wasVisited: Array<Boolean> = Array(currentTree.size + 1, { false })
-        for (i in 1..currentTree.size + 1) {
+        for (i in 1..currentTree.size) {
             if (!wasVisited[i]) {
+                wasVisited[i] = true
                 tree += i
-                for (j in i + 1..currentTree.size + 1) {
+                for (j in i + 1..currentTree.size) {
                     if (currentTree.connected(i, j)) {
-                        tree += format(" <-> %d", j)
+                        tree += format(" <~> %d", j)
+                        wasVisited[j] = true
                     }
                 }
                 tree += "\n"
@@ -96,5 +96,26 @@ class MainActivity : AppCompatActivity() {
         }
         tree = tree.trim()
         treeField.setText(tree)
+    }
+
+    private fun refreshConnections() {
+        val connectionsField: TextView = findViewById(R.id.connections)
+
+        if (currentTree.size == 0) {
+            return
+        }
+
+        var connections = ""
+        val edges: Array<String> = Array(currentTree.size + 1, { i -> format("%d is connected to:", i) })
+        for (i in 1..currentTree.size) {
+            for (j in 1..currentTree.size) {
+                if (currentTree.directlyConnected(i, j)) {
+                    edges[i] += format(" %d", j)
+                }
+            }
+            connections += format("%s\n", edges[i])
+        }
+        connections = connections.trim()
+        connectionsField.setText(connections)
     }
 }
