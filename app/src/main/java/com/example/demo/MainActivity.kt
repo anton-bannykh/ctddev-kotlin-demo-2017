76090946a37ec1f0a1eaac7f77bc7df3a67c75df
teremax <34099603+teremax@users.noreply.github.com>
2017-12-12 23:37:08 +0300

    Added Kruskal

--- app/src/main/java/com/example/demo/MainActivity.kt
+++ app/src/main/java/com/example/demo/MainActivity.kt
@@ -1,15 +1,46 @@
-package com.example.demo
+package com.example.maxim.kruskal
 
+import android.annotation.SuppressLint
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import my.lib.sumFun
+import android.view.View
+import android.widget.EditText
+import android.widget.TextView
+import kruskal.Edge
+import kruskal.getWeightOfMST
 
 class MainActivity : AppCompatActivity() {
 
+    lateinit var vertecesTextEditor: EditText
+    lateinit var uTextEditor: EditText
+    lateinit var vTextEditor: EditText
+    lateinit var wTextEditor: EditText
+    lateinit var resultTextView: TextView
+    var n = 0
+    val l = ArrayList<Edge>()
+
+    @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        vertecesTextEditor = findViewById(R.id.verteces_input)
+        uTextEditor = findViewById(R.id.u_input)
+        vTextEditor = findViewById(R.id.v_input)
+        wTextEditor = findViewById(R.id.w_input)
+        resultTextView = findViewById(R.id.tv_result_output)
     }
 
-    fun test() = sumFun(1, 2, 3)
+    fun click1(v: View) {
+        n = vertecesTextEditor.text.toString().toInt()
+    }
+
+    fun click2(v: View) {
+        l.add(Edge(uTextEditor.text.toString().toInt(),
+                vTextEditor.text.toString().toInt(),
+                wTextEditor.text.toString().toInt()))
+    }
+
+    fun click3(v: View) {
+        resultTextView.setText(getWeightOfMST(n, l.toTypedArray()).toString())
+    }
 }
