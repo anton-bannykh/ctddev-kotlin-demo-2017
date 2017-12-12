acd624eb7ef9fd8c9035c1abc8c3baac647c1f3d
Alexandr Kravchenko <ratatuy.1995@mail.ru>
2017-12-12 16:58:54 +0300

    fix2

--- app/src/main/java/com/example/demo/MainActivity.kt
+++ app/src/main/java/com/example/demo/MainActivity.kt
@@ -15,7 +15,7 @@ class MainActivity : AppCompatActivity() {
         text = findViewById<EditText>(R.id.input)
     }
 
-    fun Go(v: View) {
+    fun go(v: View) {
         val intent = Intent(this, Main2Activity::class.java)
         val numberArr = text.getText().toString().split(" ")
         val num: ArrayList<Int> = ArrayList()
