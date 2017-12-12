c15ae2a0a6b6326fcbb7bf6d31a4f125cca45512
Egor Galkin <gogex300@gmail.com>
2017-12-12 20:13:32 +0300

    Implement android app realizing UI for LCA algorithm

--- app/src/main/java/com/example/demo/SecondActivity.kt
+++ app/src/main/java/com/example/demo/SecondActivity.kt
@@ -6,7 +6,7 @@ import android.os.Bundle
 import android.view.View
 import android.widget.EditText
 import android.widget.TextView
-import com.example.demo.lca.Tree
+import my.lib.Tree
 
 class SecondActivity : AppCompatActivity() {
 
@@ -14,14 +14,14 @@ class SecondActivity : AppCompatActivity() {
     var sequence = ""
     var errorsFlag = false
 
-    lateinit private var tree : Tree
+    private lateinit var tree : Tree
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_second)
+        setContentView(R.layout.activity_second2)
         val intent = getIntent()
         sequence = intent.getStringExtra("seq")
-        num = intent.getIntExtra("num",1)
+        num = intent.getIntExtra("num", 1)
         println(num)
         buildLca()
     }
@@ -29,7 +29,7 @@ class SecondActivity : AppCompatActivity() {
     private fun buildLca() {
         val showErrors = findViewById<View>(R.id.textView4) as TextView
         val tmp = sequence.split(" ")
-        if (tmp.size != num - 1){
+        if (tmp.size != num - 1) {
             errorsFlag = true
             showErrors.text = "Incorrect sequence length.Return to previous screen and enter correct data."
             return
@@ -38,7 +38,7 @@ class SecondActivity : AppCompatActivity() {
         val g = Array(num ) { ArrayList<Int>(0) }
         var index = 1
         for (it in tmp) {
-            if (Integer.parseInt(it) > tmp.size + 1){
+            if (Integer.parseInt(it) > tmp.size + 1) {
                 errorsFlag = true
                 showErrors.text = "Incorrect vertices value. Return to previous screen and enter correct data."
                 return
@@ -50,10 +50,10 @@ class SecondActivity : AppCompatActivity() {
         tree = Tree(num + 1, g)
     }
 
-    fun getLca(view: View){
+    fun getLca(view: View) {
         val showLcaView = findViewById<View>(R.id.editText) as EditText
         val showOutput = findViewById<View>(R.id.textView3) as TextView
-        if (!errorsFlag){
+        if (!errorsFlag) {
             val lcaVerticesString = showLcaView.text.toString()
             lcaVerticesString.trim()
             val tmp = lcaVerticesString.split(" ")
@@ -69,7 +69,7 @@ class SecondActivity : AppCompatActivity() {
 
     }
 
-    fun backToMainActivity(view : View){
+    fun backToMainActivity(view : View) {
         val intent = Intent(this@SecondActivity, MainActivity::class.java)
         startActivity(intent)
     }
