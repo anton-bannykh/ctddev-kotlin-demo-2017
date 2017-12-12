e4cae95ac65b34a91a36a7eb8eea7dc91faeb140
Beandas <34092536+Beandas@users.noreply.github.com>
2017-12-12 15:56:31 +0300

    update

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,15 +1,11 @@
-fun main(args: Array<String>) {
-    println("Hello world!")
-}
-
-fun foo() = 10
-
-fun sum(vararg ints: Int): Int {
-    var result = 0
-    for (v in ints) {
-        result += v
+fun binSearch(a: IntArray, l: Int, r: Int, x: Int): Int {
+    val m = (l + r) / 2
+    if (x == a[m]) {
+        return m
     }
-    return result
+    return if (a[m] >= x) {
+        binSearch(a, l, m, x)
+    } else {
+        binSearch(a, m + 1, r, x)
+    }
 }
-
-fun sumFun(vararg ints: Int) = ints.fold(0) { acc, i -> acc + i }
--- src/test/kotlin/MainTest.kt
+++ src/test/kotlin/MainTest.kt
@@ -3,37 +3,23 @@ import org.junit.Test
 
 class MainTest {
     @Test
-    fun testFoo() {
-        assertEquals(10, foo())
+    fun test1() {
+        val a: IntArray = intArrayOf(1, 2, 3, 8, 10, 13, 15, 18, 22, 100)
+        assertEquals(3, binSearch(a, -1, a.size, 8))
+        assertEquals(8, binSearch(a, -1, a.size, 22))
     }
 
     @Test
-    fun testSumEmpty() {
-        assertEquals(0, sum())
+    fun test2() {
+        val a: IntArray = intArrayOf(10, 20, 30, 80, 100, 103, 150, 180, 202, 300, 405, 409, 430, 502, 600)
+        assertEquals(14, binSearch(a, -1, a.size, 600))
+        assertEquals(11, binSearch(a, -1, a.size, 409))
     }
 
     @Test
-    fun testSumSingle() {
-        assertEquals(42, sum(42))
-    }
-
-    @Test
-    fun testSumMany() {
-        assertEquals(6, sum(1, 2, 3))
-    }
-
-    @Test
-    fun testSumFunEmpty() {
-        assertEquals(0, sumFun())
-    }
-
-    @Test
-    fun testSumFunSingle() {
-        assertEquals(42, sumFun(42))
-    }
-
-    @Test
-    fun testSumFunMany() {
-        assertEquals(6, sumFun(1, 2, 3))
+    fun test3() {
+        val a: IntArray = intArrayOf(2, 4, 6, 8, 13, 14, 20, 22, 64, 100, 200, 300, 400, 500, 600, 700, 1000, 1200, 50000, 700000)
+        assertEquals(17, binSearch(a, -1, a.size, 1200))
+        assertEquals(6, binSearch(a, -1, a.size, 20))
     }
 }
