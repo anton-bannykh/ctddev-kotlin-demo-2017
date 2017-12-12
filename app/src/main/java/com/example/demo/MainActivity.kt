2f74ba2645ee066f90ec6567210a2f6b41a0033c
Anton Kopitsa <anton17.04.1998@gmail.com>
2017-12-12 13:00:28 +0300

    Add - main activity layout

--- app/src/main/java/com/example/demo/MainActivity.kt
+++ app/src/main/java/com/example/demo/MainActivity.kt
@@ -1,15 +1,26 @@
 package com.example.demo
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import my.lib.sumFun
+import android.support.v7.app.AppCompatActivity
+import android.widget.Button
+import android.widget.EditText
+
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-    }
 
-    fun test() = sumFun(1, 2, 3)
+        val buildButton: Button = findViewById(R.id.build)
+        val nodesCount: EditText = findViewById(R.id.nodes_count)
+
+        val linkButton: Button = findViewById(R.id.link)
+        val cutButton: Button = findViewById(R.id.cut)
+        val connectedButton: Button = findViewById(R.id.connected)
+
+        val firstNode: EditText = findViewById(R.id.first_node_input)
+        val secondNode: EditText = findViewById(R.id.second_node_input)
+
+    }
 }
