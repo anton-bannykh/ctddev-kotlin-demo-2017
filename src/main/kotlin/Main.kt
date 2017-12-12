d491aa6053d2b58ec3f06bdf7be68463dd49a64c
ViktoriaErokhina <viktoria.erokhina@gmail.com>
2017-12-12 20:29:15 +0300

    Upload homework

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,15 +1,62 @@
+import java.util.Scanner
+
 fun main(args: Array<String>) {
-    println("Hello world!")
+    val scn = Scanner(System.`in`)
+    val n = scn.nextInt() // size of array
+    val q = scn.nextInt() // number of queries
+    val fenwickTree = createFenwickTree(n)
+    for (i in 0 until q) {
+        val type = scn.nextInt()
+        if (type == 0) {
+            val l = scn.nextInt()
+            val r = scn.nextInt()
+            val x = scn.nextInt()
+            updateAtSegment(l, r, x, fenwickTree)
+        } else {
+            val index = scn.nextInt()
+            val x = scn.nextInt()
+            updateAtPoint(index, x, fenwickTree)
+        }
+    }
+    for (i in 0 until q) {
+        System.out.println(getElement(i, fenwickTree))
+    }
 }
 
-fun foo() = 10
+fun createFenwickTree(n: Int): Array<Int> {
+    return Array<Int>(n, { 0 })
+}
 
-fun sum(vararg ints: Int): Int {
+fun updateAtPoint(index: Int, value: Int, fw: Array<Int>) {
+    updateAtSegment(index, index, value, fw)
+}
+
+fun update(index: Int, value: Int, fw: Array<Int>) {
+    var currentIndex = index
+    while (currentIndex < fw.size) {
+        fw[currentIndex] += value
+        currentIndex = (currentIndex.or(currentIndex + 1))
+    }
+}
+
+fun updateAtSegment(leftBorder: Int, rightBorder: Int, value: Int, fw: Array<Int>) {
+    update(leftBorder, value, fw)
+    if (rightBorder + 1 < fw.size) {
+        update(rightBorder + 1, -value, fw)
+    }
+}
+
+// Getting sum from [0..r]
+fun getSum(r: Int, fw: Array<Int>): Int {
+    var currentIndex = r
     var result = 0
-    for (v in ints) {
-        result += v
+    while (currentIndex >= 0) {
+        result += fw[currentIndex]
+        currentIndex = currentIndex.and(currentIndex + 1) - 1
     }
     return result
 }
 
-fun sumFun(vararg ints: Int) = ints.fold(0) { acc, i -> acc + i }
+fun getElement(position: Int, fw: Array<Int>): Int {
+    return getSum(position, fw)
+}
--- src/test/kotlin/MainTest.kt
+++ src/test/kotlin/MainTest.kt
@@ -1,39 +1,65 @@
 import org.junit.Assert.assertEquals
 import org.junit.Test
+import java.util.Random
 
 class MainTest {
+
     @Test
-    fun testFoo() {
-        assertEquals(10, foo())
+    fun testNoUpdates() {
+        val fw = createFenwickTree(10)
+        for (i in 0 until fw.size) {
+            assertEquals(getElement(i, fw), 0)
+        }
     }
 
     @Test
-    fun testSumEmpty() {
-        assertEquals(0, sum())
+    fun testPointUpdates() {
+        val n = 12345
+        val r = Random()
+        var currentArray = Array<Int>(n, { 0 })
+        val fw = createFenwickTree(n)
+        val q = Math.abs(r.nextInt()) % 1000 + 10000
+        for (i in 0..q) {
+            val index = Math.abs(r.nextInt()) % currentArray.size
+            val value = Math.abs(r.nextInt()) % 1000
+            currentArray[index] += value
+            updateAtPoint(index, value, fw)
+        }
+        for (i in 0 until fw.size) {
+            assertEquals(currentArray[i], getElement(i, fw))
+        }
     }
 
     @Test
-    fun testSumSingle() {
-        assertEquals(42, sum(42))
-    }
-
-    @Test
-    fun testSumMany() {
-        assertEquals(6, sum(1, 2, 3))
-    }
-
-    @Test
-    fun testSumFunEmpty() {
-        assertEquals(0, sumFun())
-    }
-
-    @Test
-    fun testSumFunSingle() {
-        assertEquals(42, sumFun(42))
-    }
-
-    @Test
-    fun testSumFunMany() {
-        assertEquals(6, sumFun(1, 2, 3))
+    fun testAllUpdates() {
+        val n = 123
+        val r = Random()
+        var currentArray = Array<Int>(n, { 0 })
+        val fw = createFenwickTree(n)
+        val q = Math.abs(r.nextInt()) % 1000 + 10000
+        for (i in 0..q) {
+            val index = Math.abs(r.nextInt()) % currentArray.size
+            val value = Math.abs(r.nextInt()) % 1000
+            currentArray[index] += value
+            updateAtPoint(index, value, fw)
+        }
+        val qq = r.nextInt()
+        for (i in 0..qq) {
+            var l = Math.abs(r.nextInt()) % fw.size
+            var rr = Math.abs(r.nextInt()) % fw.size
+            if (l > rr) {
+                val tmp = l
+                l = rr
+                rr = tmp
+            }
+            val value = Math.abs(r.nextInt()) % 1000
+            updateAtSegment(l, rr, value, fw)
+            for (j in l..rr) {
+                currentArray[j] += value
+            }
+        }
+        for (i in 0 until fw.size) {
+            assertEquals(currentArray[i], getElement(i, fw))
+        }
     }
 }
