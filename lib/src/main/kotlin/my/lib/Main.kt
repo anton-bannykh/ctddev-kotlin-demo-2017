bef62a39090222344cef084645107a346a011bfb
Shostina <shostina77@gmail.com>
2017-12-13 15:57:07 +0300

    inital commit 	modified:   app/build.gradle 	modified:   app/src/main/java/com/example/demo/MainActivity.kt 	modified:   app/src/main/res/layout/activity_main.xml 	modified:   app/src/main/res/values/colors.xml 	modified:   app/src/main/res/values/strings.xml 	modified:   lib/src/main/kotlin/my/lib/Main.kt 	modified:   lib/src/test/kotlin/my/lib/MainTest.kt

--- lib/src/main/kotlin/my/lib/Main.kt
+++ lib/src/main/kotlin/my/lib/Main.kt
@@ -4,14 +4,41 @@ fun main(args: Array<String>) {
     println("Hello world!")
 }
 
-fun foo() = 10
-
-fun sum(vararg ints: Int): Int {
-    var result = 0
-    for (v in ints) {
-        result += v
-    }
-    return result
+fun MutableList<Int>.swap(index1: Int, index2: Int) {
+    val x = this[index1]
+    this[index1] = this[index2]
+    this[index2] = x
 }
 
-fun sumFun(vararg ints: Int) = ints.fold(0) { acc, i -> acc + i }
+fun nextPerm(args:MutableList<Int>):MutableList<Int> {
+    var i = args.size - 2
+    val n:Int = args.size
+    var res:MutableList<Int> = mutableListOf()
+    if (i > 0) {
+        while ((i >= 0) and (args[i] > args[i + 1])) {
+            i -= 1
+            if (i < 0) {
+                break
+            }
+        }
+    }
+    if (i < 0){
+        for(j in 0 .. n - 1) {
+            res.add(0)
+        }
+        return res
+    } else {
+        var j = args.size - 1
+        while(args[j] <= args[i]){
+            j -= 1
+        }
+        for(j in 0 .. n - 1) {
+            res.add(args[j])
+        }
+        res.swap(i, j)
+        for(j in 0 .. ((n - i - 1) / 2) - 1) {
+            res.swap(i + 1 + j, n - 1 - j)
+        }
+    }
+    return res
+}
