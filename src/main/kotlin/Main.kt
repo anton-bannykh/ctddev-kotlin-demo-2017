2cf5ebab5cb7fc7725890fa85c9aa1b3e6cc291e
Egor Yung <yungegor@gmail.com>
2017-12-14 19:50:17 +0300

    Update Main.kt

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,15 +1,38 @@
-fun main(args: Array<String>) {
-    println("Hello world!")
-}
+import kotlin.collections.ArrayList
 
-fun foo() = 10
+var graph: ArrayList<ArrayList<Int>> = arrayListOf()
+var visited: ArrayList<Boolean> = arrayListOf()
+var result = arrayListOf<Int>()
 
-fun sum(vararg ints: Int): Int {
-    var result = 0
-    for (v in ints) {
-        result += v
+fun dfs(n: Int, m: Int, edges: ArrayList<Pair<Int, Int>>, root: Int): ArrayList<Int> {
+    //val scanner = Scanner(System.`in`)
+    //print("Enter amount of vertexes: ")
+    //var n = scanner.nextInt()
+    graph = arrayListOf()
+    visited = arrayListOf()
+    result = arrayListOf()
+    for (i in 0 .. n - 1) {
+        graph.add(arrayListOf<Int>())
+        visited.add(false)
     }
+    //print("Enter amount of edges: ")
+    //var m = scanner.nextInt()
+    //println("Enter $m pairs of linked vertexes ")
+    for (i in 0 .. m - 1) {
+        val u = edges[i].first
+        val v = edges[i].second
+        graph[u - 1].add(v - 1)
+        graph[v - 1].add(u - 1)
+    }
+    dfsich(root - 1)
     return result
 }
 
-fun sumFun(vararg ints: Int) = ints.fold(0) { acc, i -> acc + i }
+fun dfsich(vertex : Int) {
+    visited[vertex] = true
+    result.add(vertex + 1)
+    for (i in 0 .. graph[vertex].size - 1)
+            if (!visited[graph[vertex][i]]) {
+                dfsich(graph[vertex][i])
+            }
+}
