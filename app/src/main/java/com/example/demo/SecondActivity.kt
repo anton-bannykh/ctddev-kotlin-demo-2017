aaa737affeefdf257bd2d5877dd7c6cb3a8c9428
RuzikHan <dim.ionow@yandex.ru>
2017-12-14 23:54:54 +0300

    files for hw2 added

---
+++ app/src/main/java/com/example/demo/SecondActivity.kt
@@ -0,0 +1,63 @@
+package com.example.demo
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_second.*
+import my.lib.RMQ
+import java.util.*
+
+class SecondActivity : AppCompatActivity() {
+
+    var rmq1 = RMQ()
+
+    companion object {
+        const val TOTAL_COUNT = "total_count"
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_second)
+        fillMeImpl()
+    }
+
+    fun fillMeImpl() {
+        val count = intent.getIntExtra(TOTAL_COUNT, 0)
+        val a = Array<Int>(count, { Random().nextInt(21) - 10 })
+        rmq1.create_segment_tree(count, a)
+        showMe()
+    }
+
+    fun setMe(view: View) {
+        val count = intent.getIntExtra(TOTAL_COUNT, 0)
+        val pos = Random().nextInt(count) + 1
+        val num = Random().nextInt(21) - 10
+        rmq1.set(pos, num)
+        showMe()
+        val myText = Toast.makeText(this, "Successfully put num " + num.toString() + " to " + pos.toString() + " position", Toast.LENGTH_SHORT)
+        myText.show()
+    }
+
+    fun minMe(view: View) {
+        val count = intent.getIntExtra(TOTAL_COUNT, 0)
+        var pos1 = Random().nextInt(count) + 1
+        var pos2 = Random().nextInt(count) + 1
+        if (pos1 > pos2) {
+            val x = pos1
+            pos1 = pos2
+            pos2 = x
+        }
+        val ans = rmq1.minstart(pos1, pos2)
+        val myText = Toast.makeText(this, "Min on segment [" + pos1.toString() + "," + pos2.toString() + "] is " + ans.toString(), Toast.LENGTH_SHORT)
+        myText.show()
+    }
+
+    fun showMe() {
+        var s = ""
+        for (i in rmq1.y..rmq1.y + rmq1.n - 1) {
+            s += rmq1.a[i].toString() + ' '
+        }
+        textView.text = getString(R.string.array, s)
+    }
+}
--- lib/src/test/kotlin/my/lib/MainTest.kt
+++ lib/src/test/kotlin/my/lib/MainTest.kt
@@ -5,37 +5,45 @@ import org.junit.Test
 
 class MainTest {
     @Test
-    fun testFoo() {
-        assertEquals(10, foo())
+    fun test1() {
+        val rmq1 = RMQ()
+        rmq1.create_segment_tree(5, arrayOf(1, 2, 3, 4, 5))
+        assertEquals(1, rmq1.minstart(1, 2))
+        assertEquals(1, rmq1.minstart(1, 3))
+        rmq1.set(3, -1)
+        assertEquals(-1, rmq1.minstart(2, 3))
+        assertEquals(4, rmq1.minstart(4, 5))
+        rmq1.set(4, -5)
+        assertEquals(-5, rmq1.minstart(1, 5))
+        rmq1.set(5, 10)
+        assertEquals(10, rmq1.minstart(5, 5))
     }
 
     @Test
-    fun testSumEmpty() {
-        assertEquals(0, sum())
+    fun test2() {
+        val rmq2 = RMQ()
+        rmq2.create_segment_tree(10, arrayOf(-1, 4, 0, 5, 12, 18, -4, -2, 6, 1))
+        assertEquals(-4, rmq2.minstart(1, 10))
+        assertEquals(-1, rmq2.minstart(1, 3))
+        assertEquals(0, rmq2.minstart(3, 3))
+        rmq2.set(3, 4)
+        assertEquals(-4, rmq2.minstart(3, 7))
+        rmq2.set(1, 2)
+        assertEquals(2, rmq2.minstart(1, 6))
     }
 
     @Test
-    fun testSumSingle() {
-        assertEquals(42, sum(42))
-    }
-
-    @Test
-    fun testSumMany() {
-        assertEquals(6, sum(1, 2, 3))
-    }
-
-    @Test
-    fun testSumFunEmpty() {
-        assertEquals(0, sumFun())
-    }
-
-    @Test
-    fun testSumFunSingle() {
-        assertEquals(42, sumFun(42))
-    }
-
-    @Test
-    fun testSumFunMany() {
-        assertEquals(6, sumFun(1, 2, 3))
+    fun test3() {
+        val rmq3 = RMQ()
+        rmq3.create_segment_tree(7, arrayOf(1, 1, 1, 1, 1, 1, 1))
+        assertEquals(1, rmq3.minstart(5, 5))
+        rmq3.set(5, 6)
+        assertEquals(6, rmq3.minstart(5, 5))
+        assertEquals(1, rmq3.minstart(4, 5))
+        rmq3.set(7, -3)
+        rmq3.set(6, -4)
+        assertEquals(-4, rmq3.minstart(6, 7))
+        rmq3.set(1, 0)
+        assertEquals(0, rmq3.minstart(1, 4))
     }
 }
