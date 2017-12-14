73fa586f1810c31082c228a454d04e06054978bd
VladislavN <33672258+VladislavN@users.noreply.github.com>
2017-12-14 19:36:00 +0300

    Update Main.kt

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,55 +1,42 @@
-import java.util.*
+import java.util.PriorityQueue
 
-class Bfs(n : Int, m : Int, inp : MutableList<Int>, var s : Int = -1) {
+fun bfs(n : Int, m : Int, inp : MutableList<Int>, s : Int) : MutableList<Int> {
     var used = mutableListOf<Boolean>()
-    var p  = mutableListOf<Int>()
-    var d  = mutableListOf<Int>()
+    var p = mutableListOf<Int>()
+    var d = mutableListOf<Int>()
     var gr : MutableList<MutableList<Int>> = mutableListOf()
     var q : PriorityQueue<Int> = PriorityQueue<Int>()
-    var ans  = mutableListOf<Int>()
+    var ans = mutableListOf<Int>()
 
-    init {
-        for (i in 0..n - 1) {
-            gr.add(mutableListOf<Int>())
-            used.add(false)
-            d.add(0)
-            p.add(1000000)
-        }
-        for (i in 0..m - 1) {
-            val x = inp[i * 2]
-            val y = inp[i * 2 + 1]
-            gr[x - 1].add(y - 1)
-            gr[y - 1].add(x - 1)
-        }
-        val random = Random()
-        if (s == -1) {
-            s = random.nextInt(n - 1)
-        }
-        bfs(s)
+    for (i in 0..n - 1) {
+        gr.add(mutableListOf<Int>())
+        used.add(false)
+        d.add(0)
+        p.add(1000000)
     }
-
-    fun bfs(s: Int) {
-        ans.add(s+1)
-        used[s] = true
-        q.add(s)
-        p[s] = -1
-        while (!(q.isEmpty())) {
-            var v = q.element()
-            q.remove()
-            for (i in 0..gr[v].size-1) {
-                var to = gr[v][i]
-                if (!used[to]) {
-                    ans.add(to+1)
-                    used[to] = true
-                    q.add(to)
-                    d[to] = d[v] + 1
-                    p[to] = v
-                }
+    for (i in 0..m - 1) {
+        val x = inp[i * 2]
+        val y = inp[i * 2 + 1]
+        gr[x - 1].add(y - 1)
+        gr[y - 1].add(x - 1)
+    }
+    ans.add(s + 1)
+    used[s] = true
+    q.add(s)
+    p[s] = -1
+    while (!(q.isEmpty())) {
+        var v = q.element()
+        q.remove()
+        for (i in 0..gr[v].size - 1) {
+            var to = gr[v][i]
+            if (!used[to]) {
+                ans.add(to + 1)
+                used[to] = true
+                q.add(to)
+                d[to] = d[v] + 1
+                p[to] = v
             }
         }
     }
-
-    fun check() : MutableList<Int> {
-        return ans
-    }
+    return ans
 }
