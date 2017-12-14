396459cf26d3c2441680aa8fb8a7bdaf190b497a
RuzikHan <dim.ionow@yandex.ru>
2017-12-14 23:54:54 +0300

    files for hw2 added

--- app/src/main/java/com/example/demo/MainActivity.kt
+++ app/src/main/java/com/example/demo/MainActivity.kt
@@ -1,8 +1,11 @@
 package com.example.demo
 
-import android.support.v7.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
-import my.lib.sumFun
+import android.support.v7.app.AppCompatActivity
+import android.view.View
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -11,5 +14,28 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
     }
 
-    fun test() = sumFun(1, 2, 3)
+    fun countMePlus (view: View) {
+        val count: Int = Integer.parseInt(textView.text.toString()) + 1
+        textView.text = count.toString()
+    }
+
+    fun countMeMinus (view: View) {
+        var count: Int = Integer.parseInt(textView.text.toString())
+        if (Integer.parseInt(textView.text.toString()) > 0) {
+            count--
+        }
+        textView.text = count.toString()
+    }
+
+    fun fillMe(view: View) {
+        if (Integer.parseInt(textView.text.toString()) > 0) {
+            val randomIntent = Intent(this, SecondActivity::class.java)
+            val count = Integer.parseInt(textView.text.toString())
+            randomIntent.putExtra(SecondActivity.TOTAL_COUNT, count)
+            startActivity(randomIntent)
+        } else {
+            val myText = Toast.makeText(this, "Count must be >0 to fill", Toast.LENGTH_SHORT)
+            myText.show()
+        }
+    }
 }
