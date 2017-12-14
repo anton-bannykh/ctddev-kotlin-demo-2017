27e351685fa0255a0b0e79494b1a73c21ce9d57d
PruStephan <prustephan98@gmail.com>
2017-12-14 19:12:46 +0300

    Add files via upload

--- src/test/kotlin/MainTest.kt
+++ src/test/kotlin/MainTest.kt
@@ -2,38 +2,38 @@ import org.junit.Assert.assertEquals
 import org.junit.Test
 
 class MainTest {
-    @Test
-    fun testFoo() {
-        assertEquals(10, foo())
+//    @Test
+
+    var s = arrayOf(11, 1, 0, 1, 2, 0, 1, 3, 0, 1, 4, 1, 4, 5, 1, 8, 6, 1, 16, 7, 2, 32, 8, 3, 64, 9, 3, 128, 10, 5, 512)
+    var l = Math.log(11.toDouble()).toInt()
+    var builded = Build_graph(s)
+
+    fun test4_6() {
+        assertEquals(20, LCA(4, 6, l))
     }
 
     @Test
-    fun testSumEmpty() {
-        assertEquals(0, sum())
+    fun test4_0() {
+        assertEquals(5, LCA(4, 0, l))
     }
 
     @Test
-    fun testSumSingle() {
-        assertEquals(42, sum(42))
+    fun test4_9() {
+        assertEquals(134, LCA(4, 9, l))
     }
 
     @Test
-    fun testSumMany() {
-        assertEquals(6, sum(1, 2, 3))
+    fun test10_6() {
+        assertEquals(536, LCA(10, 6, l))
     }
 
     @Test
-    fun testSumFunEmpty() {
-        assertEquals(0, sumFun())
+    fun test0_0() {
+        assertEquals(0, LCA(0, 0, l))
     }
 
     @Test
-    fun testSumFunSingle() {
-        assertEquals(42, sumFun(42))
-    }
-
-    @Test
-    fun testSumFunMany() {
-        assertEquals(6, sumFun(1, 2, 3))
+    fun test6_8() {
+        assertEquals(82, LCA(6, 8, l))
     }
 }
