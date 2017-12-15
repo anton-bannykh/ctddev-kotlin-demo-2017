3760678260cc239765a56c8e2330bfa4e3b33631
Osipov Aleksandr <sashaosipov.98@gmail.com>
2017-12-15 22:30:19 +0300

    add android app

--- app/src/main/java/com/example/demo/MainActivity.kt
+++ app/src/main/java/com/example/demo/MainActivity.kt
@@ -2,14 +2,30 @@ package com.example.demo
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import my.lib.sumFun
+import android.text.method.ScrollingMovementMethod
+import kotlinx.android.synthetic.main.activity_main.*
+import my.lib.suffixArray
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        textView.movementMethod = ScrollingMovementMethod()
+        button.setOnClickListener {
+            try {
+                val str: String = editText.text.toString()
+                val temp = suffixArray(str.toLowerCase())
+                textView2.text = Arrays.toString(temp)
+                val s = StringBuilder()
+                for (i in temp) s.append(str.subSequence(i, str.length)).append("\n")
+                textView.text = s
+            } catch (ex: Exception) {
+                textView2.text = "unsupported string"
+                textView.text = ""
+            }
+        }
     }
 
-    fun test() = sumFun(1, 2, 3)
 }
