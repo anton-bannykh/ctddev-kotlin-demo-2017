c9bb08df0d4ac5f81a83a2a3144c1a7a677540d8
airosso <ros.arina1@gmail.com>
2017-12-15 23:35:51 +0300

    fix

--- lib/src/main/kotlin/my/lib/Topsort.kt
+++ lib/src/main/kotlin/my/lib/Topsort.kt
@@ -1,12 +1,10 @@
-/**
- * Created by User on 05.12.2017.
- */
-import java.lang.Integer.parseInt
-import java.util.*
+package my.lib
+
+import java.util.Stack
 
 val st: Stack<Int> = Stack()
 
-fun dfs(color: Array<Int>, graph: Array<ArrayList<Int>>, v: Int): Boolean {
+fun dfs(color: IntArray, graph: Array<ArrayList<Int>>, v: Int): Boolean {
     if (color[v] == 1) return true
     if (color[v] == 2) return false
     color[v] = 1
@@ -18,7 +16,7 @@ fun dfs(color: Array<Int>, graph: Array<ArrayList<Int>>, v: Int): Boolean {
     return false
 }
 
-fun topsort(color: Array<Int>, graph: Array<ArrayList<Int>>, topol: Array<Int>): Boolean {
+fun topsort(color: IntArray, graph: Array<ArrayList<Int>>, topol: IntArray): Boolean {
     var cyclic: Boolean
     for (i in 0..graph.size - 1) {
         cyclic = dfs(color, graph, i)
