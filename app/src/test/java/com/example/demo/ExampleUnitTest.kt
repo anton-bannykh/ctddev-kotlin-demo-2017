4194b44779c90fcdb179e3ad8bb5fbb46a78e8f3
horoyami <34141926+horoyami@users.noreply.github.com>
2017-12-17 00:00:07 +0300

    Add files via upload

---
+++ app/src/test/java/com/example/demo/ExampleUnitTest.kt
@@ -0,0 +1,11 @@
+package com.example.demo
+
+import org.junit.Test
+import org.junit.Assert.assertEquals
+
+class ExampleUnitTest {
+    @Test
+    fun addition_isCorrect() {
+        assertEquals(4, 2 + 2)
+    }
+}
