ef9b80963db2f9d6a6ce6a4539935ff2bdfae0d2
unknown <bloomashka@mail.ru>
2017-12-16 23:36:11 +0300

    Added HW2

--- app/src/main/java/com/example/demo/MainActivity.kt
+++ app/src/main/java/com/example/demo/MainActivity.kt
@@ -2,14 +2,18 @@ package com.example.demo
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import my.lib.sumFun
+import kotlinx.android.synthetic.main.activity_main.*
+import my.lib.lcs
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        button.setOnClickListener {
+            answer.text = lcs(first.text.toString().toList(), second.text.toString().toList()).toString()
+        }
     }
 
-    fun test() = sumFun(1, 2, 3)
+
 }
