6e190bb170f59f878ca9f3aafd0e754bf2150b03
Nikita Voloshin <nik.volosh@yandex.ru>
2017-12-17 00:18:26 +0300

    Android app implementation

--- app/src/main/java/com/example/demo/MainActivity.kt
+++ app/src/main/java/com/example/demo/MainActivity.kt
@@ -1,15 +1,36 @@
 package com.example.demo
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import my.lib.sumFun
+import android.support.v7.app.AlertDialog
+import android.support.v7.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+import my.lib.kth
 
 class MainActivity : AppCompatActivity() {
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-    }
 
-    fun test() = sumFun(1, 2, 3)
+        calcButton.setOnClickListener {
+            val arrayText = arrayField.text.toString()
+            val kText = kField.text.toString()
+
+            val array = arrayText.split(" ").map { s -> s.toInt() }.toIntArray()
+            val k = kText.toInt()
+
+            try {
+                answerField.text = kth(array, k).toString()
+            } catch (ex: IllegalArgumentException) {
+                val alert = AlertDialog.Builder(this@MainActivity).create()
+                alert.setTitle("Error")
+                alert.setMessage(ex.message)
+
+                alert.setButton(AlertDialog.BUTTON_NEUTRAL, "Try again", {
+                    dialogInterface, _ -> dialogInterface.cancel()
+                })
+
+                alert.show()
+            }
+        }
+    }
 }
