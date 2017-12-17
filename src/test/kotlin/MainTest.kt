3b374c01b163584deb251c2bbf9d159c8aedc142
Rudenko-Alexandr <n_aix@list.ru>
2017-12-17 15:53:36 +0300

    hw1

--- src/test/kotlin/MainTest.kt
+++ src/test/kotlin/MainTest.kt
@@ -1,39 +1,43 @@
-import org.junit.Assert.assertEquals
+
+import org.junit.Assert.assertTrue
 import org.junit.Test
+import java.util.Arrays
 
 class MainTest {
+
     @Test
-    fun testFoo() {
-        assertEquals(10, foo())
+    fun test1() {
+        val ms = intArrayOf(0, 1, 2, 3, 4, 5, 6, 7)
+        val ans = ms.clone()
+        quickSort(ms)
+        ans.sort()
+        assertTrue(Arrays.equals(ans, ms))
     }
 
     @Test
-    fun testSumEmpty() {
-        assertEquals(0, sum())
+    fun test2() {
+        val ms = intArrayOf(10, 13, - 17, 10, 0, -100, -1)
+        val ans = ms.clone()
+        quickSort(ms)
+        ans.sort()
+        assertTrue(Arrays.equals(ans, ms))
     }
 
     @Test
-    fun testSumSingle() {
-        assertEquals(42, sum(42))
+    fun test3() {
+        val ms = intArrayOf(1, 4, 2, 3, 1, 8765, 876875, 876, 76786, 97678, 876876, -99999999)
+        val ans = ms.clone()
+        quickSort(ms)
+        ans.sort()
+        assertTrue(Arrays.equals(ans, ms))
     }
 
     @Test
-    fun testSumMany() {
-        assertEquals(6, sum(1, 2, 3))
-    }
-
-    @Test
-    fun testSumFunEmpty() {
-        assertEquals(0, sumFun())
-    }
-
-    @Test
-    fun testSumFunSingle() {
-        assertEquals(42, sumFun(42))
-    }
-
-    @Test
-    fun testSumFunMany() {
-        assertEquals(6, sumFun(1, 2, 3))
+    fun test4() {
+        val ms = intArrayOf(0, 0, -1, 5, 6, 3, 9, -1000000 )
+        val ans = ms.clone()
+        quickSort(ms)
+        ans.sort()
+        assertTrue(Arrays.equals(ans, ms))
     }
 }
