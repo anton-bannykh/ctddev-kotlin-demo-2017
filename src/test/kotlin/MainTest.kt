5d0f0786ff902a9534276c88f3cf90ff4028f3a7
unknown <not.antony@yandex.ru>
2017-12-18 01:06:40 +0300

    HW1 first try

--- src/test/kotlin/MainTest.kt
+++ src/test/kotlin/MainTest.kt
@@ -1,39 +1,90 @@
 import org.junit.Assert.assertEquals
 import org.junit.Test
+import java.util.Random
+import kotlin.collections.ArrayList
 
 class MainTest {
-    @Test
-    fun testFoo() {
-        assertEquals(10, foo())
+    private val random = Random()
+
+    private fun MutableList<Int>.sum(l: Int, r: Int): Int {
+        var sum = 0
+        for (i in l..(r - 1)) {
+            sum += this[i]
+        }
+        return sum
+    }
+
+    private fun randomQuery(tree: SegmentTree, list: MutableList<Int>, mode: Int) {
+        val q = random.nextInt(mode)
+        var a = random.nextInt(tree.size())
+        var b = random.nextInt(tree.size())
+        if (a > b) {
+            val t = a
+            a = b
+            b = t
+        }
+        b++
+        val c = random.nextInt(1000) - 1000
+        when (q) {
+            0 -> assertEquals(list.sum(a, b), tree.ask(a, b))
+            1 -> {
+                tree[a] = c
+                list[a] = c
+            }
+            2 -> assertEquals(list, tree.toList())
+        }
     }
 
     @Test
-    fun testSumEmpty() {
-        assertEquals(0, sum())
+    fun zeros100() {
+        val tree = SegmentTree(100)
+        val list = ArrayList<Int>()
+        for (i in 0..99) {
+            list.add(0)
+        }
+        for (i in 1..1000) {
+            randomQuery(tree, list, 3)
+        }
     }
 
     @Test
-    fun testSumSingle() {
-        assertEquals(42, sum(42))
+    fun zeros10000() {
+        val tree = SegmentTree(10000)
+        val list = ArrayList<Int>()
+        for (i in 0..9999) {
+            list.add(0)
+        }
+        for (i in 1..1000) {
+            randomQuery(tree, list, 2)
+        }
     }
 
     @Test
-    fun testSumMany() {
-        assertEquals(6, sum(1, 2, 3))
+    fun random100() {
+        val tree = SegmentTree(100)
+        val list = ArrayList<Int>()
+        for (i in 0..99) {
+            val t = random.nextInt(1000) - 1000
+            list.add(t)
+            tree[i] = t
+        }
+        for (i in 1..1000) {
+            randomQuery(tree, list, 3)
+        }
     }
 
     @Test
-    fun testSumFunEmpty() {
-        assertEquals(0, sumFun())
+    fun random10000() {
+        val tree = SegmentTree(10000)
+        val list = ArrayList<Int>()
+        for (i in 0..9999) {
+            val t = random.nextInt(1000) - 1000
+            list.add(t)
+            tree[i] = t
+        }
+        for (i in 1..1000) {
+            randomQuery(tree, list, 2)
+        }
     }
 
-    @Test
-    fun testSumFunSingle() {
-        assertEquals(42, sumFun(42))
-    }
-
-    @Test
-    fun testSumFunMany() {
-        assertEquals(6, sumFun(1, 2, 3))
-    }
 }
