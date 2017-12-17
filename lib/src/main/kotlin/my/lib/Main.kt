cf71a9767f394f10a7ab0d03a43e7e50eb676c3f
alexandr <ustinov1998s@mail.ru>
2017-12-17 14:32:50 +0300

    hw2

--- lib/src/main/kotlin/my/lib/Main.kt
+++ lib/src/main/kotlin/my/lib/Main.kt
@@ -1,17 +1,53 @@
 package my.lib
 
-fun main(args: Array<String>) {
-    println("Hello world!")
-}
-
-fun foo() = 10
-
-fun sum(vararg ints: Int): Int {
-    var result = 0
-    for (v in ints) {
-        result += v
+private fun make_ans(i: Int, j: Int, weight: IntArray, ans: ArrayList<Int>, d: Array<IntArray>) {
+    if (d[i][j] == 0) {
+        return
     }
-    return result
+    if (d[i - 1][j] == d[i][j]) {
+        make_ans(i - 1, j, weight, ans, d)
+    } else {
+        make_ans(i - 1, j - weight[i], weight, ans, d)
+        ans.add(i)
+    }
 }
 
-fun sumFun(vararg ints: Int) = ints.fold(0) { acc, i -> acc + i }
+fun solve(n: Int, W: Int, weight2: IntArray, price2: IntArray): ArrayList<Int> {
+    val price = intArrayOf(0).plus(price2.plus(IntArray(1000 - n, { 0 })))
+    val weight = intArrayOf(0).plus(weight2.plus(IntArray(1000 - n, { 0 })))
+    val d = Array(1001, { IntArray(1001, { 0 }) })
+    val ans = ArrayList<Int>()
+
+    for (i in 1..n) {
+        for (j in 1..W) {
+            if (j >= weight[i]) {
+                d[i][j] = Math.max(d[i - 1][j],
+                        d[i - 1][j - weight[i]] + price[i])
+            } else {
+                d[i][j] = d[i - 1][j]
+            }
+        }
+    }
+
+    make_ans(n, W, weight, ans, d)
+    ans.sort()
+    return ans
+}
+
+
+fun main(args: Array<String>) {
+    val string = "1,2         3 4 , 5   ,5   5 ,  5, 2"
+    val a = parse(string)
+    for (x in a) {
+        print("$x ")
+    }
+}
+
+fun parse(string: String) =
+        string.split(',')
+                .filterNot { it.isEmpty() }
+                .map { it.split(' ') }
+                .flatMap { it }
+                .filterNot { it.isEmpty() }
+                .map { it.toInt() }.toIntArray()
+
