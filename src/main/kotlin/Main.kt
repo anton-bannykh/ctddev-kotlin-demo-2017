7b78fac719241c300957e6808222e783b08cf3dd
DrRudi <dr.rudi2@gmail.com>
2017-12-17 23:30:02 +0300

    OK?

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,11 +1,13 @@
+package my.lib
+
 class MyDSU(sizeX: Int) {
 
-    private var parent: Array<Int>? = Array(sizeX + 1, { it })
-    private var size: Array<Int>? = Array(sizeX + 1, { 1 })
-    private var countSets: Int = sizeX
+    private val parent = Array(sizeX + 1, { it })
+    private val size = Array(sizeX + 1, { 1 })
+    private var countSets = sizeX
 
     fun size(x: Int): Int {
-        return size!![find(x)]
+        return size[find(x)]
     }
 
     fun countSets(): Int {
@@ -16,18 +18,18 @@ class MyDSU(sizeX: Int) {
         var x = find(x)
         var y = find(y)
         if (x == y) return
-        if (size!![x] > size!![y]) x = y.also { y = x }
-        parent!![x] = y
-        size!![y] += size!![x]
+        if (size[x] > size[y]) x = y.also { y = x }
+        parent[x] = y
+        size[y] += size[x]
         countSets -= 1
     }
 
     fun find(x: Int): Int {
-        if (parent!![x] == x)
-            parent!![x] = x
+        if (parent[x] == x)
+            parent[x] = x
         else
-            parent!![x] = find(parent!![x])
-        return parent!![x]
+            parent[x] = find(parent[x])
+        return parent[x]
     }
 }
 
