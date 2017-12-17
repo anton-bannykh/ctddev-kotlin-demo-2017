43705e395f3e10961db88e34c242694a08759f33
Rudenko-Alexandr <n_aix@list.ru>
2017-12-17 15:53:36 +0300

    hw1

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,15 +1,26 @@
-fun main(args: Array<String>) {
-    println("Hello world!")
-}
-
-fun foo() = 10
-
-fun sum(vararg ints: Int): Int {
-    var result = 0
-    for (v in ints) {
-        result += v
+fun quickSort(array: IntArray, left: Int = 0, right: Int = array.size - 1) {
+    if (left >= right)
+        return
+    var i = left
+    var j = right
+    var mid = i - (i - j) / 2
+    while (i < j) {
+        while (i < mid && array[i] <= array[mid]) {
+            i++
+        }
+        while (j > mid && array[mid] <= array[j]) {
+            j--
+        }
+        if (i < j) {
+            val temp = array[i]
+            array[i] = array[j]
+            array[j] = temp
+            if (i == mid)
+                mid = j
+            else if (j == mid)
+                mid = i
+        }
     }
-    return result
+    quickSort(array, left, mid)
+    quickSort(array, mid + 1, right)
 }
-
-fun sumFun(vararg ints: Int) = ints.fold(0) { acc, i -> acc + i }
