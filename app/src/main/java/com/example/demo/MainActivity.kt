9efc13d460fe7035a62d72f52dda7ec154903c83
Evgenii Glukhov <gluhov1998@yandex.ru>
2017-12-18 02:04:39 +0300

    fix

--- app/src/main/java/com/example/demo/MainActivity.kt
+++ app/src/main/java/com/example/demo/MainActivity.kt
@@ -4,11 +4,11 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import kotlinx.android.synthetic.main.activity_main.*
-import my.lib.*
+import my.lib.create
 
 class MainActivity : AppCompatActivity() {
     var n = 0
-    var array = Array<Int>(1, {0})
+    var array = Array<Int>(1, { 0 })
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -20,8 +20,9 @@ class MainActivity : AppCompatActivity() {
 
     fun readArray(view: View) {
         val arrayString = readArray.text.toString().split(" ")
-        array = Array<Int>(arrayString.size, {i -> arrayString[i].toInt()})
+        array = Array<Int>(arrayString.size, { i -> arrayString[i].toInt() })
     }
+
     fun startQuickSort(view: View) {
         val unsortedArray = array
         var unsortedArrayString = ""
