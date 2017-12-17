3ffe0117c927ba022008dc4bda263684c91f49aa
Glebwin <glebwin@gmail.com>
2017-12-17 12:56:52 +0300

    Add android app for quick sort algorithm.

--- app/src/main/java/com/example/demo/MainActivity.kt
+++ app/src/main/java/com/example/demo/MainActivity.kt
@@ -1,15 +1,33 @@
 package com.example.demo
 
-import android.support.v7.app.AppCompatActivity
+import android.content.ClipData
+import android.content.ClipboardManager
+import android.content.Context
 import android.os.Bundle
-import my.lib.sumFun
+import android.support.v7.app.AppCompatActivity
+import android.view.View
+import android.widget.EditText
+import quickSort
+
 
 class MainActivity : AppCompatActivity() {
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
 
-    fun test() = sumFun(1, 2, 3)
+    fun sortButtonOnClick(v: View) {
+        val inputEditText = findViewById<EditText>(R.id.inputEditText)
+        val outputEditText = findViewById<EditText>(R.id.outputEditText)
+        val array = ArrayList(inputEditText.text.split(" ").map { it.toInt() })
+        quickSort(array)
+        outputEditText.setText(array.joinToString(" ") { it.toString() })
+    }
+
+    fun copyButtonOnClick(v: View) {
+        val outputEditText = findViewById<EditText>(R.id.outputEditText)
+        val clipboard = getSystemService(Context.CLIPBOARD_SERVICE) as ClipboardManager
+        val clip = ClipData.newPlainText("sorted data", outputEditText.text)
+        clipboard.primaryClip = clip
+    }
 }
--- lib/src/test/kotlin/my/lib/MainTest.kt
+++ lib/src/test/kotlin/my/lib/MainTest.kt
@@ -3,6 +3,7 @@ package my.lib
 import org.junit.Assert.assertEquals
 import org.junit.Assert.assertTrue
 import org.junit.Test
+import quickSort
 import java.util.Random
 
 class MainTest {
