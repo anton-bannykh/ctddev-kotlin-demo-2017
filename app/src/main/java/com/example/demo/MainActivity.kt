55fad75e46c520f8e65572a89332f3a6c8683861
Glebwin <glebwin@gmail.com>
2017-12-17 14:04:24 +0300

    Move buttons' onClick methods into lambdas.

--- app/src/main/java/com/example/demo/MainActivity.kt
+++ app/src/main/java/com/example/demo/MainActivity.kt
@@ -5,7 +5,7 @@ import android.content.ClipboardManager
 import android.content.Context
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
-import android.view.View
+import android.widget.Button
 import android.widget.EditText
 import quickSort
 
@@ -13,20 +13,20 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-    }
-
-    fun sortButtonOnClick(v: View) {
-        val inputEditText = findViewById<EditText>(R.id.inputEditText)
-        val outputEditText = findViewById<EditText>(R.id.outputEditText)
-        val array = ArrayList(inputEditText.text.split(" ").map { it.toInt() })
-        quickSort(array)
-        outputEditText.setText(array.joinToString(" ") { it.toString() })
-    }
-
-    fun copyButtonOnClick(v: View) {
-        val outputEditText = findViewById<EditText>(R.id.outputEditText)
-        val clipboard = getSystemService(Context.CLIPBOARD_SERVICE) as ClipboardManager
-        val clip = ClipData.newPlainText("sorted data", outputEditText.text)
-        clipboard.primaryClip = clip
+        val sortButton = findViewById<Button>(R.id.sortButton)
+        val copyButton = findViewById<Button>(R.id.copyButton)
+        sortButton.setOnClickListener {
+            val inputEditText = findViewById<EditText>(R.id.inputEditText)
+            val outputEditText = findViewById<EditText>(R.id.outputEditText)
+            val array = ArrayList(inputEditText.text.split(" ").map { it.toInt() })
+            quickSort(array)
+            outputEditText.setText(array.joinToString(" ") { it.toString() })
+        }
+        copyButton.setOnClickListener {
+            val outputEditText = findViewById<EditText>(R.id.outputEditText)
+            val clipboard = getSystemService(Context.CLIPBOARD_SERVICE) as ClipboardManager
+            val clip = ClipData.newPlainText("sorted data", outputEditText.text)
+            clipboard.primaryClip = clip
+        }
     }
 }
