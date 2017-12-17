7afb391882fc4b45fe826675fd81b055eb932ed2
zelenoff4 <zelenoff9988@gmail.com>
2017-12-17 18:38:10 +0300

    Update HW2

--- app/src/main/java/com/example/demo/MainActivity.kt
+++ app/src/main/java/com/example/demo/MainActivity.kt
@@ -1,8 +1,10 @@
-package com.example.demo
+package com.example.mcyoba.cbs
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import my.lib.sumFun
+import android.view.View
+import android.widget.TextView
+import com.example.demo.R
 
 class MainActivity : AppCompatActivity() {
 
@@ -11,5 +13,53 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
     }
 
-    fun test() = sumFun(1, 2, 3)
+    class Stack<T> {
+        val elements: MutableList<T> = mutableListOf()
+        fun isEmpty() = elements.isEmpty()
+        fun count() = elements.size
+        fun push(item: T) = elements.add(item)
+        fun pop(): T? {
+            val item = elements.lastOrNull()
+            if (!isEmpty()) {
+                elements.removeAt(elements.size - 1)
+            }
+            return item
+        }
+
+        fun peek(): T? = elements.lastOrNull()
+
+    }
+
+    fun cbs(s: String): Boolean {
+        val st = Stack<Char>()
+        var flag = false
+        for (i in s) {
+            if (i == '(')
+                st.push(i)
+            else {
+                if (st.isEmpty()) {
+                    flag = true
+                    break
+                } else
+                    st.pop()
+
+            }
+        }
+        return (st.isEmpty() && !flag)
+
+    }
+
+    fun input(view: View) {
+        val inputText = findViewById<View>(R.id.input_field) as TextView
+        val sequence = inputText.text.toString()
+        inputText.text = ""
+        val outputText = findViewById<View>(R.id.answer) as TextView
+        if (cbs(sequence)) {
+            outputText.text = getString(R.string.Corr)
+
+        } else {
+            outputText.text = getString(R.string.False)
+        }
+    }
+
 }
