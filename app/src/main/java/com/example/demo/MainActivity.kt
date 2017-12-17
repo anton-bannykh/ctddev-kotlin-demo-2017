3c28356a31dac840251061c9d56764d915f73cb8
Ilya <ilja.sahipoff@yandex.ru>
2017-12-17 14:52:12 +0300

    HW2

--- app/src/main/java/com/example/demo/MainActivity.kt
+++ app/src/main/java/com/example/demo/MainActivity.kt
@@ -2,14 +2,38 @@ package com.example.demo
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import my.lib.sumFun
+import android.widget.Button
+import android.widget.TextView
+import my.lib.sort_
 
 class MainActivity : AppCompatActivity() {
-
+    val ms = intArrayOf(1, 4, 2, 5, 6, 4, -4, -43, 313, 242, 32, 42, 2, 4, 42)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        var tv = findViewById(R.id.tv) as TextView
+        var text: String
+        text = ""
+        for (it in ms) {
+            text += it
+            text += " "
+        }
+        tv.setText(text)
+        var btn_sort = findViewById(R.id.btn_sort) as Button
+        btn_sort.setOnClickListener {
+            sort_(ms)
+            val sorted_ms = ms
+            text = ""
+            for (it in sorted_ms) {
+                text += it
+                text += ' '
+            }
+            tv.setText(text)
+        }
     }
 
-    fun test() = sumFun(1, 2, 3)
+    //fun test() = sumFun(1, 2, 3)
+
 }
+
+
