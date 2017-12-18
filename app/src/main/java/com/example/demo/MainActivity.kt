75146525a0529cd45c3c36cafea9847cbd9edcf0
Dogzik <levdovzhik@gmail.com>
2017-12-18 06:20:22 +0300

    First working prototype

--- app/src/main/java/com/example/demo/MainActivity.kt
+++ app/src/main/java/com/example/demo/MainActivity.kt
@@ -2,45 +2,124 @@ package com.example.demo
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.constraint.ConstraintSet
 import android.view.View
+import android.widget.EditText
 import android.widget.TextView
-import kotlinx.android.synthetic.main.activity_main.*
 import kotlin.math.min
 import my.lib.random
 import my.lib.genTest
 import my.lib.maxFlow
+import dsl.constraintLayout
 
 class MainActivity : AppCompatActivity() {
-    var TextView.value : Int
+    var TextView.value: Int
         get() = text.toString().toInt()
         set(x) {
             text = x.toString()
         }
 
+    val LEFT = ConstraintSet.START
+    val RIGHT = ConstraintSet.END
+    val TOP = ConstraintSet.TOP
+    val BOTTOM = ConstraintSet.BOTTOM
+
+    val layoutId = 1
+    val text1Id = 11
+    val text2Id = 12
+    val text3Id = 13
+    val text4Id = 14
+    val number1Id = 21
+    val number2Id = 22
+    val number3Id = 23
+    val number4Id = 24
+    val startId = 31
+    val randId = 32
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(
+                constraintLayout(layoutId) {
+                    textView(text1Id) {
+                        text = "Number of vertices:"
+                        topMargin(layoutId, TOP, dp(24))
+                        leftMargin(layoutId, LEFT, dp(8))
+                    }
+
+                    textView(text2Id) {
+                        text = "Number of edges:"
+                        topMargin(text1Id, BOTTOM, dp(16))
+                        leftMargin(layoutId, LEFT, dp(8))
+                    }
+
+                    textView(text3Id) {
+                        text = "Max capacity:"
+                        topMargin(text2Id, BOTTOM, dp(16))
+                        leftMargin(layoutId, LEFT, dp(8))
+                    }
+
+                    number(number1Id) {
+                        topMargin(layoutId, TOP, dp(24))
+                        rightMargin(layoutId, RIGHT, dp(16))
+                    }
+
+                    number(number2Id) {
+                        topMargin(number1Id, BOTTOM, dp(16))
+                        rightMargin(layoutId, RIGHT, dp(16))
+                    }
+
+                    number(number3Id) {
+                        topMargin(number2Id, BOTTOM, dp(16))
+                        rightMargin(layoutId, RIGHT, dp(16))
+                    }
+
+                    number(number4Id) {
+                        bottomMargin(layoutId, BOTTOM, dp(52))
+                        center(layoutId)
+                    }
+
+                    textView(text4Id) {
+                        bottomMargin(number4Id, TOP, 0)
+                        center(layoutId)
+                        text = "Maximum Flow"
+                        textAlignment = View.TEXT_ALIGNMENT_CENTER
+                    }
+
+                    button(startId) {
+                        topMargin(number3Id, BOTTOM, dp(32))
+                        rightMargin(layoutId, RIGHT, dp(44))
+                        text = "START"
+
+                        onCLick {
+                            val n = this@MainActivity.findViewById<EditText>(number1Id).value
+                            val m = this@MainActivity.findViewById<EditText>(number2Id).value
+                            val data = genTest(n, m, this@MainActivity.findViewById<EditText>(number3Id).value)
+                            val flows = maxFlow(n, m, data.v, data.u, data.c, 0, n - 1)
+                            var res = 0
+                            data.v.forEachIndexed { ind, ver -> if (ver == 1) res += flows[ind] }
+                            this@MainActivity.findViewById<EditText>(number4Id).value = res
+                        }
+                    }
+
+                    button(randId) {
+                        topMargin(startId, TOP, 0)
+                        leftMargin(layoutId, LEFT, dp(8))
+                        rightMargin(startId, LEFT, dp(8))
+                        text = "RANDOM"
+
+                        onCLick {
+                            makeRandomInput()
+                        }
+                    }
+                }
+        )
         makeRandomInput()
     }
 
     private fun makeRandomInput() {
         val n = (2..10).random()
-        numberOfVertices.value = n
-        numberOfEdges.value = (1..min(50, n * (n - 1))).random()
-        maxCapacity.value = (1..100).random()
-    }
-
-    fun genRandomInput(view: View) {
-        makeRandomInput()
-    }
-
-    fun start(view: View) {
-        val n = numberOfVertices.value
-        val m = numberOfEdges.value
-        val data = genTest(n, m, maxCapacity.value)
-        val flows = maxFlow(n, m, data.v, data.u, data.c, 0, n - 1)
-        var res = 0
-        data.v.forEachIndexed { ind, ver -> if (ver == 1) res += flows[ind] }
-        Ans.value = res
+        findViewById<EditText>(number1Id).value = n
+        findViewById<EditText>(number2Id).value = (1..min(50, n * (n - 1))).random()
+        findViewById<EditText>(number3Id).value = (1..100).random()
     }
 }
---
+++ app/src/main/java/dsl/DSL.kt
@@ -0,0 +1,138 @@
+package dsl
+
+import android.support.constraint.ConstraintLayout
+import android.support.constraint.ConstraintSet
+import android.support.v7.app.AppCompatActivity
+import android.text.InputType
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import com.example.demo.R
+
+class MyConstraintLayout(private val act: AppCompatActivity, name: Int) : ConstraintLayout(act) {
+    private val k = act.applicationContext.resources.displayMetrics.density
+    private val bounds = ConstraintSet()
+
+    init {
+        id = name
+    }
+
+    private fun <T : View> addElem(elem: T, init: T.() -> Unit) {
+        addView(elem)
+        bounds.clone(this)
+        elem.init()
+        bounds.applyTo(this)
+    }
+
+    fun textView(id: Int, init: MyTextView.() -> Unit) = addElem(MyTextView(act, id, bounds, k), init)
+
+    fun number(id: Int, init: MyNumber.() -> Unit) = addElem(MyNumber(act, id, bounds, k), init)
+
+    fun button(id: Int, init: MyButton.() -> Unit) = addElem(MyButton(act, id, bounds, k), init)
+}
+
+class MyTextView(act: AppCompatActivity, name: Int, private val bounds: ConstraintSet, private val k: Float) : TextView(act) {
+    init {
+        id = name
+        width = dp(195)
+        height = dp(42)
+        textSize = 22f
+        textAlignment = View.TEXT_ALIGNMENT_VIEW_END
+        setTextAppearance(act, R.style.Base_TextAppearance_AppCompat_Large)
+    }
+
+    fun dp(x: Int): Int = (k * x).toInt()
+
+    fun leftMargin(other: Int, side: Int, dist: Int) {
+        bounds.connect(id, ConstraintSet.START, other, side, dist)
+    }
+
+    fun rightMargin(other: Int, side: Int, dist: Int) {
+        bounds.connect(id, ConstraintSet.END, other, side, dist)
+    }
+
+    fun topMargin(other: Int, side: Int, dist: Int) {
+        bounds.connect(id, ConstraintSet.TOP, other, side, dist)
+    }
+
+    fun bottomMargin(other: Int, side: Int, dist: Int) {
+        bounds.connect(id, ConstraintSet.BOTTOM, other, side, dist)
+    }
+
+    fun center(layoutId: Int) {
+        leftMargin(layoutId, ConstraintSet.START, 8)
+        rightMargin(layoutId, ConstraintSet.END, 8)
+    }
+}
+
+class MyNumber(act: AppCompatActivity, name: Int, private val bounds: ConstraintSet, private val k: Float) : EditText(act) {
+    init {
+        id = name
+        layoutParams = ViewGroup.LayoutParams(dp(137), dp(42))
+        textSize = 20f
+        inputType = InputType.TYPE_CLASS_NUMBER
+        textAlignment = View.TEXT_ALIGNMENT_CENTER
+    }
+
+    fun dp(x: Int): Int = (k * x).toInt()
+
+    fun leftMargin(other: Int, side: Int, dist: Int) {
+        bounds.connect(id, ConstraintSet.START, other, side, dist)
+    }
+
+    fun rightMargin(other: Int, side: Int, dist: Int) {
+        bounds.connect(id, ConstraintSet.END, other, side, dist)
+    }
+
+    fun topMargin(other: Int, side: Int, dist: Int) {
+        bounds.connect(id, ConstraintSet.TOP, other, side, dist)
+    }
+
+    fun bottomMargin(other: Int, side: Int, dist: Int) {
+        bounds.connect(id, ConstraintSet.BOTTOM, other, side, dist)
+    }
+
+    fun center(layoutId: Int) {
+        leftMargin(layoutId, ConstraintSet.START, 8)
+        rightMargin(layoutId, ConstraintSet.END, 8)
+    }
+}
+
+class MyButton(act: AppCompatActivity, name: Int, val bounds: ConstraintSet, private val k: Float) : Button(act) {
+    init {
+        id = name
+        width = dp(115)
+        height = dp(42)
+
+    }
+
+    fun dp(x: Int): Int = (k * x).toInt()
+
+    fun leftMargin(other: Int, side: Int, dist: Int) {
+        bounds.connect(id, ConstraintSet.START, other, side, dist)
+    }
+
+    fun rightMargin(other: Int, side: Int, dist: Int) {
+        bounds.connect(id, ConstraintSet.END, other, side, dist)
+    }
+
+    fun topMargin(other: Int, side: Int, dist: Int) {
+        bounds.connect(id, ConstraintSet.TOP, other, side, dist)
+    }
+
+    fun bottomMargin(other: Int, side: Int, dist: Int) {
+        bounds.connect(id, ConstraintSet.BOTTOM, other, side, dist)
+    }
+
+    fun onCLick(init: () -> Unit) {
+        setOnClickListener { init() }
+    }
+}
+
+fun AppCompatActivity.constraintLayout(name: Int, init: MyConstraintLayout.() -> Unit): MyConstraintLayout {
+    val layout = MyConstraintLayout(this, name)
+    layout.init()
+    return layout
+}
