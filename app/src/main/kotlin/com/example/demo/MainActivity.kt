b3a8ab1069d8e7f31a86e953b13facb699c73a87
Greg Pevnev <gpevnev@gmail.com>
2017-12-18 18:46:31 +0300

    Added Linear layout builder

--- app/src/main/kotlin/com/example/demo/MainActivity.kt
+++ app/src/main/kotlin/com/example/demo/MainActivity.kt
@@ -1,29 +1,76 @@
 package com.example.demo
 
 import android.content.res.Configuration
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
 import android.text.InputType
 import android.view.View
 import android.widget.EditText
+import android.widget.LinearLayout
 import hw1.splay.splaySetOf
-import kotlinx.android.synthetic.main.activity_main.*
 import org.jetbrains.anko.alert
 import org.jetbrains.anko.cancelButton
 import org.jetbrains.anko.toast
 import org.jetbrains.anko.yesButton
 
-class MainActivity : AppCompatActivity() {
-    val splaySet = splaySetOf<Int>()
+
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+    private val splaySet = splaySetOf<Int>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(
+                linearLayout {
+                    layoutParams = LinearLayout.LayoutParams(
+                            LinearLayout.LayoutParams.MATCH_PARENT,
+                            LinearLayout.LayoutParams.MATCH_PARENT
+                    )
+
+                    orientation = LinearLayout.VERTICAL
+
+
+                    val layoutParams =
+                        LinearLayout.LayoutParams(
+                            LinearLayout.LayoutParams.MATCH_PARENT,
+                            LinearLayout.LayoutParams.WRAP_CONTENT,
+                            1F
+                        ).apply {
+                            setMargins(16.toDp(), 16.toDp(), 16.toDp(), 16.toDp())
+                        }
+
+                    textView(text = resources.getText(R.string.greeting)) {
+                        setTextColor(resources.getColor(R.color.colorPrimaryDark))
+                        textSize = 36F
+                    }
+
+                    button(
+                            id = R.id.button_add,
+                            text = resources.getText(R.string.add_number),
+                            onClickListener = this@MainActivity
+                    )
+
+                    button(
+                            id = R.id.button_contains,
+                            text = resources.getText(R.string.contains_number),
+                            onClickListener = this@MainActivity
+                    )
+
+                    button(
+                            id = R.id.button_remove,
+                            text = resources.getText(R.string.remove_number),
+                            onClickListener = this@MainActivity
+                    )
+
+                    forEachView {
+                        this.layoutParams = layoutParams
+                    }
+                }
+        )
     }
 
-    fun onSetQueryClick(view: View) {
+    override fun onClick(view: View) {
         when (view.id) {
-            button_add.id -> {
+            R.id.button_add -> {
                 alert {
                     title = "Insert number"
 
@@ -45,7 +92,7 @@ class MainActivity : AppCompatActivity() {
                 }.show()
             }
 
-            button_remove.id -> {
+            R.id.button_remove -> {
                 alert {
                     title = "Remove number"
 
@@ -70,7 +117,7 @@ class MainActivity : AppCompatActivity() {
                 }.show()
             }
 
-            button_contains.id -> {
+            R.id.button_contains -> {
                 alert {
                     title = "Contains number"
 
