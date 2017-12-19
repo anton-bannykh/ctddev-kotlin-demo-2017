c5e09733b424b4391a9cfe1baaf27073925b87fd
Greg Pevnev <gpevnev@gmail.com>
2017-12-19 18:42:57 +0300

    A batch of features added and fixed

---
+++ app/src/main/kotlin/com/example/demo/builder/ViewGroupBuilders.kt
@@ -0,0 +1,14 @@
+package com.example.demo.builder
+
+import android.content.Context
+import android.widget.LinearLayout
+
+/**
+ * Created by greg on 19/12/2017.
+ */
+
+fun Context.linearLayout(init: LinearLayout.() -> Unit = nop): LinearLayout {
+    val linearLayout = LinearLayout(this)
+    linearLayout.init()
+    return linearLayout
+}
