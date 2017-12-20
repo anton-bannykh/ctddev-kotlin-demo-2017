2d6bef487d4a6208ab8f7b0fa94eb507777b7d76
Vons <maksim.vns@mail.com>
2017-12-20 07:53:49 +0300

    hw1 main

--- lib/src/main/kotlin/my/lib/Main.kt
+++ lib/src/main/kotlin/my/lib/Main.kt
@@ -1,17 +1,34 @@
 package my.lib
 
-fun main(args: Array<String>) {
-    println("Hello world!")
-}
+private fun prefixFunction(s: String): Array<Int> {
+    val p = Array(s.length, { 0 })
 
-fun foo() = 10
-
-fun sum(vararg ints: Int): Int {
-    var result = 0
-    for (v in ints) {
-        result += v
+    for (i in 1 until s.length) {
+        var k = p[i - 1]
+        while (k > 0 && s[i] != s[k])
+            k = p[k - 1]
+        if (s[i] == s[k])
+            k++
+        p[i] = k
     }
-    return result
+
+    return p;
 }
 
-fun sumFun(vararg ints: Int) = ints.fold(0) { acc, i -> acc + i }
+fun substringSearch(s1: String, s2: String): ArrayList<Int> {
+    val answer = ArrayList<Int>()
+    if(s1.isEmpty()||s2.isEmpty()) return answer
+
+    val sLength = s1.length
+
+    val s = s1 + "#" + s2
+    val p = prefixFunction(s)
+
+    for (i in sLength until p.size) {
+        if (p[i] == sLength) {
+            answer.add(i - 2 * sLength)
+        }
+    }
+
+    return answer
+}
