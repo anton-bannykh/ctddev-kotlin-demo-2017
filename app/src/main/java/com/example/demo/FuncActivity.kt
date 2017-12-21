22b0d916e6731c352e62596d0cff5ceed98c7789
Artem <atem199700@gmail.com>
2017-12-21 21:30:46 +0300

    Add My_DSL

--- app/src/main/java/com/example/demo/FuncActivity.kt
+++ app/src/main/java/com/example/demo/FuncActivity.kt
@@ -1,10 +1,8 @@
 package com.example.demo
 
 import android.os.Bundle
-import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity
 import android.view.View
-import android.widget.TextView
 import my.lib.SegmentTree
 
 import kotlinx.android.synthetic.main.activity_func.*
