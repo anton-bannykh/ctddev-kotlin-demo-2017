18d7adf81d46ead0d44179423811d129d026b8dc
Artem <atem199700@gmail.com>
2017-12-24 21:37:00 +0300

    Add Exceptions

--- app/src/main/java/com/example/demo/FuncActivity.kt
+++ app/src/main/java/com/example/demo/FuncActivity.kt
@@ -3,6 +3,7 @@ package com.example.demo
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.view.View
+import android.widget.Toast
 import my.lib.SegmentTree
 
 import kotlinx.android.synthetic.main.activity_func.*
@@ -29,31 +30,65 @@ class FuncActivity : AppCompatActivity() {
         TREE = SegmentTree(arr)
     }
 
-    fun AddElem (v: View) {
-        val index = editAddInd.text.toString().toInt()
-        val value = editAddVal.text.toString().toInt()
-        TREE.add(index, value)
-        editAddInd.setText("")
-        editAddVal.setText("")
+    fun Check(index: String, value: String): Boolean {
+        var f = true
+        var _index = -1
+        try {
+            _index = index.toInt()
+        } catch (e: Exception) {
+            Toast.makeText(this, "!Wrong Index!", Toast.LENGTH_LONG).show()
+            f = false
+        }
+        if (_index < 0 || _index >= SIZE) {
+            Toast.makeText(this, "!Wrong Index!", Toast.LENGTH_LONG).show()
+            f = false
+        }
+        try {
+            value.toInt()
+        } catch (e: Exception) {
+            Toast.makeText(this, "!Wrong value!", Toast.LENGTH_LONG).show()
+            f = false
+        }
+        return f
     }
 
-    fun SetElem (v: View) {
-        val index = editSetInd.text.toString().toInt()
-        val value = editSetVal.text.toString().toInt()
-        TREE.set(index, value)
-        editSetInd.setText("")
-        editSetVal.setText("")
+    fun AddElem(v: View) {
+        val _index = editAddInd.text.toString()
+        val _value = editAddVal.text.toString()
+        if (Check(_index, _value)) {
+            val index = _index.toInt()
+            val value = _value.toInt()
+            TREE.add(index, value)
+            editAddInd.setText("")
+            editAddVal.setText("")
+        }
     }
 
-    fun GetRes (v: View) {
-        val left = editGetSt.text.toString().toInt()
-        val right = editGetFn.text.toString().toInt()
-        ViewAns.text = TREE.get(left, right).toString()
-        editGetFn.setText("")
-        editGetSt.setText("")
+    fun SetElem(v: View) {
+        val _index = editSetInd.text.toString()
+        val _value = editSetVal.text.toString()
+        if (Check(_index, _value)) {
+            val index = _index.toInt()
+            val value = _value.toInt()
+            TREE.set(index, value)
+            editAddInd.setText("")
+            editAddVal.setText("")
+        }
     }
 
-    fun Close (v: View) {
+    fun GetRes(v: View) {
+        try {
+            val left = editGetSt.text.toString().toInt()
+            val right = editGetFn.text.toString().toInt()
+            ViewAns.text = TREE.get(left, right).toString()
+            editGetFn.setText("")
+            editGetSt.setText("")
+        } catch (e: Exception) {
+            Toast.makeText(this, "Try to use index in: [0...$SIZE]" , Toast.LENGTH_LONG).show()
+        }
+    }
+
+    fun Close(v: View) {
         finish()
     }
 }
