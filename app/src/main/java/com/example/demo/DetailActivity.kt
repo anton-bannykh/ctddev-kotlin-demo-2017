4c228c5a78a5aff67b4d16d0a975825cf28735f9
Yury Bandarchuk <yury.bandarchuk@gmail.com>
2017-12-24 22:28:25 +0300

    Generate XML from code through DSL

--- app/src/main/java/com/example/demo/DetailActivity.kt
+++ app/src/main/java/com/example/demo/DetailActivity.kt
@@ -1,7 +1,11 @@
 package com.example.demo
 
+import android.graphics.Typeface
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.constraint.ConstraintSet
+import android.util.TypedValue
+import android.view.Gravity
 import android.widget.TextView
 import my.lib.Graph
 
@@ -9,7 +13,33 @@ class DetailActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_details)
+        setContentView(
+            myContraintLayout(R.id.detailsLayoutId) {
+                textView(R.id.headTextView2) {
+                    width = dp(366)
+                    height = dp(50)
+                    text = resources.getText(R.string.result_of_scc_algorithm)
+                    gravity = Gravity.CENTER
+                    setTextColor(R.style.niceBlue)
+                    setTextSize(TypedValue.COMPLEX_UNIT_SP, 20.0f)
+                    setTypeface(null, Typeface.BOLD)
+                    leftMargin(ConstraintSet.LEFT, dp(8))
+                    rightMargin(ConstraintSet.RIGHT, dp(8))
+                    topMargin(ConstraintSet.TOP, dp(8))
+                }
+                textView(R.id.resultTextView) {
+                    width = dp(337)
+                    height = dp(391)
+                    maxLines = 10000000
+                    setTextSize(TypedValue.COMPLEX_UNIT_SP, 17.0f)
+                    setTextColor(R.style.anotherNiceBlue)
+                    leftMargin(ConstraintSet.LEFT, dp(8))
+                    rightMargin(ConstraintSet.RIGHT, dp(8))
+                    topMargin(ConstraintSet.BOTTOM, R.id.headTextView2, dp(8))
+                    bottomMargin(ConstraintSet.BOTTOM, dp(32))
+                }
+            }
+        )
 
         val graph = intent.getSerializableExtra("graph") as Graph
 
@@ -27,4 +57,6 @@ class DetailActivity : AppCompatActivity() {
 
         resultTextView.text = stringBuilder.toString()
     }
+
+    private fun dp(value: Int) = (this.applicationContext.resources.displayMetrics.density * value).toInt()
 }
--- app/src/main/java/com/example/demo/MainActivity.kt
+++ app/src/main/java/com/example/demo/MainActivity.kt
@@ -5,6 +5,12 @@ import android.os.Bundle
 import android.widget.EditText
 import my.lib.Graph
 import android.content.Intent
+import android.graphics.Typeface
+import android.support.constraint.ConstraintSet
+import android.text.InputType
+import android.util.TypedValue
+import android.view.Gravity
+import android.view.ViewGroup
 import android.widget.Button
 import android.widget.Toast
 
@@ -14,7 +20,102 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(
+                myContraintLayout(R.id.layoutId) {
+                    textView(R.id.headerTextView1) {
+                        width = dp(366)
+                        height = dp(50)
+                        text = resources.getText(R.string.headerText)
+                        leftMargin(ConstraintSet.LEFT, dp(8))
+                        rightMargin(ConstraintSet.RIGHT, dp(8))
+                        topMargin(ConstraintSet.TOP, dp(8))
+                        setTextColor(R.style.niceBlue)
+                        setTextSize(TypedValue.COMPLEX_UNIT_SP, 20.0f)
+                        setTypeface(null, Typeface.BOLD)
+                        gravity = Gravity.CENTER
+                    }
+                    textView(R.id.enterVerticesLabel) {
+                        width = dp(368)
+                        height = dp(28)
+                        leftMargin(ConstraintSet.LEFT, dp(8))
+                        rightMargin(ConstraintSet.RIGHT, dp(8))
+                        topMargin(ConstraintSet.BOTTOM, R.id.headerTextView1, dp(8))
+                        setTextColor(R.style.anotherNiceBlue)
+                        setTextSize(TypedValue.COMPLEX_UNIT_SP, 17.0f)
+                        text = resources.getText(R.string.amountOfVersInGraph)
+                        gravity = Gravity.CENTER
+                    }
+                   editText(R.id.vertexAmount) {
+                        width = dp(230)
+                        height = dp(38)
+                        setEms(10)
+                        leftMargin(ConstraintSet.LEFT, R.id.enterVerticesLabel, dp(8))
+                        rightMargin(ConstraintSet.RIGHT, R.id.enterVerticesLabel, dp(8))
+                        topMargin(ConstraintSet.BOTTOM, R.id.enterVerticesLabel, dp(8))
+                        inputType = InputType.TYPE_CLASS_NUMBER
+                        gravity = Gravity.CENTER
+                    }
+                    button(R.id.button) {
+                        width = dp(130)
+                        height = dp(40)
+                        text = resources.getText(R.string.create_graph)
+                        leftMargin(ConstraintSet.LEFT, R.id.vertexAmount, dp(8))
+                        rightMargin(ConstraintSet.RIGHT, R.id.vertexAmount, dp(8))
+                        topMargin(ConstraintSet.BOTTOM, R.id.vertexAmount, dp(8))
+                        setBackgroundColor(R.style.anotherNiceBlue)
+                    }
+                    textView(R.id.textView2) {
+                        val params = layoutParams
+                        params.height = ViewGroup.LayoutParams.WRAP_CONTENT
+                        params.width = ViewGroup.LayoutParams.WRAP_CONTENT
+                        layoutParams = params
+                        text = resources.getText(R.string.enter_vertices_numbers)
+                        gravity = Gravity.CENTER
+                        setTextColor(R.style.anotherNiceBlue)
+                        setTextSize(TypedValue.COMPLEX_UNIT_SP, 17.0f)
+                        leftMargin(ConstraintSet.LEFT, dp(8))
+                        rightMargin(ConstraintSet.RIGHT, dp(8))
+                        topMargin(ConstraintSet.BOTTOM, R.id.button, dp(8))
+                    }
+                    editText(R.id.vertexU) {
+                        width = dp(168)
+                        height = dp(50)
+                        setEms(10)
+                        setHint(R.string.number_of_vertex_u)
+                        inputType = InputType.TYPE_CLASS_NUMBER
+                        leftMargin(ConstraintSet.LEFT, R.id.textView2, dp(8))
+                        topMargin(ConstraintSet.BOTTOM, R.id.textView2, dp(8))
+                    }
+                    editText(R.id.vertexV) {
+                        width = dp(186)
+                        height = dp(51)
+                        setEms(10)
+                        setHint(R.string.number_of_vertex_v)
+                        inputType = InputType.TYPE_CLASS_NUMBER
+                        leftMargin(ConstraintSet.RIGHT, R.id.textView2, dp(8))
+                        rightMargin(ConstraintSet.RIGHT, R.id.vertexU,  dp(8))
+                        topMargin(ConstraintSet.BOTTOM, R.id.textView2, dp(8))
+                    }
+                    button(R.id.addEdgeButton) {
+                        width = dp(236)
+                        height = dp(37)
+                        text = resources.getText(R.string.add_edge)
+                        setBackgroundColor(R.style.anotherNiceBlue)
+                        leftMargin(ConstraintSet.LEFT, dp(8))
+                        rightMargin(ConstraintSet.RIGHT,  dp(8))
+                        topMargin(ConstraintSet.BOTTOM, R.id.vertexU, dp(8))
+                    }
+                    button(R.id.runAlgorithmButton) {
+                        width = dp(252)
+                        height = dp(48)
+                        text = resources.getText(R.string.run_algorithm)
+                        setBackgroundColor(R.style.anotherNiceBlue)
+                        leftMargin(ConstraintSet.LEFT, dp(8))
+                        rightMargin(ConstraintSet.RIGHT,  dp(8))
+                        topMargin(ConstraintSet.BOTTOM, R.id.addEdgeButton, dp(32))
+                    }
+                }
+        )
 
         val vertexUTextField = findViewById<EditText>(R.id.vertexU)
         val vertexVTextField = findViewById<EditText>(R.id.vertexV)
@@ -48,6 +149,8 @@ class MainActivity : AppCompatActivity() {
             runAlgorithm()
         }
     }
+
+    private fun dp(value: Int) = (this.applicationContext.resources.displayMetrics.density * value).toInt()
 
     private fun runAlgorithm() {
         val intent = Intent(this, DetailActivity::class.java)
