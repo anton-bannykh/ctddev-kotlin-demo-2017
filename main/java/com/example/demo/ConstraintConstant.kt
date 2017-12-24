15cbb582633cbd896ba3f7da1b61f04a72cf4da9
Duginets Nikita <duginetsn98@gmail.com>
2017-12-24 19:56:25 +0300

    hw3

---
+++ main/java/com/example/demo/ConstraintConstant.kt
@@ -0,0 +1,11 @@
+package com.example.demo
+
+import android.support.constraint.ConstraintSet
+
+/**
+ * Created by Python on 24.12.2017.
+ */
+val LEFT = ConstraintSet.START
+val RIGHT = ConstraintSet.END
+val TOP = ConstraintSet.TOP
+val BOTTOM = ConstraintSet.BOTTOM
