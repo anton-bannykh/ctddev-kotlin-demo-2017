807a6c165a1129c194d73d0fbeee0a708b97dc20
Daria Zvyagintseva <daryazvyaginceva@yandex.ru>
2017-12-25 17:04:45 +0300

    Update MainActivity.kt

--- app/src/main/java/com/example/demo/MainActivity.kt
+++ app/src/main/java/com/example/demo/MainActivity.kt
@@ -1,39 +1,11 @@
 package com.example.demo
 
-
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
-import android.widget.EditText
 import android.widget.TextView
 
+
 class MainActivity : AppCompatActivity() {
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-    }
-
-    fun сlickButton(view: View) {
-        var editText1: EditText = findViewById(R.id.editText3)
-        var textView: TextView = findViewById(R.id.textView3)
-        var a = mutableListOf<Int>()
-        val parts = (editText1.getText().toString()).split("\u0020")
-        for (j in parts.indices) {
-            if (parts[j].compareTo("") > 0) {
-                a.add(Integer.parseInt(parts[j]))
-            }
-        }
-        val arr = IntArray(a.size);
-        for (j in a.indices) arr[j] = a[j];
-        mergeSort(arr);
-        var ans = "Sorted: ";
-        for (j in arr.indices) {
-            ans += arr[j].toString() + " ";
-        }
-        textView.text = ans;
-    }
-
     fun merge(a: IntArray, l: Int, L: Int, r: Int, R: Int) {
         var i = l;
         val i0 = L;
@@ -78,5 +50,59 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-}
+    var TextView.value: IntArray
+        get() {
+            var a = mutableListOf<Int>()
+            val parts = (text.toString()).split("\u0020")
+            for (j in parts.indices) {
+                if (parts[j].compareTo("") > 0) {
+                    a.add(Integer.parseInt(parts[j]))
+                }
+            }
+            val arr = IntArray(a.size);
+            for (j in a.indices) arr[j] = a[j];
+            return arr;
+        }
+        set(x) {
+            var res = "Sorted: ";
+            for (j in x.indices) {
+                res += x[j].toString() + " ";
+            }
+            text = res
+        }
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(
+                constraintLayout(R.id.layoutId) {
+                    textView(R.id.hello) {
+                        text = "I can sort your arrays!"
+                        topMargin(R.id.layoutId, TOP, dp(24))
+                    }
+                    textView(R.id.inArrayText) {
+                        text = "Enter values separated by a space:"
+                        topMargin(R.id.hello, TOP, dp(24))
+                    }
+
+                    val initArray = number(R.id.inputArray) {
+                        topMargin(R.id.inArrayText, TOP, dp(50))
+                    }
+
+                    val ans = number(R.id.outputArray) {
+                        topMargin(R.id.sortButton, BOTTOM, dp(40))
+                    }
+
+                    button(R.id.sortButton) {
+                        topMargin(R.id.inputArray, BOTTOM, dp(32))
+                        center(R.id.layoutId)
+                        text = "Sort!"
+                        onCLick {
+                            val a = initArray.value;
+                            mergeSort(a);
+                            ans.value = a;
+                        }
+                    }
+                }
+        )
+    }
+}
