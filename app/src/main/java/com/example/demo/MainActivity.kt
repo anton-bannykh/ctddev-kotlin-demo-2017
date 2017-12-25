b7378c4d9fd919510b2112cada0de4d4e28b5589
unknown <bloomashka@mail.ru>
2017-12-25 22:35:59 +0300

    HW3

--- app/src/main/java/com/example/demo/MainActivity.kt
+++ app/src/main/java/com/example/demo/MainActivity.kt
@@ -2,6 +2,10 @@ package com.example.demo
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.ViewGroup.LayoutParams.MATCH_PARENT
+import android.view.ViewGroup.LayoutParams.WRAP_CONTENT
+import android.widget.LinearLayout
 import kotlinx.android.synthetic.main.activity_main.*
 import my.lib.lcs
 
@@ -9,10 +13,36 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        button.setOnClickListener {
-            answer.text = lcs(first.text.toString().toList(), second.text.toString().toList()).toString()
+                val linear = linearLayout {
+            orientation = LinearLayout.VERTICAL
+            setWidth(MATCH_PARENT)
+            setHeight(WRAP_CONTENT)
+
+            editText {
+                id = R.id.first
+                hint = getString(R.string.enter_the_first_sequence)
+            }
+
+            editText {
+                id = R.id.second
+                hint = getString(R.string.enter_the_second_sequence)
+            }
+
+            button {
+                id = R.id.button
+                text = getString(R.string.go)
+                onClick = View.OnClickListener {
+                    answer.text = lcs(first.text.toString().toList(), second.text.toString().toList()).toString()
+
+                }
+            }
+
+            textView {
+                id = R.id.answer
+            }
         }
+        setContentView(linear.build(this))
+
     }
 
 
