94909337ab9bff1fb250f7dacb695de51909f39c
horoyami <34141926+horoyami@users.noreply.github.com>
2017-12-26 02:03:59 +0300

    Add files via upload

--- app/src/main/java/com/example/demo/MainActivity.kt
+++ app/src/main/java/com/example/demo/MainActivity.kt
@@ -2,14 +2,27 @@ package com.example.demo
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import my.lib.sumFun
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(constraintLayout(R.id.globalLay) {
+
+            textView(R.id.text) {
+
+                text = "Вы дебилы"
+
+            }
+
+            button(R.id.but) {
+                text = "Нажать!"
+                onCLick {
+                    text = "ЕЩЕ!!!"
+                }
+            }
+
+        })
     }
 
-    fun test() = sumFun(1, 2, 3)
 }
