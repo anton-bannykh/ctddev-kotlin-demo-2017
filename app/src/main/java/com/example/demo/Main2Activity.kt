4d5665b0b01b6f1ecad3f0e6e778e87c5bde52ca
Alexandr Kravchenko <ratatuy.1995@mail.ru>
2017-12-27 00:35:27 +0300

    Final decision HW3

--- app/src/main/java/com/example/demo/Main2Activity.kt
+++ app/src/main/java/com/example/demo/Main2Activity.kt
@@ -1,8 +1,9 @@
 package com.example.demo
 
 import android.annotation.SuppressLint
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.widget.LinearLayout
 import android.widget.TextView
 import my.lib.Quicksort
 
@@ -11,13 +12,23 @@ class Main2Activity : AppCompatActivity() {
     @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main2)
+        //setContentView(R.layout.activity_main2)
+        val linearL: LinearLayout = LinearLayout(this)
+        linearL.orientation = LinearLayout.VERTICAL
+        val lPar: LinearLayout.LayoutParams = LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT)
+        setContentView(linearL, lPar)
+        val ed = TextView(this)
+        linearL.addView(ed)
+        val out = TextView(this)
+        linearL.addView(out)
 
-        val ed = findViewById<TextView>(R.id.ed)
+        //val ed = findViewById<TextView>(R.id.ed)
         val num = intent.getIntegerArrayListExtra("NUM_ARR").toIntArray()
         ed.text = "Исходный массив: " + num.contentToString()
         Quicksort(num)
-        val out = findViewById<TextView>(R.id.vivod)
+        //val out = findViewById<TextView>(R.id.vivod)
         out.text = "Сортированный массив: " + num.contentToString()
+
     }
 }
--- app/src/main/java/com/example/demo/MainActivity.kt
+++ app/src/main/java/com/example/demo/MainActivity.kt
@@ -3,16 +3,48 @@ package com.example.demo
 import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.text.InputType
 import android.view.View
+import android.widget.Button
 import android.widget.EditText
+import android.widget.LinearLayout
 
 class MainActivity : AppCompatActivity() {
 
     lateinit var text: EditText
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        text = findViewById<EditText>(R.id.input)
+        // setContentView(R.layout.activity_main)
+        //text = findViewById<EditText>(R.id.input)
+        val linearL: LinearLayout = LinearLayout(this)
+        linearL.orientation = LinearLayout.VERTICAL
+        val lPar: LinearLayout.LayoutParams = LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT)
+        setContentView(linearL, lPar)
+
+
+
+        text = EditText(this)
+        text.setInputType(InputType.TYPE_CLASS_PHONE)
+        linearL.addView(text)
+        val btn = Button(this)
+        btn.setText("GO")
+        btn.setOnClickListener {
+            val intent = Intent(this, Main2Activity::class.java)
+            val numberArr = text.getText().toString().split(" ")
+            val num: ArrayList<Int> = ArrayList()
+            for (i in numberArr) {
+                if (!i.isEmpty()) {
+                    num.add(i.trim().toInt())
+                }
+            }
+            intent.putExtra("NUM_ARR", num)
+            startActivity(intent)
+
+        }
+        linearL.addView(btn)
+
+
     }
 
     fun go(v: View) {
