66e8a060ce339f57c966d86811b0704602491c5f
hangedplane <hangedplane@gmail.com>
2017-12-27 02:36:52 +0300

    create DSL

--- app/src/main/java/com/example/demo/MainActivity.kt
+++ app/src/main/java/com/example/demo/MainActivity.kt
@@ -2,11 +2,12 @@ package com.example.demo
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
-import android.view.View
+import android.view.Gravity
 import android.widget.EditText
+import android.widget.RelativeLayout
 import android.widget.TextView
+import com.example.demo.UI.*
 import my.lib.huffman.HuffmanEncode
-import my.lib.sumFun
 
 class MainActivity : AppCompatActivity() {
 
@@ -17,32 +18,86 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(
+                relativeLayout {
+                    initRelativeLayoutParams {
+                        width = MATCH_PARENT
+                        height = MATCH_PARENT
 
-        mEditText = findViewById(R.id.et_encode_text)
+                        setMargins(15, 15, 15, 15)
+                    }
 
-        mCodeText = findViewById(R.id.tv_code)
+                    scrollView {
+                        initRelativeLayoutParams {
+                            width = MATCH_PARENT
+                            height = WRAP_CONTENT
+
+                            gravity = Gravity.BOTTOM or Gravity.CENTER
+
+                            addRule(RelativeLayout.ALIGN_PARENT_TOP)
+                            addRule(RelativeLayout.ABOVE, R.id.encode_string_layout)
+                        }
+
+                        mCodeText = textView(R.id.tv_code) {
+                            initRelativeLayoutParams {
+                                width = MATCH_PARENT
+                                height = WRAP_CONTENT
+                            }
+
+                            textSize = 25F
+                        }
+                    }
+
+                    linearLayout(R.id.encode_string_layout) {
+                        initRelativeLayoutParams {
+                            width = MATCH_PARENT
+                            height = WRAP_CONTENT
+                            addRule(RelativeLayout.ALIGN_PARENT_BOTTOM)
+                        }
+
+                        mEditText = editText(R.id.et_encode_text) {
+                            initLinearLayoutParams {
+                                width = WRAP_CONTENT
+                                height = MATCH_PARENT
+                                weight = 9F
+                            }
+
+                            hint = getString(R.string.et_hint)
+                            textSize = 15F
+                        }
+
+                        button(R.id.button_encode) {
+                            initLinearLayoutParams {
+                                width = WRAP_CONTENT
+                                height = MATCH_PARENT
+                                weight = 1F
+                            }
+
+                            text = getString(R.string.encode_command)
+                            textSize = 12F
+
+                            this.setOnClickListener({
+                                val stringToEncode = mEditText.text.toString()
+
+                                if (stringToEncode != "") {
+                                    mCodeText.text = buildString {
+                                        append(getString(R.string.input_string_text))
+                                        append(" '$stringToEncode'\n")
+
+                                        append(getString(R.string.code_text))
+                                        append(" '${encoder.huffmanEncode(stringToEncode)}'")
+                                        append("\n\n\n")
+
+                                        append(getString(R.string.digits_info))
+                                        append(encoder.digitsCode)
+                                    }
+                                } else {
+                                    mCodeText.text = getString(R.string.input_string_text)
+                                }
+                            })
+                        }
+                    }
+                }
+        )
     }
-
-    fun onClick(v : View) {
-        val stringToEncode = mEditText.text.toString()
-
-        if (stringToEncode != "") {
-            mCodeText.text = buildString {
-                append(getString(R.string.input_string_text))
-                append(" '$stringToEncode'\n")
-
-                append(getString(R.string.code_text))
-                append(" '${encoder.huffmanEncode(stringToEncode)}'")
-                append("\n\n\n")
-
-                append(getString(R.string.digits_info))
-                append(encoder.digitsCode)
-            }
-        } else {
-            mCodeText.text = getString(R.string.input_string_text)
-        }
-    }
-
-    fun test() = sumFun(1, 2, 3)
 }
---
+++ app/src/main/java/com/example/demo/UI/buildUI.kt
@@ -0,0 +1,36 @@
+package com.example.demo.UI
+
+import android.content.Context
+import android.view.View
+import android.view.ViewGroup
+import android.widget.*
+
+/**
+ * Created by Sergey on 24.12.2017.
+ */
+
+fun Context.relativeLayout(id: Int? = null, init: RelativeLayout.() -> Unit) = RelativeLayout(this).apply {
+    if (id != null) {
+        this.id = id
+    }
+}.apply(init)
+
+fun <T: View> ViewGroup.initCustomView(view: T, id: Int?, init : T.() -> Unit): T {
+    view.init()
+    if (id != null) {
+        view.id = id
+    }
+    this.addView(view)
+    return view
+}
+
+fun ViewGroup.linearLayout(id: Int? = null, init: LinearLayout.() -> Unit) = initCustomView(LinearLayout(context), id, init)
+
+fun ViewGroup.scrollView(id: Int? = null, init: ScrollView.() -> Unit) = initCustomView(ScrollView(context), id, init)
+
+fun ViewGroup.textView(id: Int, init: TextView.() -> Unit) = initCustomView(TextView(context), id, init)
+
+fun ViewGroup.editText(id: Int, init: EditText.() -> Unit) = initCustomView(EditText(context), id, init)
+
+fun ViewGroup.button(id: Int, init: Button.() -> Unit) = initCustomView(Button(context), id, init)
+
