8333ac733f704c6aad7983359ccf0f6011a4e157
Paul1298 <poul1298@gmail.com>
2017-12-26 20:02:48 +0300

    add first homework

--- lib/src/main/kotlin/my/lib/Main.kt
+++ lib/src/main/kotlin/my/lib/Main.kt
@@ -1,17 +1,72 @@
 package my.lib
 
-fun main(args: Array<String>) {
-    println("Hello world!")
-}
+import kotlin.collections.ArrayList
+import java.util.TreeSet
 
-fun foo() = 10
+val INF = 1000000009
 
-fun sum(vararg ints: Int): Int {
-    var result = 0
-    for (v in ints) {
-        result += v
+class Graph {
+    class Edge(val from: Int, val to: Int, val weight: Int)
+    class Vertex(val num: Int) : Comparable<Vertex> {
+        var key: Int = INF
+        override fun compareTo(other: Vertex) = key.compareTo(other.key)
     }
-    return result
+
+    var graph: ArrayList<MutableList<Edge> > = ArrayList()
+    var vertices: ArrayList<Vertex> = ArrayList()
+    var vertexCount: Int = 0
+    fun addEdge(from: Int, to: Int, weight: Int) {
+        graph[from - 1].add(Edge(from - 1, to - 1, weight))
+        graph[to - 1].add(Edge(to - 1, from - 1, weight))
+    }
+
+    fun setSize(size: Int) {
+        vertexCount = size
+        graph.ensureCapacity(size)
+        vertices.ensureCapacity(size)
+        for (i in 0..vertexCount) {
+            graph.add(mutableListOf())
+            vertices.add(Vertex(i))
+        }
+    }
+
+    fun getMST(): List<Pair<Int, Int> > {
+        val p = IntArray(vertexCount, { _ -> -1 })
+        val isInQueue = BooleanArray(vertexCount, { _ -> true })
+        vertices[0].key = 0
+        val q = TreeSet<Vertex>()
+        for (i in 0..vertexCount - 1) {
+            q.add(vertices[i])
+        }
+        val result = mutableListOf<Pair<Int, Int> >()
+        while (!q.isEmpty()) {
+            val v = q.pollFirst().num
+            if (p[v] != -1) {
+                result.add(Pair(v, p[v]))
+            }
+            isInQueue[v] = false
+            for (e in graph[v]) {
+                if (isInQueue[e.to] && vertices[e.to].key > e.weight) {
+                    p[e.to] = v
+                    q.remove(vertices[e.to])
+                    vertices[e.to].key = e.weight
+                    q.add(vertices[e.to])
+                }
+            }
+        }
+        return result
+    }
+
+    fun answer(): String {
+        val edges = getMST().sortedWith(compareBy({ it.second }, { it.first }))
+        val sb = StringBuilder()
+        for (e in edges) {
+            sb.append(Math.min(e.first + 1, e.second + 1).toString() + " " + Math.max(e.first + 1, e.second + 1) + " ")
+        }
+        return sb.toString()
+    }
 }
 
-fun sumFun(vararg ints: Int) = ints.fold(0) { acc, i -> acc + i }
+fun main(args: Array<String>) {
+    //
+}
