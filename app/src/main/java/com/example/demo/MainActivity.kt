f209d872783e7cf4eb58a25e9655e6940c64d8d1
Kowalski1337 <33070907+Kowalski1337@users.noreply.github.com>
2017-12-26 22:05:20 +0300

    HW3

--- app/src/main/java/com/example/demo/MainActivity.kt
+++ app/src/main/java/com/example/demo/MainActivity.kt
@@ -3,10 +3,12 @@ package com.example.demo
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.view.ViewGroup.LayoutParams.WRAP_CONTENT
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
 import android.widget.Toast
+import dsl.makeLayout
 import my.lib.TwoSat
 
 
@@ -19,7 +21,53 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(
+                makeLayout(R.id.Layout) {
+
+                    editText(R.id.editText2) {
+                        topMargin(R.id.Layout, TOP, 24)
+                        leftMargin(R.id.Layout, LEFT, 8)
+                        rightMargin(R.id.Layout, RIGHT, 8)
+                        width = 1000
+                        height = 200
+                        hint = "Number of variables"
+                        textSize = 26f
+                        textAlignment = View.TEXT_ALIGNMENT_CENTER
+                    }
+
+                    editText(R.id.editText) {
+                        topMargin(R.id.editText2, BOTTOM, 32)
+                        leftMargin(R.id.Layout, LEFT, 8)
+                        rightMargin(R.id.Layout, RIGHT, 8)
+                        width = 1000
+                        height = 200
+                        hint = "Input function"
+                        textSize = 26f
+                        textAlignment = View.TEXT_ALIGNMENT_CENTER
+                    }
+
+                    button(R.id.button) {
+                        topMargin(R.id.editText, BOTTOM, 20)
+                        leftMargin(R.id.Layout, LEFT, 8)
+                        rightMargin(R.id.Layout, RIGHT, 8)
+                        width = 400
+                        height = 100
+                        text = "Solve"
+                        onClick { solve() }
+                    }
+
+                    viewText(R.id.textView2) {
+                        topMargin(R.id.button, BOTTOM, 100)
+                        bottomMargin(R.id.Layout, BOTTOM, 8)
+                        leftMargin(R.id.Layout, LEFT, 8)
+                        rightMargin(R.id.Layout, RIGHT, 8)
+                        width = 1000
+                        height = 800
+                        textSize = 26f
+                        textAlignment = View.TEXT_ALIGNMENT_CENTER
+                    }
+                }
+        )
 
         numOfVar = findViewById(R.id.editText2)
         solveButton = findViewById(R.id.button)
@@ -28,7 +76,7 @@ class MainActivity : AppCompatActivity() {
 
     }
 
-    fun solve(v: View) {
+    fun solve() {
         result.text = TwoSat().twoSat(numOfVar.text.toString().toInt(), inputText.text.toString())
     }
 
