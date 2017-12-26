576d5b85a62a2a62a6bc1248c7f5e5c3ded0fa0e
KristinaMihajlenko <34263871+KristinaMihajlenko@users.noreply.github.com>
2017-12-27 02:57:46 +0300

    HW3

--- app/src/main/java/com/example/demo/MainActivity.kt
+++ app/src/main/java/com/example/demo/MainActivity.kt
@@ -1,10 +1,11 @@
 package com.example.demo
 
+import DSL.constraintLayout
+import DSL.onCLick
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
-import android.widget.EditText
-import android.widget.TextView
+import android.support.v7.appcompat.R.id.wrap_content
+import kotlinx.android.synthetic.main.activity_main.*
 import android.widget.Toast
 import my.lib.BinarySearch
 
@@ -14,48 +15,205 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val edit1 = findViewById<EditText>(R.id.editText)
-        val edit2 = findViewById<EditText>(R.id.editText2)
-        val buttonAdd = findViewById<Button>(R.id.button)
-        val buttonAns = findViewById<Button>(R.id.button2)
-        val textArr = findViewById<TextView>(R.id.textView2)
-        val textAns = findViewById<TextView>(R.id.textView4)
-        val clear = findViewById<Button>(R.id.button3)
+        setContentView(
 
-        var clean = true
-        val arr = ArrayList<Int>()
-        buttonAdd.setOnClickListener {
-            if (clean) {
-                val x = Integer.parseInt(edit1.text.toString())
-                if (!arr.isEmpty() && arr.last() > x) {
-                    Toast.makeText(this, "Введите x побольше", Toast.LENGTH_SHORT).show()
-                    edit1.setText("")
-                } else {
-                    edit1.setText("")
-                    arr.add(x)
-                    textArr.text = textArr.text.toString().plus(" $x")
+                constraintLayout(R.id.layoutId) {
+
+                    textView(R.id.textView5) {
+
+                        topMargin(R.id.layoutId, TOP, dp(15))
+
+                        hint = "Добавить элемент массива"
+
+                        leftMargin(R.id.layoutId, LEFT, dp(8))
+
+                        rightMargin(R.id.layoutId, LEFT, dp(8))
+
+                    }
+
+                    editText(R.id.editText) {
+
+                        width = dp(150)
+
+                        topMargin(R.id.textView5, TOP, dp(43))
+
+                        leftMargin(R.id.layoutId, LEFT, dp(8))
+
+//                        rightMargin(R.id.layoutId, LEFT, dp(8))
+
+
+                    }
+
+
+                    button(R.id.button) {
+
+                        width = dp(150)
+
+                        topMargin(R.id.textView5, BOTTOM, dp(0))
+
+                        rightMargin(R.id.layoutId, RIGHT, dp(8))
+
+                        // rightMargin(R.id.layoutId, LEFT, dp(8))
+
+                        text = "ДОБАВИТЬ!"
+
+                        onCLick {
+
+                            push()
+
+                        }
+
+                    }
+
+
+                    textView(R.id.textView) {
+
+                        topMargin(R.id.editText, BOTTOM, dp(25))
+
+                        width = dp(190)
+
+                        leftMargin(R.id.layoutId, LEFT, dp(8))
+
+//                        rightMargin(R.id.layoutId, LEFT, dp(8))
+
+                        text = "Массив:"
+
+                    }
+
+
+                    textView(R.id.textView2) {
+
+//                        width = dp(200)
+                        height = dp(170)
+
+                        topMargin(R.id.textView, BOTTOM, dp(8))
+
+                        leftMargin(R.id.layoutId, LEFT, dp(8))
+
+                        rightMargin(R.id.layoutId, LEFT, dp(8))
+
+                    }
+
+                    textView(R.id.textView3) {
+
+//                        width = dp(280)
+                        height = dp(50)
+
+                        topMargin(R.id.textView2, BOTTOM, dp(8))
+
+                        hint = "Индекс какого числа надо найти?"
+
+                        leftMargin(R.id.layoutId, LEFT, dp(8))
+
+                        //rightMargin(R.id.layoutId, LEFT, dp(8))
+
+                    }
+
+                    editText(R.id.editText2) {
+
+                        width = dp(150)
+
+                        topMargin(R.id.textView3, TOP, dp(70))
+
+                        leftMargin(R.id.layoutId, LEFT, dp(8))
+
+                    }
+
+
+
+                    button(R.id.button2) {
+
+                        width = dp(150)
+
+//                        height = dp(80)
+
+                        topMargin(R.id.textView3, BOTTOM, dp(18))
+
+                        rightMargin(R.id.layoutId, RIGHT, dp(8))
+
+                        text = "НАЙТИ!"
+
+                        onCLick {
+
+                            push2()
+
+                        }
+
+                    }
+
+                    button(R.id.button3) {
+
+                        width = dp(150)
+
+//                        height = dp(80)
+
+                        topMargin(R.id.button2, BOTTOM, dp(8))
+
+                        rightMargin(R.id.layoutId, RIGHT, dp(8))
+
+                        text = "ОЧИСТИТЬ"
+
+                        onCLick {
+
+                            push3()
+
+                        }
+
+                    }
+
+
+
+
+
+                    textView(R.id.textView4) {
+
+                        //width = dp(280)
+
+                        topMargin(R.id.editText2, BOTTOM, dp(24))
+
+                        leftMargin(R.id.layoutId, LEFT, dp(8))
+
+                        rightMargin(R.id.button3, LEFT, dp(8))
+
+                    }
                 }
-            }
-        }
+        )
+    }
 
-        buttonAns.setOnClickListener {
-            if (arr.isEmpty()) {
-                Toast.makeText(this, "Сначала введите массив", Toast.LENGTH_SHORT).show()
+    var clean = true
+    val arr = ArrayList<Int>()
+    fun push() {
+        if (clean) {
+            val x = Integer.parseInt(editText.text.toString())
+            if (!arr.isEmpty() && arr.last() > x) {
+                Toast.makeText(this, "Введите x побольше", Toast.LENGTH_SHORT).show()
+                editText.setText("")
             } else {
-                val x = Integer.parseInt(edit2.text.toString())
-                val ans = BinarySearch(arr.toIntArray(), x)
-                textAns.text = "$ans"
+                editText.setText("")
+                arr.add(x)
+                textView2.text = textView2.text.toString().plus(" $x")
             }
-            clean = false
-        }
-
-        clear.setOnClickListener {
-            clean = true
-            textAns.text = ""
-            textArr.text = ""
-            edit2.setText("")
-            arr.clear()
         }
     }
 
+    fun push2(){
+        if (arr.isEmpty()) {
+            Toast.makeText(this, "Сначала введите массив", Toast.LENGTH_SHORT).show()
+        } else {
+            val x = Integer.parseInt(editText2.text.toString())
+            val ans = BinarySearch(arr.toIntArray(), x)
+            textView4.text = "$ans"
+        }
+        clean = false
+    }
+
+    fun push3(){
+        clean = true
+        textView4.text = ""
+        textView2.text = ""
+        editText2.setText("")
+        arr.clear()
+    }
 }
+
+
