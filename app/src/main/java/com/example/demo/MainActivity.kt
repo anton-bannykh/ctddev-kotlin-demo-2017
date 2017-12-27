4cea1cfa18247dee7b221c390330885afe79ec27
Max <alvinmax@mail.ru>
2017-12-27 19:46:01 +0300

    HW2

--- app/src/main/java/com/example/demo/MainActivity.kt
+++ app/src/main/java/com/example/demo/MainActivity.kt
@@ -2,14 +2,32 @@ package com.example.demo
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import my.lib.sumFun
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import my.lib.getLIS
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-    }
 
-    fun test() = sumFun(1, 2, 3)
+        val getNumbers: EditText = findViewById(R.id.editText1)
+        val calculate: Button = findViewById(R.id.button1)
+        val result: TextView = findViewById(R.id.textView1)
+
+        calculate.setOnClickListener({
+            val b : List <String> = getNumbers.text.toString().split(" ").filter { !it.isEmpty() }
+            val a = IntArray(b.size)
+
+            for(i in 0 until b.size) {
+                a[i] = b[i].toInt()
+            }
+
+            val answer = "Ответ: " + getLIS(a).toString()
+            result.setText(answer)
+
+        })
+    }
 }
