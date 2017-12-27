d9f7d14accd95a3fc35b02355792a950b3df8fc3
Marina K <qwertyqwert97@mail.ru>
2017-12-27 11:59:13 +0300

    Update MainTest.kt

--- src/test/kotlin/MainTest.kt
+++ src/test/kotlin/MainTest.kt
@@ -3,15 +3,15 @@ import org.junit.Test
 
 class MainTest {
     @Test
-    fun testFoo() {
+    fun test23() {
         assertEquals(true, isPrime(23))
     }
     @Test
-    fun testFoo() {
+    fun test133() {
         assertEquals(false, isPrime(133))
     }
     @Test
-    fun testFoo() {
+    fun test7() {
         assertEquals(true, isPrime(7))
     }
 
