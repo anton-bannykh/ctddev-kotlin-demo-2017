899049bbe2da5c9087a386942dd69e3a6872f0b4
mikkiamirov <mikkiamirov17@gmail.com>
2017-12-27 13:00:01 +0300

    HW2

--- app/src/main/java/com/example/demo/MainActivity.kt
+++ app/src/main/java/com/example/demo/MainActivity.kt
@@ -2,8 +2,9 @@ package com.example.demo
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import my.lib.sumFun
-
+import android.view.View
+import java.util.Random
+import kotlinx.android.synthetic.main.activity_main.*
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -11,5 +12,64 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
     }
 
-    fun test() = sumFun(1, 2, 3)
+    fun isOdd(n: Int): Boolean {
+        return (n % 2 == 0)
+    }
+
+    fun pow(a: Long, t: Long, mod: Int): Long {
+        var b = a
+        var c = 1L
+        var _t = t
+        while (_t > 0) {
+            if (_t % 2 == 1L) {
+                c *= b
+                c %= mod
+            }
+            b = b * b % mod
+            _t /= 2L
+        }
+        return c
+    }
+
+    fun simpleCheck(n: Int): String {
+        when {
+            n == 2 || n == 3 -> return "Simple"
+
+            isOdd(n) -> return "NotSimple"
+            else -> {
+                loop@ for (i in 1..20) {
+                    var x: Long = n - 1L;
+                    var s = 0
+
+                    while (x % 2 == 0L) {
+                        x /= 2
+                        s++
+                    }
+                    val random = Random()
+                    var a = (random.nextInt(n - 4) + 2).toLong()
+                    a = pow(a, x, n)
+                    if (a == 1L || a == n - 1L)
+                        continue@loop
+
+                    for (j in 1..s) {
+                        a = a * a % n
+                        if (a == 1L) {
+                            return "NotSimple"
+                        }
+                        if (a == n - 1L) {
+                            continue@loop
+                        }
+                    }
+                    return "NotSimple"
+                }
+                return "Simple"
+            }
+        }
+    }
+
+    fun onClick(v: View) {
+        val x = editT.getText().toString().toInt(10)
+        answer.setText(simpleCheck(x))
+
+    }
 }
