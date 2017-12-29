ba05fc8bd6f1b1e101ae9c193281b50274de8edb
Max <alvinmax@mail.ru>
2017-12-29 04:21:33 +0300

    HW3

--- app/src/main/java/com/example/demo/MainActivity.kt
+++ app/src/main/java/com/example/demo/MainActivity.kt
@@ -1,33 +1,59 @@
 package com.example.demo
 
+import android.graphics.Paint
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
-import android.widget.EditText
-import android.widget.TextView
+import com.example.demo.dsl.constraintLayout
+import com.example.demo.dsl.onCLick
 import my.lib.getLIS
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(
+                constraintLayout(R.id.layoutId) {
+                    textView(R.id.description) {
+                        text = "Введите массив: "
+                        topConstaint(R.id.layoutId, TOP, dp(4))
+                        bottomConstaint(R.id.array, TOP, dp(100))
+                        center(R.id.layoutId)
+                    }
 
-        val getNumbers: EditText = findViewById(R.id.editText1)
-        val calculate: Button = findViewById(R.id.button1)
-        val result: TextView = findViewById(R.id.textView1)
+                    val array = editView(R.id.array) {
+                        topConstaint(R.id.description, BOTTOM, dp(100))
+                        bottomConstaint(R.id.array, TOP, dp(100))
+                        center(R.id.layoutId)
+                    }
 
-        calculate.setOnClickListener({
-            val b : List <String> = getNumbers.text.toString().split(" ").filter { !it.isEmpty() }
-            val a = IntArray(b.size)
+                    val ans = textView(R.id.answer) {
+                        topConstaint(R.id.calculate, BOTTOM, dp(100))
+                        bottomConstaint(R.id.layoutId, BOTTOM, dp(8))
+                        center(R.id.layoutId)
+                        gravity = 1
+                    }
 
-            for(i in 0 until b.size) {
-                a[i] = b[i].toInt()
-            }
+                    button(R.id.calculate) {
+                        topConstaint(R.id.array, BOTTOM, dp(100))
+                        bottomConstaint(R.id.answer, TOP, dp(100))
+                        center(R.id.layoutId)
 
-            val answer = "Ответ: " + getLIS(a).toString()
-            result.setText(answer)
+                        text = "CALCULATE"
 
-        })
+                        onCLick {
+                            val b : List<String> = array.text.toString().split(" ").filter { !it.isEmpty() }
+                            val a = IntArray(b.size)
+                            for(i in 0 until b.size) {
+                                a[i] = b[i].toInt()
+                            }
+
+                            val answer = "Ответ: " + getLIS(a).toString()
+                            ans.text = answer
+                        }
+                    }
+
+
+                }
+        )
     }
 }
