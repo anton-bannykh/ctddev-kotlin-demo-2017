68c820893b936a2fdd7f08c5dad62be5a57cb9a8
glebwin <glebwin@gmail.com>
2018-01-09 17:05:08 +0300

    Add HW3.

--- app/src/main/java/com/example/demo/MainActivity.kt
+++ app/src/main/java/com/example/demo/MainActivity.kt
@@ -4,29 +4,70 @@ import android.content.ClipData
 import android.content.ClipboardManager
 import android.content.Context
 import android.os.Bundle
+import android.support.constraint.ConstraintSet
 import android.support.v7.app.AppCompatActivity
-import android.widget.Button
-import android.widget.EditText
+import android.text.InputType
+import android.view.ViewGroup
 import quickSort
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        val sortButton = findViewById<Button>(R.id.sortButton)
-        val copyButton = findViewById<Button>(R.id.copyButton)
-        sortButton.setOnClickListener {
-            val inputEditText = findViewById<EditText>(R.id.inputEditText)
-            val outputEditText = findViewById<EditText>(R.id.outputEditText)
-            val array = ArrayList(inputEditText.text.split(" ").map { it.toInt() })
-            quickSort(array)
-            outputEditText.setText(array.joinToString(" ") { it.toString() })
-        }
-        copyButton.setOnClickListener {
-            val outputEditText = findViewById<EditText>(R.id.outputEditText)
-            val clipboard = getSystemService(Context.CLIPBOARD_SERVICE) as ClipboardManager
-            val clip = ClipData.newPlainText("sorted data", outputEditText.text)
-            clipboard.primaryClip = clip
-        }
+
+        setContentView(
+                createDemoConstraintLayout(R.id.layoutId) {
+                    val inputEditText = editText(R.id.inputEditText) {
+                        width = ViewGroup.LayoutParams.WRAP_CONTENT
+                        height = ViewGroup.LayoutParams.WRAP_CONTENT
+                        setEms(10)
+                        hint = resources.getText(R.string.input)
+                        inputType = InputType.TYPE_TEXT_VARIATION_PERSON_NAME
+                        addConstraint(ConstraintSet.BOTTOM, R.id.layoutId, ConstraintSet.BOTTOM)
+                        addConstraint(ConstraintSet.END, R.id.layoutId, ConstraintSet.END)
+                        addConstraint(ConstraintSet.START, R.id.layoutId, ConstraintSet.START)
+                        addConstraint(ConstraintSet.TOP, R.id.layoutId, ConstraintSet.TOP)
+                        setVerticalBias(0.3f)
+                    }
+
+                    val outputEditText = editText(R.id.outputEditText) {
+                        width = ViewGroup.LayoutParams.WRAP_CONTENT
+                        height = ViewGroup.LayoutParams.WRAP_CONTENT
+                        setEms(10)
+                        hint = resources.getText(R.string.output)
+                        inputType = InputType.TYPE_TEXT_VARIATION_PERSON_NAME
+                        addConstraint(ConstraintSet.END, R.id.layoutId, ConstraintSet.END)
+                        addConstraint(ConstraintSet.START, R.id.layoutId, ConstraintSet.START)
+                        addConstraint(ConstraintSet.TOP, R.id.sortButton, ConstraintSet.BOTTOM, dp(16))
+                    }
+
+                    button(R.id.sortButton) {
+                        width = ViewGroup.LayoutParams.WRAP_CONTENT
+                        height = ViewGroup.LayoutParams.WRAP_CONTENT
+                        text = resources.getText(R.string.sort)
+                        addConstraint(ConstraintSet.END, R.id.layoutId, ConstraintSet.END)
+                        addConstraint(ConstraintSet.START, R.id.layoutId, ConstraintSet.START)
+                        addConstraint(ConstraintSet.TOP, R.id.inputEditText, ConstraintSet.BOTTOM, dp(16))
+                        setOnClickListener {
+                            val array = ArrayList(inputEditText.text.split(" ").map { it.toInt() })
+                            quickSort(array)
+                            outputEditText.setText(array.joinToString(" ") { it.toString() })
+                        }
+                    }
+
+                    button(R.id.copyButton) {
+                        width = ViewGroup.LayoutParams.WRAP_CONTENT
+                        height = ViewGroup.LayoutParams.WRAP_CONTENT
+                        text = resources.getText(R.string.copy)
+                        addConstraint(ConstraintSet.END, R.id.layoutId, ConstraintSet.END)
+                        addConstraint(ConstraintSet.START, R.id.layoutId, ConstraintSet.START)
+                        addConstraint(ConstraintSet.TOP, R.id.outputEditText, ConstraintSet.BOTTOM, dp(16))
+                        setOnClickListener {
+                            val clipboard = getSystemService(Context.CLIPBOARD_SERVICE) as ClipboardManager
+                            val clip = ClipData.newPlainText("sorted data", outputEditText.text)
+                            clipboard.primaryClip = clip
+                        }
+                    }
+                }
+        )
     }
 }
