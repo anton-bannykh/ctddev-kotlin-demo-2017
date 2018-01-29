7a38bbac6d5f91848bf982b63bbc2322e1fb1d1b
Ilya <ilja.sahipoff@yandex.ru>
2018-01-29 19:37:48 +0300

    Added DSL

---
+++ app/src/main/java/com/example/demo/DSL.kt
@@ -0,0 +1,60 @@
+package com.example.demo
+
+import android.support.constraint.ConstraintLayout
+import android.support.constraint.ConstraintSet
+import android.support.v7.app.AppCompatActivity
+import android.view.View
+import android.widget.*
+
+@DslMarker
+annotation class LayoutBuilder
+
+@LayoutBuilder
+class MyConstraintLayout(private val activity: AppCompatActivity, private val layoutId: Int) {
+    private val constraintSet = ConstraintSet()
+    val LEFT = ConstraintSet.START
+    val RIGHT = ConstraintSet.END
+    val TOP = ConstraintSet.TOP
+    val BOTTOM = ConstraintSet.BOTTOM
+    val layout = ConstraintLayout(activity).apply {
+        id = layoutId
+    }
+
+    private fun <T : View> addElement(elem: T, init: T.() -> Unit): T {
+        layout.addView(elem)
+        constraintSet.clone(layout)
+        elem.init()
+        constraintSet.applyTo(layout)
+        return elem
+    }
+
+    fun <T : View> T.margin(startSide: Int, endID: Int, endSide: Int, margin: Int) {
+        constraintSet.connect(id, startSide, endID, endSide, dp(margin))
+    }
+
+    fun button(id: Int, init: Button.() -> Unit): Button {
+        val button = Button(activity)
+        button.id = id
+        return addElement(button, init)
+    }
+
+    fun textView(id: Int, init: TextView.() -> Unit): TextView {
+        val textView = TextView(activity)
+        textView.id = id
+        textView.textSize = 20f
+        return addElement(textView, init)
+    }
+
+    fun Button.onMyClick(init: () -> Unit) {
+        setOnClickListener { init() }
+    }
+
+    fun dp(value: Int): Int = (activity.applicationContext.resources.displayMetrics.density * value).toInt()
+
+}
+
+fun AppCompatActivity.createConstraintLayout(id: Int, init: MyConstraintLayout.() -> Unit): ConstraintLayout {
+    val element = MyConstraintLayout(this, id)
+    element.init()
+    return element.layout
+}
--- app/src/main/java/com/example/demo/MainActivity.kt
+++ app/src/main/java/com/example/demo/MainActivity.kt
@@ -2,37 +2,39 @@ package com.example.demo
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
 import android.widget.TextView
 import my.lib.sortH
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var txt: TextView
+    fun makeText(s: String) {
+        txt.text = s;
+    }
+
     val ms = intArrayOf(1, 4, 2, 5, 6, 4, -4, -43, 313, 242, 32, 42, 2, 4, 42)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        var tv = findViewById(R.id.tv) as TextView
-        var text: String
-        text = ""
-        for (it in ms) {
-            text += it
-            text += " "
-        }
-        tv.setText(text)
-        var btn_sort = findViewById(R.id.btn_sort) as Button
-        btn_sort.setOnClickListener {
-            sortH(ms)
-            val sorted_ms = ms
-            text = ""
-            for (it in sorted_ms) {
-                text += it
-                text += ' '
-            }
-            tv.setText(text)
-        }
+        setContentView(
+                createConstraintLayout(R.id.main_layout) {
+                    textView(R.id.tv) {
+                        text = (ms.contentToString())
+                        margin(TOP, R.id.main_layout, TOP, 80)
+                        margin(LEFT, R.id.main_layout, LEFT, 140)
+                    }
+                    button(R.id.btn_sort) {
+                        onMyClick {
+                            sortH(ms)
+                            makeText(ms.contentToString())
+                        }
+                        text = "SORT BY HEAPSORT"
+                        margin(RIGHT, R.id.main_layout, RIGHT, 8)
+                        margin(TOP, R.id.main_layout, TOP, 8)
+                        margin(LEFT, R.id.main_layout, RIGHT, 8)
+                    }
+                }
+        )
+        txt = findViewById<TextView>(R.id.tv)
     }
-
-    //fun test() = sumFun(1, 2, 3)
-
 }
 
